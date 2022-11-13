ca6bafa25819243a2cfbd7f6bcb7cd0f53721875
Thorsten Otto <admin@tho-otto.de>
2022-11-13 07:55:56 +0100

    Cleanup transfm.c

--- src/transfm.c
+++ src/transfm.c
@@ -47,37 +47,359 @@
 #include "ext_rsc.h"
 
 
-/*------------ lokale Funktionen ------------------*/
-static void actualize_convpopups(int dither_algo, int depth, int pal);
-static void do_transform(int conv_depth, int conv_dither, int conv_pal);
-
-static int dither_destruktiv(int dest_depth, int dest_dither, int dest_pal);
-static int autoreduce_image(void);
-
 /*------------ globales fÅr die Konvertierung ------*/
-static int conv_depth = 24;
-static int conv_dither = DIT1;
-static int conv_pal = CR_SYSPAL;
+static short conv_depth = 24;
+static short conv_dither = DIT1;
+static short conv_pal = CR_SYSPAL;
 
-static int tfm_fix_red[256];
-static int tfm_fix_blue[256];
-static int tfm_fix_green[256];
+static WORD tfm_fix_red[256];
+static WORD tfm_fix_blue[256];
+static WORD tfm_fix_green[256];
 
+
+
+/**************************************************************	*/
+/*			Aktualisieren der Popups im Wandeln-Dialog			*/
+/**************************************************************	*/
+static void actualize_convpopups(short dither_algo, short depth, short pal)
+{
+	WORD mode_pal;
+	WORD mode_dit;
+	WORD mode_load;
+	OBJECT *res;
+
+	res = wind_s[WIND_TRANSFORM].resource_form;
+
+	if (ditmod_info[dither_algo - 1]->pal_mode == FIXPAL)
+	{
+		conv_pal = CR_FIXPAL;
+		strncpy(res[CONV_PALPB].TextCast, colred_popup[CR_FIXPAL].TextCast, 15);
+		mode_pal = OS_DISABLED;
+		mode_dit = OS_ENABLED;
+		mode_load = OS_DISABLED;
+	}
+
+	if (depth > smurf_picture[active_pic]->depth || depth == 16 || depth == 24)
+	{
+		strcpy(res[CONV_PALPB].TextCast, "-");
+		strcpy(res[CONV_DITHPB].TextCast, "-");
+		mode_pal = OS_DISABLED;
+		mode_dit = OS_DISABLED;
+		mode_load = OS_DISABLED;
+	} else if (depth <= smurf_picture[active_pic]->depth)
+	{
+		strcpy(res[CONV_DITHPB].TextCast, col_pop[dither_algo].TextCast);
+		mode_dit = OS_ENABLED;
+
+		if (ditmod_info[dither_algo - 1]->pal_mode != FIXPAL)
+		{
+			strcpy(res[CONV_PALPB].TextCast, colred_popup[pal].TextCast);
+			mode_pal = OS_ENABLED;
+		}
+
+		if (pal == CR_FILEPAL)
+			mode_load = OS_ENABLED;
+		else
+			mode_load = OS_DISABLED;
+	}
+
+	if (depth == 1)
+	{
+		strcpy(res[CONV_PALPB].TextCast, "s/w");
+		mode_pal = OS_DISABLED;
+	}
+
+	change_object(&wind_s[WIND_TRANSFORM], CONV_PALPB, mode_pal, 1);
+	change_object(&wind_s[WIND_TRANSFORM], CONV_PALCB, mode_pal, 1);
+	change_object(&wind_s[WIND_TRANSFORM], CONV_DITHPB, mode_dit, 1);
+	change_object(&wind_s[WIND_TRANSFORM], CONV_DITHCB, mode_dit, 1);
+	change_object(&wind_s[WIND_TRANSFORM], TFM_LOADPAL, mode_load, 1);
+}
+
+
+/**************************************************************	*/
+/*		Ditherdispatcher fÅr destruktives Dithern 				*/
+/*	Der einzige Teil von Smurf, der nicht konstruktiv ist ...	*/
+/**************************************************************	*/
+static short dither_destruktiv(short dest_depth, short dest_dither, short dest_pal)
+{
+	uint8_t *dest_palette;
+	uint8_t dest_form;
+	short t;
+	SMURF_PIC *convpic;
+	MOD_ABILITY old_export_modabs;
+	DISPLAY_MODES new_display;
+	SYSTEM_INFO Cheat_sysinfo;
+
+	convpic = smurf_picture[active_pic];
+
+	if (dest_depth < 8)
+		dest_form = FORM_STANDARD;
+	else
+		dest_form = FORM_PIXELPAK;
+
+	/*----------- neue Strukturen zum Dithern generieren bzw. alte retten -----------*/
+	memcpy(&old_export_modabs, &export_mod_ability, sizeof(MOD_ABILITY));
+	memcpy(&new_display, &Display_Opt, sizeof(DISPLAY_MODES));
+	memcpy(&Cheat_sysinfo, &Sys_info, sizeof(SYSTEM_INFO));
+
+	/*----- sind wir in einer anderen Farbtiefe (muû eine andere NCT geladen werden)? -----*/
+	if (dest_pal == CR_SYSPAL && dest_depth != Sys_info.bitplanes)
+	{
+		if (loadNCT(dest_depth, &Cheat_sysinfo) != 0)
+		{
+			/* FIXME: translate */
+			Dialog.winAlert.openAlert("Kein NCT-File fÅr die gewÑhlte Farbtiefe gefunden. Dithering nicht mîglich.", NULL, NULL, NULL, 1);
+			return -1;
+		}
+	}
+
+	/*---------------- neue Strukturen zurechtbescheiûen -----*/
+	Cheat_sysinfo.bitplanes = dest_depth;
+	Cheat_sysinfo.Max_col = (1 << dest_depth) - 1;
+
+	memset(&export_mod_ability, 0x0, sizeof(MOD_ABILITY));
+	export_mod_ability.depth1 = dest_depth;
+	export_mod_ability.form1 = dest_form;
+
+	exp_conf.exp_depth = dest_depth;
+	exp_conf.exp_form = dest_form;
+
+	new_display.syspal_4 = dest_pal;
+	new_display.syspal_8 = dest_pal;
+	new_display.syspal_24 = dest_pal;
+	new_display.dither_4 = dest_dither;
+	new_display.dither_8 = dest_dither;
+	new_display.dither_24 = dest_dither;
+
+	/* destruktiv dithern */
+	export_dither_dispatcher(convpic, &Cheat_sysinfo, &new_display, tfm_fix_red, tfm_fix_green, tfm_fix_blue);
+
+	/*---------------- Bilddaten aus dem MFDB Åbertragen */
+	SMfree(convpic->pic_data);
+	convpic->pic_data = convpic->screen_pic->fd_addr;
+	convpic->depth = Cheat_sysinfo.bitplanes;
+	convpic->format_type = dest_form;
+
+	free(convpic->screen_pic);			/* MFDB freigeben, um das Dithering */
+	convpic->screen_pic = NULL;			/* problemlos zu ermîglichen */
+
+	/*---------------- Palette Åbertragen */
+	dest_palette = convpic->palette;
+	memset(dest_palette, 0x0, 1024);
+	for (t = 0; t < 256; t++)
+	{
+		*dest_palette++ = convpic->red[t];
+		*dest_palette++ = convpic->grn[t];
+		*dest_palette++ = convpic->blu[t];
+	}
+
+	/*---------------- und jetzt noch konvertieren, dann sind wir fertig. */
+	/* Wird wohl in Zukunft wegfallen kînnen */
+	/* f_convert(convpic, mod_abs, dest_colsys, SAME, 0); */
+
+	/*---------- alles aus der Cheat_sysinfo wegwerfen, -----*/
+	/*---------- was in der load_nct angefordert wurde ------*/
+	if (dest_pal == CR_SYSPAL && dest_depth != Sys_info.bitplanes)
+	{
+		free(Cheat_sysinfo.red);
+		free(Cheat_sysinfo.grn);
+		free(Cheat_sysinfo.blu);
+		free(Cheat_sysinfo.plane_table);
+		SMfree(Cheat_sysinfo.nc_table);
+	}
+
+	/* und jetzt Screendisplay neu dithern. */
+	if (!Sys_info.realtime_dither)
+		f_dither(convpic, &Sys_info, 0, NULL, &Display_Opt);
+
+	Window.redraw(&picture_windows[active_pic], NULL, 0, 0);
+
+	Dialog.busy.ok();
+
+	return 0;
+}
+
+
+
+/**************************************************************	*/
+/*						Bild wandeln							*/
+/**************************************************************	*/
+static void do_transform(short conv_depth, short conv_dither, short conv_pal)
+{
+	short picdepth;
+	MOD_ABILITY abs;
+
+	memset(&abs, 0, sizeof(MOD_ABILITY));	/* lîschen... */
+
+	abs.depth1 = conv_depth;
+
+	if (conv_depth < 8)
+		abs.form1 = FORM_STANDARD;
+	else
+		abs.form1 = FORM_PIXELPAK;
+
+	picdepth = smurf_picture[active_pic]->depth;
+
+	/*
+	 * 24 nach 16 Bit
+	 */
+	if (picdepth == 24 && conv_depth == 16)
+		tfm_24_to_16(smurf_picture[active_pic], SAME);
+	else
+		/* raufwÑrts ... */
+	if (conv_depth > picdepth || (conv_depth == 24 && picdepth == 24))
+		f_convert(smurf_picture[active_pic], &abs, RGB, SAME, 0);
+	else
+		/* ... und runterwÑrts */
+		dither_destruktiv(conv_depth, conv_dither, conv_pal);
+}
+
+
+static short autoreduce_image(void)
+{
+	uint8_t *rt, *gt, *bt;
+	uint8_t *data;
+	uint8_t R, G, B;
+	short idx;
+	short *intdata;
+	short depth;
+	BOOLEAN found = TRUE;
+	short ddepth;
+	long pixlen;
+	long t;
+	long colcount = 0;
+	long find = 0;
+	SMURF_PIC *convpic;
+
+	/* zu aller erst mal die Farben im Bild zÑhlen */
+	rt = SMalloc(32769L);
+	if (rt == NULL)
+		return M_MEMORY;
+
+	gt = SMalloc(32769L);
+	if (gt == NULL)
+	{
+		SMfree(rt);
+		return M_MEMORY;
+	}
+
+	bt = SMalloc(32769L);
+	if (bt == NULL)
+	{
+		SMfree(rt);
+		SMfree(gt);
+		return M_MEMORY;
+	}
+
+	Dialog.busy.reset(0, "zÑhle Farben...");
+
+	convpic = smurf_picture[active_pic];
+	pixlen = (long) convpic->pic_width * (long) convpic->pic_height;
+	depth = convpic->depth;
+	data = convpic->pic_data;
+	intdata = (short *) data;
+
+	for (t = 0; t < pixlen; t++)
+	{
+		if (!(t & 2047))
+			Dialog.busy.draw((short) ((long) t * 128L / pixlen));
+		if (convpic->format_type == FORM_PIXELPAK)
+		{
+			/*--------- RGB-Werte holen----- */
+			switch (depth)
+			{
+			case 24:
+				R = *data++;
+				G = *data++;
+				B = *data++;
+				break;
+
+			case 16:
+				idx = *intdata++;
+				R = idx >> 11;
+				G = (idx >> 6) & 31;
+				B = idx & 31;
+				break;
+
+			case 8:
+				idx = *data++;
+				R = *(convpic->palette + idx + idx + idx);
+				G = *(convpic->palette + idx + idx + idx + 1);
+				B = *(convpic->palette + idx + idx + idx + 2);
+				break;
+			}
+
+			/*----------- Farbe in der Tabelle suchen */
+			found = FALSE;
+			find = colcount;
+			while (find--)
+			{
+				if (rt[find] == R && gt[find] == G && bt[find] == B)
+				{
+					found = TRUE;
+					break;
+				}
+			}
+
+			if (found == FALSE)				/* NICHT gefunden? -> Eintragen und FarbzÑhler erhîhen */
+			{
+				colcount++;
+				rt[colcount] = R;
+				gt[colcount] = G;
+				bt[colcount] = B;
+			}
+		}
+
+		if (colcount > 32768L)
+			break;
+	}
+
+	/* Such-Tabellen freigeben */
+	SMfree(rt);
+	SMfree(gt);
+	SMfree(bt);
+
+	/* Bild reduzieren */
+	if (colcount > 32768L)
+		return 0;
+	else if (colcount <= 2)
+		ddepth = 1;
+	else if (colcount <= 4)
+		ddepth = 2;
+	else if (colcount <= 16)
+		ddepth = 4;
+	else if (colcount <= 256)
+		ddepth = 8;
+	else if (colcount <= 32768L)
+	{
+		if (smurf_picture[active_pic]->depth == 24)
+		{
+			/* FIXME: translate */
+			if (Dialog.winAlert.openAlert("Das Bild kann nach 16 Bit Farbtiefe gewandelt werden. Folge kann aber ein QualitÑtsverlust sein!", "Abbruch", " OK ", NULL, 1) == 2)
+				tfm_24_to_16(smurf_picture[active_pic], NEW);
+		}
+		return 0;
+	}
+
+	if (ddepth != depth)
+		dither_destruktiv(ddepth, DIT1, CR_MEDIAN);
+
+	return 0;
+}
 
 
 void transform_pic(void)
 {
-	static char tfm_filepal_name[32] = "feste Palette";
-	char *name,
-	 str[4];
+	static char tfm_filepal_name[32] = "feste Palette"; /* FIXME: translate */
+	char *name;
+	char str[4];
 	char *pal_loadpath;
-
-	int button,
-	 popbut,
-	 enablemode;
-	int dbutton,
-	 back;
-
+	WORD button;
+	WORD popbut;
+	WORD enablemode;
+	WORD dbutton;
+	WORD back;
 	OBJECT *resource;
 
 	button = Dialog.init(WIND_TRANSFORM, CONV_START);
@@ -301,355 +623,4 @@ void transform_pic(void)
 		Window.redraw(&wind_s[WIND_TRANSFORM], NULL, CONV_OLDPIC, 0);
 		break;
 	}
-
-	return;
-}
-
-
-/**************************************************************	*/
-/*			Aktualisieren der Popups im Wandeln-Dialog			*/
-/**************************************************************	*/
-static void actualize_convpopups(int dither_algo, int depth, int pal)
-{
-	int mode_pal,
-	 mode_dit,
-	 mode_load;
-
-	OBJECT *res;
-
-
-	res = wind_s[WIND_TRANSFORM].resource_form;
-
-	if (ditmod_info[dither_algo - 1]->pal_mode == FIXPAL)
-	{
-		conv_pal = CR_FIXPAL;
-		strncpy(res[CONV_PALPB].TextCast, colred_popup[CR_FIXPAL].TextCast, 15);
-		mode_pal = OS_DISABLED;
-		mode_dit = OS_ENABLED;
-		mode_load = OS_DISABLED;
-	}
-
-	if (depth > smurf_picture[active_pic]->depth || depth == 16 || depth == 24)
-	{
-		strcpy(res[CONV_PALPB].TextCast, "-");
-		strcpy(res[CONV_DITHPB].TextCast, "-");
-		mode_pal = OS_DISABLED;
-		mode_dit = OS_DISABLED;
-		mode_load = OS_DISABLED;
-	} else if (depth <= smurf_picture[active_pic]->depth)
-	{
-		strcpy(res[CONV_DITHPB].TextCast, col_pop[dither_algo].TextCast);
-		mode_dit = OS_ENABLED;
-
-		if (ditmod_info[dither_algo - 1]->pal_mode != FIXPAL)
-		{
-			strcpy(res[CONV_PALPB].TextCast, colred_popup[pal].TextCast);
-			mode_pal = OS_ENABLED;
-		}
-
-		if (pal == CR_FILEPAL)
-			mode_load = OS_ENABLED;
-		else
-			mode_load = OS_DISABLED;
-	}
-
-	if (depth == 1)
-	{
-		strcpy(res[CONV_PALPB].TextCast, "s/w");
-		mode_pal = OS_DISABLED;
-	}
-
-	change_object(&wind_s[WIND_TRANSFORM], CONV_PALPB, mode_pal, 1);
-	change_object(&wind_s[WIND_TRANSFORM], CONV_PALCB, mode_pal, 1);
-	change_object(&wind_s[WIND_TRANSFORM], CONV_DITHPB, mode_dit, 1);
-	change_object(&wind_s[WIND_TRANSFORM], CONV_DITHCB, mode_dit, 1);
-	change_object(&wind_s[WIND_TRANSFORM], TFM_LOADPAL, mode_load, 1);
-
-	return;
-}
-
-
-/**************************************************************	*/
-/*						Bild wandeln							*/
-/**************************************************************	*/
-static void do_transform(int conv_depth, int conv_dither, int conv_pal)
-{
-	char picdepth;
-
-	MOD_ABILITY abs;
-
-
-	memset(&abs, 0x0, sizeof(MOD_ABILITY));	/* lîschen... */
-
-	abs.depth1 = conv_depth;
-
-	if (conv_depth < 8)
-		abs.form1 = FORM_STANDARD;
-	else
-		abs.form1 = FORM_PIXELPAK;
-
-	picdepth = smurf_picture[active_pic]->depth;
-
-	/*
-	 * 24 nach 16 Bit
-	 */
-	if (picdepth == 24 && conv_depth == 16)
-		tfm_24_to_16(smurf_picture[active_pic], SAME);
-	else
-		/* raufwÑrts ... */
-	if (conv_depth > picdepth || (conv_depth == 24 && picdepth == 24))
-		f_convert(smurf_picture[active_pic], &abs, RGB, SAME, 0);
-	else
-		/* ... und runterwÑrts */
-		dither_destruktiv(conv_depth, conv_dither, conv_pal);
-}
-
-
-/**************************************************************	*/
-/*		Ditherdispatcher fÅr destruktives Dithern 				*/
-/*	Der einzige Teil von Smurf, der nicht konstruktiv ist ...	*/
-/**************************************************************	*/
-static int dither_destruktiv(int dest_depth, int dest_dither, int dest_pal)
-{
-	char *dest_palette,
-	 dest_form;
-
-	int t;
-
-	SMURF_PIC *convpic;
-	MOD_ABILITY old_export_modabs;
-	DISPLAY_MODES new_display;
-	SYSTEM_INFO Cheat_sysinfo;
-
-	convpic = smurf_picture[active_pic];
-
-	if (dest_depth < 8)
-		dest_form = FORM_STANDARD;
-	else
-		dest_form = FORM_PIXELPAK;
-
-	/*----------- neue Strukturen zum Dithern generieren bzw. alte retten -----------*/
-	memcpy(&old_export_modabs, &export_mod_ability, sizeof(MOD_ABILITY));
-	memcpy(&new_display, &Display_Opt, sizeof(DISPLAY_MODES));
-	memcpy(&Cheat_sysinfo, &Sys_info, sizeof(SYSTEM_INFO));
-
-	/*----- sind wir in einer anderen Farbtiefe (muû eine andere NCT geladen werden)? -----*/
-	if (dest_pal == CR_SYSPAL && dest_depth != Sys_info.bitplanes)
-	{
-		if (loadNCT(dest_depth, &Cheat_sysinfo) != 0)
-		{
-			Dialog.winAlert.openAlert("Kein NCT-File fÅr die gewÑhlte Farbtiefe gefunden. Dithering nicht mîglich.",
-									  NULL, NULL, NULL, 1);
-			return (-1);
-		}
-	}
-
-	/*---------------- neue Strukturen zurechtbescheiûen -----*/
-	Cheat_sysinfo.bitplanes = dest_depth;
-	Cheat_sysinfo.Max_col = (1 << dest_depth) - 1;
-
-	memset(&export_mod_ability, 0x0, sizeof(MOD_ABILITY));
-	export_mod_ability.depth1 = dest_depth;
-	export_mod_ability.form1 = dest_form;
-
-	exp_conf.exp_depth = dest_depth;
-	exp_conf.exp_form = dest_form;
-
-	new_display.syspal_4 = dest_pal;
-	new_display.syspal_8 = dest_pal;
-	new_display.syspal_24 = dest_pal;
-	new_display.dither_4 = dest_dither;
-	new_display.dither_8 = dest_dither;
-	new_display.dither_24 = dest_dither;
-
-	/* destruktiv dithern */
-	export_dither_dispatcher(convpic, &Cheat_sysinfo, &new_display, tfm_fix_red, tfm_fix_green, tfm_fix_blue);
-
-	/*---------------- Bilddaten aus dem MFDB Åbertragen */
-	SMfree(convpic->pic_data);
-	convpic->pic_data = convpic->screen_pic->fd_addr;
-	convpic->depth = Cheat_sysinfo.bitplanes;
-	convpic->format_type = dest_form;
-
-	free(convpic->screen_pic);			/* MFDB freigeben, um das Dithering */
-	convpic->screen_pic = NULL;			/* problemlos zu ermîglichen */
-
-	/*---------------- Palette Åbertragen */
-	dest_palette = convpic->palette;
-	memset(dest_palette, 0x0, 1024);
-	for (t = 0; t < 256; t++)
-	{
-		*dest_palette++ = (char) convpic->red[t];
-		*dest_palette++ = (char) convpic->grn[t];
-		*dest_palette++ = (char) convpic->blu[t];
-	}
-
-	/*---------------- und jetzt noch konvertieren, dann sind wir fertig. */
-	/* Wird wohl in Zukunft wegfallen kînnen */
-	/* f_convert(convpic, mod_abs, dest_colsys, SAME, 0); */
-
-	/*---------- alles aus der Cheat_sysinfo wegwerfen, -----*/
-	/*---------- was in der load_nct angefordert wurde ------*/
-	if (dest_pal == CR_SYSPAL && dest_depth != Sys_info.bitplanes)
-	{
-		free(Cheat_sysinfo.red);
-		free(Cheat_sysinfo.grn);
-		free(Cheat_sysinfo.blu);
-		free(Cheat_sysinfo.plane_table);
-		SMfree(Cheat_sysinfo.nc_table);
-	}
-
-	/* und jetzt Screendisplay neu dithern. */
-	if (!Sys_info.realtime_dither)
-		f_dither(convpic, &Sys_info, 0, NULL, &Display_Opt);
-
-	Window.redraw(&picture_windows[active_pic], NULL, 0, 0);
-
-	Dialog.busy.ok();
-
-	return (0);
-}
-
-
-
-static int autoreduce_image(void)
-{
-	char *rt,
-	*gt,
-	*bt,
-	*data;
-	char R,
-	 G,
-	 B;
-
-	int idx;
-	short *intdata;
-	int depth,
-	 found = 1,
-		ddepth;
-
-	long pixlen,
-	 t,
-	 colcount = 0,
-		find = 0;
-
-	SMURF_PIC *convpic;
-
-
-	/* zu aller erst mal die Farben im Bild zÑhlen */
-	rt = SMalloc(32769L);
-	if (rt == NULL)
-		return (M_MEMORY);
-
-	gt = SMalloc(32769L);
-	if (gt == NULL)
-	{
-		SMfree(rt);
-		return (M_MEMORY);
-	}
-
-	bt = SMalloc(32769L);
-	if (bt == NULL)
-	{
-		SMfree(rt);
-		SMfree(gt);
-		return (M_MEMORY);
-	}
-
-	Dialog.busy.reset(0, "zÑhle Farben...");
-
-	convpic = smurf_picture[active_pic];
-	pixlen = (long) convpic->pic_width * (long) convpic->pic_height;
-	depth = convpic->depth;
-	data = convpic->pic_data;
-	intdata = (short *) data;
-
-	for (t = 0; t < pixlen; t++)
-	{
-		if (!(t & 2047))
-			Dialog.busy.draw((int) ((long) t * 128L / pixlen));
-		if (convpic->format_type == FORM_PIXELPAK)
-		{
-			/*--------- RGB-Werte holen----- */
-			switch (depth)
-			{
-			case 24:
-				R = *data++;
-				G = *data++;
-				B = *data++;
-				break;
-
-			case 16:
-				idx = *intdata++;
-				R = idx >> 11;
-				G = (idx >> 6) & 31;
-				B = idx & 31;
-				break;
-
-			case 8:
-				idx = *data++;
-				R = *(convpic->palette + idx + idx + idx);
-				G = *(convpic->palette + idx + idx + idx + 1);
-				B = *(convpic->palette + idx + idx + idx + 2);
-				break;
-			}
-
-			/*----------- Farbe in der Tabelle suchen */
-			found = 0;
-			find = colcount;
-			while (find--)
-			{
-				if (rt[find] == R && gt[find] == G && bt[find] == B)
-				{
-					found = 1;
-					break;
-				}
-			}
-
-			if (found == 0)				/* NICHT gefunden? -> Eintragen und FarbzÑhler erhîhen */
-			{
-				colcount++;
-				rt[colcount] = R;
-				gt[colcount] = G;
-				bt[colcount] = B;
-			}
-		}
-
-		if (colcount > 32768L)
-			break;
-	}
-
-/*	printf("\n %li Farben", colcount);*/
-
-	/* Such-Tabellen freigeben */
-	SMfree(rt);
-	SMfree(gt);
-	SMfree(bt);
-
-	/* Bild reduzieren */
-	if (colcount > 32768L)
-		return (0);
-	else if (colcount <= 2)
-		ddepth = 1;
-	else if (colcount <= 4)
-		ddepth = 2;
-	else if (colcount <= 16)
-		ddepth = 4;
-	else if (colcount <= 256)
-		ddepth = 8;
-	else if (colcount <= 32768L)
-	{
-		if (smurf_picture[active_pic]->depth == 24)
-			if (Dialog.winAlert.
-				openAlert
-				("Das Bild kann nach 16 Bit Farbtiefe gewandelt werden. Folge kann aber ein QualitÑtsverlust sein!",
-				 "Abbruch", " OK ", NULL, 1) == 2)
-				tfm_24_to_16(smurf_picture[active_pic], NEW);
-		return (0);
-	}
-
-	if (ddepth != depth)
-		dither_destruktiv(ddepth, DIT1, CR_MEDIAN);
-
-	return (0);
 }
