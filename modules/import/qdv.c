09e3f94569ea4fe780307d9a8615444bf7a5792d
Thorsten Otto <admin@tho-otto.de>
2022-11-14 11:22:25 +0100

    Cleanup import/qdv.c

--- modules/import/qdv.c
+++ modules/import/qdv.c
@@ -43,7 +43,7 @@ MOD_INFO module_info = {
 	"Edit 1", "Edit 2", "Edit 3", "Edit 4",
 	/* min/max values for slider */
 	0,128, 0,128, 0,128, 0,128,
-	/* min/max values for slider */
+	/* min/max values for editobjects */
 	0,10, 0,10, 0,10, 0,10,
 	/* default values for slider */
 	0, 0, 0, 0,
@@ -57,131 +57,12 @@ MOD_INFO module_info = {
 	NULL, NULL, NULL, NULL, NULL, NULL
 };
 
-static void *(*SMalloc)(long amount);
-static void (*SMfree)(void *ptr);
-
 struct header {
 	uint16_t width;
 	uint16_t height;
 	uint8_t colors;
 	uint8_t data[];
 };
-
-static short SwitchOrient(uint8_t *est, uint16_t width, uint16_t height, uint8_t depth, uint8_t o31);
-
-/* -------------------------------------------------*/
-/* -------------------------------------------------*/
-/*				GEM Icon-Format (ICN)				*/
-/*		1, 2, 4, 8 Bit, unkomprimiert				*/
-/* -------------------------------------------------*/
-/* -------------------------------------------------*/
-short imp_module_main(GARGAMEL *smurf_struct)
-{
-	uint8_t *pic_data;
-	uint16_t width;
-	uint16_t height;
-	uint8_t colors;
-	uint8_t depth;
-	void *pixbuf;
-	uint8_t *pal;
-	uint8_t *palbuf;
-	uint8_t planes;
-	char help_str[3];
-	char impmessage[22];
-	uint16_t y;
-	long dataoffset;
-	uint8_t *dest;
-	long planelength;
-	uint8_t *linebuf;
-	
-	SMalloc = smurf_struct->services->SMalloc;
-	SMfree = smurf_struct->services->SMfree;
-
-	pic_data = smurf_struct->smurf_pic->pic_data;
-	if (smurf_struct->smurf_pic->file_len - 3 - 5 != *((uint16_t *)pic_data + 0) * *((uint16_t *)pic_data + 1) + pic_data[4] * 3)
-		return M_INVALID;
-
-	colors = pic_data[4] + 1;
-	if (colors == 2)
-	{
-		depth = 1;
-		planes = 1;
-	} else if (colors == 16)
-	{
-		depth = 4;
-		planes = 4;
-	} else
-	{
-		depth = 8;
-		planes = 1;
-	}
-	width = *((uint16_t *)pic_data + 0);
-	height = *((uint16_t *)pic_data + 1);
-	dataoffset = colors * 3 + 5;
-	strncpy(smurf_struct->smurf_pic->format_name, "Macintosh Giffer .QDV", 21);
-	smurf_struct->smurf_pic->pic_width = width;
-	smurf_struct->smurf_pic->pic_height = height;
-	smurf_struct->smurf_pic->depth = depth;
-	strcpy(impmessage, "Mac Giffer ");
-	strcat(impmessage, itoa(depth, help_str, 10));
-	strcat(impmessage, " Bit");
-	smurf_struct->services->reset_busybox(128, impmessage);
-	
-	if (depth != 4)
-	{
-		dest = pic_data;
-		planelength = ((long) width * height * depth) >> 3;
-		memmove(dest, &pic_data[dataoffset], planelength);
-		_Mshrink(dest, planelength);
-	} else
-	{
-		uint16_t x;
-
-		planelength = ((long) width * height) >> 3;
-		dest = SMalloc(((long) width * height) / 2 + 7);
-		if (dest == NULL)
-			return M_MEMORY;
-		pixbuf = dest;
-		linebuf = (uint8_t *)Malloc(width);
-		y = 0;
-		do
-		{
-			x = 0;
-			do
-			{
-				/* BUG? pic_data still points to the header, not the data */
-				linebuf[x] = *pic_data++;
-			} while (++x < width);
-			dest += setpix_std_line_TC(linebuf, dest, 4, planelength, width);
-		} while (++y < height);
-		Mfree(linebuf);
-		dest = pixbuf;
-		SMfree(pic_data);
-	}
-	
-	SwitchOrient(dest, width, height, depth, planes);
-	
-	smurf_struct->smurf_pic->pic_data = dest;
-	if (depth < 8)
-		smurf_struct->smurf_pic->format_type = FORM_STANDARD;
-	else
-		smurf_struct->smurf_pic->format_type = FORM_PIXELPAK;
-	
-	pal = smurf_struct->smurf_pic->palette;
-	palbuf = pic_data + 5;
-	{
-		uint16_t x;
-		for (x = 0; x < colors; x++)
-		{
-			*pal++ = *palbuf++;
-			*pal++ = *palbuf++;
-			*pal++ = *palbuf++;
-		}
-	}
-	smurf_struct->smurf_pic->col_format = RGB;
-	
-	return M_PICDONE;
-}
 
 
 static short SwitchOrient(uint8_t *dest, uint16_t width, uint16_t height, uint8_t depth, uint8_t planes)
@@ -223,4 +104,124 @@ static short SwitchOrient(uint8_t *dest, uint16_t width, uint16_t height, uint8_
 	Mfree(linebuf);
 	
 	return 0;
+}
+
+
+/* -------------------------------------------------*/
+/* -------------------------------------------------*/
+/*				GEM Icon-Format (ICN)				*/
+/*		1, 2, 4, 8 Bit, unkomprimiert				*/
+/* -------------------------------------------------*/
+/* -------------------------------------------------*/
+short imp_module_main(GARGAMEL *smurf_struct)
+{
+	struct header *pic_data;
+	uint16_t width;
+	uint16_t height;
+	uint8_t colors;
+	uint8_t depth;
+	void *pixbuf;
+	uint8_t planes;
+	char help_str[3];
+	char impmessage[22];
+	uint16_t y;
+	long dataoffset;
+	uint8_t *dest;
+	long planelength;
+	uint8_t *linebuf;
+	
+	pic_data = smurf_struct->smurf_pic->pic_data;
+	width = pic_data->width;
+	height = pic_data->height;
+	colors = pic_data->colors + 1;
+	/* BUG? that is only true for depth 8 */
+	if (smurf_struct->smurf_pic->file_len - 5 != width * height + colors * 3)
+		return M_INVALID;
+
+	if (colors == 2)
+	{
+		depth = 1;
+		planes = 1;
+	} else if (colors == 16)
+	{
+		depth = 4;
+		planes = 4;
+	} else
+	{
+		depth = 8;
+		planes = 1;
+	}
+	dataoffset = colors * 3;
+	strncpy(smurf_struct->smurf_pic->format_name, "Macintosh Giffer .QDV", 21);
+	smurf_struct->smurf_pic->pic_width = width;
+	smurf_struct->smurf_pic->pic_height = height;
+	smurf_struct->smurf_pic->depth = depth;
+	strcpy(impmessage, "Mac Giffer ");
+	strcat(impmessage, itoa(depth, help_str, 10));
+	strcat(impmessage, " Bit");
+	smurf_struct->services->reset_busybox(128, impmessage);
+	
+	if (depth != 4)
+	{
+		dest = (uint8_t *)pic_data;
+		planelength = ((unsigned long) width * height * depth) >> 3;
+		memmove(dest, &pic_data->data[dataoffset], planelength);
+		_Mshrink(dest, planelength);
+	} else
+	{
+		uint16_t x;
+		uint8_t *src;
+
+		src = &pic_data->data[dataoffset];
+		planelength = ((unsigned long) width * height) >> 3;
+		dest = smurf_struct->services->SMalloc(((unsigned long) width * height) / 2 + 7);
+		if (dest == NULL)
+			return M_MEMORY;
+		pixbuf = dest;
+		linebuf = (uint8_t *)smurf_struct->services->SMalloc(width);
+		if (linebuf == NULL)
+		{
+			smurf_struct->services->SMfree(dest);
+			return M_MEMORY;
+		}
+		y = 0;
+		do
+		{
+			x = 0;
+			do
+			{
+				linebuf[x] = *src++;
+			} while (++x < width);
+			dest += setpix_std_line_TC(linebuf, dest, 4, planelength, width);
+		} while (++y < height);
+		smurf_struct->services->SMfree(linebuf);
+		dest = pixbuf;
+		smurf_struct->services->SMfree(pic_data);
+	}
+	
+	SwitchOrient(dest, width, height, depth, planes);
+	
+	smurf_struct->smurf_pic->pic_data = dest;
+	if (depth < 8)
+		smurf_struct->smurf_pic->format_type = FORM_STANDARD;
+	else
+		smurf_struct->smurf_pic->format_type = FORM_PIXELPAK;
+	
+	{
+		uint16_t x;
+		uint8_t *pal;
+		uint8_t *palbuf;
+
+		pal = smurf_struct->smurf_pic->palette;
+		palbuf = pic_data->data;
+		for (x = 0; x < colors; x++)
+		{
+			*pal++ = *palbuf++;
+			*pal++ = *palbuf++;
+			*pal++ = *palbuf++;
+		}
+	}
+	smurf_struct->smurf_pic->col_format = RGB;
+	
+	return M_PICDONE;
 }
