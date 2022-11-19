7d73e2ec121555c9edcec2610a647339440ed7d0
Thorsten Otto <admin@tho-otto.de>
2022-11-19 18:22:07 +0100

    Cleanup edit/border

--- modules/edit/border.c
+++ modules/edit/border.c
@@ -31,224 +31,216 @@
 #include "smurfine.h"
 
 /*-----> Modul --------*/
-int do_it(GARGAMEL *smurf_struct);
 
 
 /*--------------- Globale Variablen --------------------------*/
-unsigned long busycount, busymax, busycall;
+static unsigned long busycount;
+static unsigned long busymax;
+static unsigned long busycall;
 
 
 /*--------------- Infostruktur fr Hauptprogramm -----*/
-MOD_INFO    module_info=
-{
-    "Border-Me-Up..",                      /* Name des Moduls */
-    0x0010,
-    "J”rg Dittmer",                                 /* Autor */
-    "","","","","","","","","","",  /* 10 Extensionen fr Importer */
+MOD_INFO module_info = {
+	"Border-Me-Up..",					/* Name des Moduls */
+	0x0010,
+	"J”rg Dittmer",						/* Autor */
+	{ "", "", "", "", "", "", "", "", "", "" },	/* 10 Extensionen fr Importer */
 /* 4 Sliderberschriften: max 8 */
-    "Red",
-    "Green",
-    "Blue",
-    "",
+	"Red",
+	"Green",
+	"Blue",
+	"",
 /* 4 Checkboxberschriften: */
-    "Center",
-    "Use Color",
-    "",
-    "",
+	"Center",
+	"Use Color",
+	"",
+	"",
 /* 4 Edit-Objekt-šberschriften: */
-    "left",
-    "right",
-    "up",
-    "down",
+	"left",
+	"right",
+	"up",
+	"down",
 /* min/max-Werte fr Slider */
-    0,255,
-    0,255,
-    0,255,
-    0,0,
+	0, 255,
+	0, 255,
+	0, 255,
+	0, 0,
 /* min/max fr Editobjekte */
-    0,32768L,
-    0,32768L,
-    0,32768L,
-    0,32768L,
+	0, 32768L,
+	0, 32768L,
+	0, 32768L,
+	0, 32768L,
 /* Defaultwerte fr Slider, Check und Edit */
-    0,0,0,0,
-    0,0,0,0,
-    0,0,0,0,
-    1,
-    "Bild 1"
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	1,
+	"Bild 1", NULL, NULL, NULL, NULL, NULL
 };
- 
+
 
 /*--------------------- Was kann ich ? ----------------------*/
-MOD_ABILITY  module_ability = {
-                        24, 0, 0, 0, 0, 0, 0, 0,    /* Farbtiefen */
-            /* Grafikmodi: */
-                        FORM_PIXELPAK,
-                        FORM_BOTH,
-                        FORM_BOTH,
-                        FORM_BOTH,
-                        FORM_BOTH,
-                        FORM_BOTH,
-                        FORM_BOTH,
-                        FORM_BOTH,
-                        0
-                        };
+MOD_ABILITY module_ability = {
+	24, 0, 0, 0, 0, 0, 0, 0,			/* Farbtiefen */
+	/* Grafikmodi: */
+	FORM_PIXELPAK,
+	FORM_BOTH,
+	FORM_BOTH,
+	FORM_BOTH,
+	FORM_BOTH,
+	FORM_BOTH,
+	FORM_BOTH,
+	FORM_BOTH,
+	0
+};
 
 
+
+
+
+/*--------------- SCALING ----------------------------*/
+static short do_it(GARGAMEL *smurf_struct)
+{
+	SMURF_PIC *picture;
+	short width, height;
+	uint8_t *pic;
+	uint8_t *n_pic;
+	uint8_t *offset_a;
+	uint8_t *offset_b;
+#if 0
+	long bpl_a;
+#endif
+	long bpl_b;
+	uint8_t bkgrd_r, bkgrd_g, bkgrd_b;
+	short bd_left, bd_right, bd_up, bd_down;
+	short n_width, n_height, x_pos, y_pos;
+	short x, y;
+
+	/*--- Slider auslesen ---------------------- */
+
+	bd_left = (short) smurf_struct->edit1;
+	bd_right = (short) smurf_struct->edit2;
+	bd_up = (short) smurf_struct->edit3;
+	bd_down = (short) smurf_struct->edit4;
+
+	/*--- Bilddaten auslesen --------------------*/
+
+	picture = smurf_struct->smurf_pic;
+	width = picture->pic_width;
+	height = picture->pic_height;
+	pic = picture->pic_data;
+
+#if 0
+	bpl_a = (long) width * 3L;
+#endif
+
+	/*--- Neues Bild festlegen ----------------*/
+	n_width = bd_left + bd_right + width;
+	n_height = bd_up + bd_down + height;
+	bpl_b = (long) n_width *3L;
+
+	n_pic = (uint8_t *)Malloc((long) n_width * n_height * 3L);
+	if (n_pic == NULL)
+		return M_MEMORY;
+
+
+	if (smurf_struct->check1)			/* Zentriert */
+	{
+		x_pos = (n_width - width) / 2;
+		y_pos = (n_height - height) / 2;
+	} else
+	{
+		x_pos = bd_left;
+		y_pos = bd_up;
+	}
+
+	if (smurf_struct->check2)
+	{
+		bkgrd_r = smurf_struct->slide1;
+		bkgrd_g = smurf_struct->slide2;
+		bkgrd_b = smurf_struct->slide3;
+	} else
+	{
+		bkgrd_r = pic[0];
+		bkgrd_g = pic[1];
+		bkgrd_b = pic[2];
+	}
+
+	offset_b = n_pic;
+	for (y = 0; y < n_height; y++)
+	{
+		for (x = 0; x < n_width; x++)
+		{
+			*offset_b++ = bkgrd_r;
+			*offset_b++ = bkgrd_g;
+			*offset_b++ = bkgrd_b;
+		}
+	}
+
+
+
+	/*--- BusyBox Vorberechnungen ----------------------*/
+	busycount = 0;
+	busycall = 1;
+	busymax = height;
+	if (busymax > 32)
+		busycall = 7;
+	if (busymax > 256)
+		busycall = 31;
+	busymax = (busymax << 10) / 127;
+
+
+	/*--- Hauptroutine --------------------*/
+
+	offset_a = pic;
+	for (y = 0; y < height; y++)
+	{
+		busycount++;
+		if (!(busycount & busycall))
+			smurf_struct->services->busybox((short) ((busycount << 10) / busymax));
+
+		offset_b = n_pic + (long) (y + y_pos) * bpl_b + (long) (x_pos) * 3L;
+		for (x = 0; x < width; x++)
+		{
+			*offset_b++ = *offset_a++;
+			*offset_b++ = *offset_a++;
+			*offset_b++ = *offset_a++;
+		}
+	}
+
+	picture->pic_width = n_width;
+	picture->pic_height = n_height;
+	picture->pic_data = n_pic;
+
+	Mfree(pic);
+
+	smurf_struct->event_par[0] = 0;
+	smurf_struct->event_par[1] = -1;
+
+	return M_PICDONE;
+}
 
 
 /*-----------------------  FUNCTION MAIN --------------------------*/
 void edit_module_main(GARGAMEL *smurf_struct)
 {
-int SmurfMessage;
-static int module_id;
+	short module_id;
 
-SmurfMessage = smurf_struct->module_mode; 
-
-/* Wenn das Modul aufgerufen wurde, */
-if(SmurfMessage == MSTART)
-{
-    module_id=smurf_struct->module_number;
-
-    smurf_struct->services->f_module_prefs(&module_info, module_id);
-    smurf_struct->module_mode=M_WAITING;
-    return; 
-}
-
-/* Wenn das Modul gestartet wurde */
-if(SmurfMessage == MEXEC)
-{
-    smurf_struct->module_mode = do_it(smurf_struct);
-    return;
-}
-
-/* Wenn das Modul sich verpissen soll */
-if(SmurfMessage == MTERM)
-{
-    smurf_struct->module_mode=M_EXIT;
-    return; 
-}
-
-} /*ende*/
-
-
-
-/*--------------- SCALING ----------------------------*/
-int do_it(GARGAMEL *smurf_struct)
-{
-    SMURF_PIC *picture;
-    int width, height;
-    char *pic, *n_pic, *offset_a, *offset_b;
-#if 0
-    long bpl_a;
-#endif
-    long bpl_b;
-    char bkgrd_r, bkgrd_g, bkgrd_b; 
-    int bd_left, bd_right, bd_up, bd_down;
-    int n_width, n_height, x_pos, y_pos;
-    
-    int x, y;
-    
-
-    /*--- Slider auslesen ---------------------- */
-    
-    bd_left = (int)smurf_struct->edit1;
-    bd_right = (int)smurf_struct->edit2;    
-    bd_up = (int)smurf_struct->edit3;
-    bd_down = (int)smurf_struct->edit4;
-    
-    /*--- Bilddaten auslesen --------------------*/
- 
-    picture = smurf_struct->smurf_pic;  
-    width   = picture->pic_width;
-    height  = picture->pic_height; 
-    pic     = picture->pic_data;
-    
-#if 0
-    bpl_a = (long)width *3L;
-#endif
-    
-    /*--- Neues Bild festlegen ----------------*/
-    n_width = bd_left + bd_right + width;
-    n_height = bd_up + bd_down + height;        
-    bpl_b = (long)n_width * 3L;
-    
-    n_pic = Malloc((long)n_width * n_height *3L);
-    if(n_pic == NULL)
-        return(M_MEMORY);
-    
-    
-    if(smurf_struct->check1)  /* Zentriert */
-    {
-        x_pos = (n_width-width)/2;
-        y_pos = (n_height-height)/2;
-    }
-    else
-    {
-        x_pos = bd_left;
-        y_pos = bd_up;
-    } 
-    
-    if(smurf_struct->check2)
-    {
-        bkgrd_r = smurf_struct->slide1; 
-        bkgrd_g = smurf_struct->slide2; 
-        bkgrd_b = smurf_struct->slide3;
-    }
-    else
-    {
-        bkgrd_r = pic[0]; 
-        bkgrd_g = pic[1]; 
-        bkgrd_b = pic[2]; 
-    }
-     
-    offset_b = n_pic;
-    for(y=0; y<n_height; y++)
-    {
-        for(x=0; x< n_width; x++)
-        {
-            *offset_b++ = bkgrd_r;
-            *offset_b++ = bkgrd_g;
-            *offset_b++ = bkgrd_b;
-        }
-    }
-     
-     
-     
-    /*--- BusyBox Vorberechnungen ----------------------*/
-    busycount = 0;
-    busycall =  1;
-    busymax = height;
-    if(busymax >  32) busycall =  7;
-    if(busymax > 256) busycall = 31;
-    busymax = (busymax<<10) /127;
-
-    
-    /*--- Hauptroutine --------------------*/
-
-    offset_a = pic;
-    for (y=0; y<height; y++)
-    {
-        busycount++;
-        if(!(busycount & busycall)) smurf_struct->services->busybox((int)((busycount<<10) / busymax));
-            
-        offset_b = n_pic + (long)(y+y_pos)*bpl_b + (long)(x_pos)*3L;
-        for(x=0; x<width; x++)
-        {
-            *offset_b++ = *offset_a++;
-            *offset_b++ = *offset_a++;
-            *offset_b++ = *offset_a++;
-        }
-    }
-    
-    picture->pic_width = n_width;
-    picture->pic_height = n_height;
-    picture->pic_data = n_pic;
-    
-    Mfree(pic);
-    
-    smurf_struct->event_par[0] = 0;
-    smurf_struct->event_par[1] = -1;
-
-    return(M_PICDONE);
+	switch (smurf_struct->module_mode)
+	{
+	case MSTART:
+		/* Wenn das Modul aufgerufen wurde, */
+		module_id = smurf_struct->module_number;
+		smurf_struct->services->f_module_prefs(&module_info, module_id);
+		smurf_struct->module_mode = M_WAITING;
+		break;
+	case MEXEC:
+		/* Wenn das Modul gestartet wurde */
+		smurf_struct->module_mode = do_it(smurf_struct);
+		break;
+	case MTERM:
+		/* Wenn das Modul sich verpissen soll */
+		smurf_struct->module_mode = M_EXIT;
+		break;
+	}
 }
