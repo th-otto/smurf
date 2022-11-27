0aa746d28dca1949fc4a85e728eb8f25a02165b6
Thorsten Otto <admin@tho-otto.de>
2022-11-27 15:16:38 +0100

    Cleanup import/esm

--- modules/import/esm.c
+++ modules/import/esm.c
@@ -29,18 +29,6 @@
 /*	  Ein Modul aus der Reihe der 2 Minuten Decoder			*/
 /* =========================================================*/
 
-#include "country.h"
-
-#if COUNTRY==1
-#define ERROR "[1][unbekanntes ESM. Bild bitte an uns einschicken!][ OK ]"
-#elif COUNTRY==0
-#define ERROR "[1][unbekanntes ESM. Bild bitte an uns einschicken!][ OK ]"
-#elif COUNTRY==2
-#define ERROR "[1][unbekanntes ESM. Bild bitte an uns einschicken!][ OK ]"
-#else
-#error "Keine Sprache!"
-#endif
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -48,37 +36,40 @@
 #include "import.h"
 #include "smurfine.h"
 
+#include "country.h"
+
 /* Infostruktur fÅr Hauptmodul */
-MOD_INFO module_info = {"ESM Enhanced Simplex",
-						0x0010,
-						"Christian Eyrich",
-						"ESM", "", "", "", "",
-						"", "", "", "", "",
-						"Slider 1",
-						"Slider 2",
-						"Slider 3",
-						"Slider 4",
-						"Checkbox 1",
-						"Checkbox 2",
-						"Checkbox 3",
-						"Checkbox 4",
-						"Edit 1",
-						"Edit 2",
-						"Edit 3",
-						"Edit 4",
-						0,128,
-						0,128,
-						0,128,
-						0,128,
-						0,10,
-						0,10,
-						0,10,
-						0,10,
-						0, 0, 0, 0,
-						0, 0, 0, 0,
-						0, 0, 0, 0,
-						0
-						};
+MOD_INFO module_info = {
+	"ESM Bitmap Modul",
+	0x0050,
+	"Dale Russell",
+	{ "ESM", "", "", "", "", "", "", "", "", "" },
+	"Slider 1",
+	"Slider 2",
+	"Slider 3",
+	"Slider 4",
+	"Checkbox 1",
+	"Checkbox 2",
+	"Checkbox 3",
+	"Checkbox 4",
+	"Edit 1",
+	"Edit 2",
+	"Edit 3",
+	"Edit 4",
+	0, 128,
+	0, 128,
+	0, 128,
+	0, 128,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0,
+	NULL, NULL, NULL, NULL, NULL, NULL
+};
 
 
 /* -------------------------------------------------*/
@@ -89,42 +80,66 @@ MOD_INFO module_info = {"ESM Enhanced Simplex",
 /* -------------------------------------------------*/
 short imp_module_main(GARGAMEL *smurf_struct)
 {
-	char *buffer, *ziel, *pal, *ppal,
-		 BitsPerPixel;
-	char dummy[3], impmessag[21];
+	uint8_t *buffer;
+	uint8_t *ziel;
+	uint8_t *pal;
+	uint8_t *ppal;
+	unsigned short BitsPerPixel;
+	char dummy[4];
+	char impmessag[22];
 
-	unsigned int width, height, i, DataOffset;
+	unsigned short width, height;
+	unsigned short i;
+	unsigned long DataOffset;
 
 	unsigned long len;
 
-
 	buffer = smurf_struct->smurf_pic->pic_data;
 
-	if(strncmp(buffer, "TMS", 3) != 0)
-		return(M_INVALID);
-	else
+	if (strncmp(buffer, "TMS", 3) != 0)
 	{
-		DataOffset = *(unsigned int *)(buffer + 0x04);
+		return M_INVALID;
+	} else
+	{
+		DataOffset = *(uint16_t *) (buffer + 0x04);
 
-		width = *(unsigned int *)(buffer + 0x06);
-		height = *(unsigned int *)(buffer + 0x08);
+		width = *(uint16_t *) (buffer + 0x06);
+		height = *(uint16_t *) (buffer + 0x08);
 
-		BitsPerPixel = *(unsigned int *)(buffer + 0x0a);
+		BitsPerPixel = *(uint16_t *) (buffer + 0x0a);
 
-		strncpy(smurf_struct->smurf_pic->format_name, "Enhanced Simplex .ESM",21);
-		smurf_struct->smurf_pic->pic_width = width;
-		smurf_struct->smurf_pic->pic_height = height;
-		smurf_struct->smurf_pic->depth = BitsPerPixel;
+		strcpy(smurf_struct->smurf_pic->format_name, "Enhanced Simplex");
 
-		strcpy(impmessag, "Enhanced Simplex ");
+		strcpy(impmessag, "ESM ");
 		strcat(impmessag, itoa(BitsPerPixel, dummy, 10));
 		strcat(impmessag, " Bit");
 		smurf_struct->services->reset_busybox(128, impmessag);
 
-
 		pal = smurf_struct->smurf_pic->palette;
 
-		if(BitsPerPixel == 1)
+		if (BitsPerPixel == 8)
+		{
+			if (DataOffset > 768)		/* Platz fÅr Palette? */
+			{
+				ppal = buffer + 0x24;
+
+				for (i = 0; i < 256; i++)
+				{
+					*pal++ = *ppal;
+					*pal++ = *(ppal + 256);
+					*pal++ = *(ppal++ + 512);
+				}
+			} else
+			{
+				for (i = 0; i < 256; i++)
+				{
+					*pal++ = i;
+					*pal++ = i;
+					*pal++ = i;
+				}
+			}
+			smurf_struct->smurf_pic->bp_pal = 24;
+		} else
 		{
 			pal[0] = 255;
 			pal[1] = 255;
@@ -132,59 +147,51 @@ short imp_module_main(GARGAMEL *smurf_struct)
 			pal[3] = 0;
 			pal[4] = 0;
 			pal[5] = 0;
+			smurf_struct->smurf_pic->bp_pal = 0;
 		}
-		else
-			if(BitsPerPixel == 8)
-				if(DataOffset > 768)			/* Platz fÅr Palette? */
-				{
-					ppal = buffer + 0x24;
-
-					for(i = 0; i < 256; i++)
-					{
-						*pal++ = *ppal;
-						*pal++ = *(ppal + 256);
-						*pal++ = *(ppal++ + 512);
-					}
-				}
-				else
-					for(i = 0; i < 256; i++)
-					{
-						*pal++ = (char)i;
-						*pal++ = (char)i;
-						*pal++ = (char)i;
-					}
-
 
 		ziel = buffer;
 
-		switch((int)BitsPerPixel)
+		switch (BitsPerPixel)
 		{
-			case 1: len = (long)(width + 7) / 8 * (long)height;
-					break;
-			case 8: len = (long)width * (long)height;
-					break;
-			case 24: len = (long)width * (long)height * 3L;
-					 break;
-			default: form_alert(0, "[1][unbekanntes ESM. Bild bitte an uns einschicken!][ OK ]");
-					 break;
+		case 1:
+			len = (unsigned long) ((width + 7) >> 3) * (unsigned long) height;
+			break;
+
+		case 8:
+			len = (unsigned long) width * (unsigned long) height;
+			break;
+
+		case 24:
+			len = (unsigned long) width * (unsigned long) height * 3L;
+			break;
+
+		default:
+			return M_UNKNOWN_TYPE;
 		}
 
-		memcpy(ziel, buffer + DataOffset, len);
+		if (len + DataOffset > smurf_struct->smurf_pic->file_len)
+			return M_INVALID;
+		memmove(ziel, buffer + DataOffset, len);
 
 		_Mshrink(ziel, len);
 
 		smurf_struct->smurf_pic->pic_data = ziel;
 
-		if(BitsPerPixel == 1)
+		smurf_struct->smurf_pic->pic_width = width;
+		smurf_struct->smurf_pic->pic_height = height;
+		smurf_struct->smurf_pic->depth = BitsPerPixel;
+
+		if (BitsPerPixel == 1)
 			smurf_struct->smurf_pic->format_type = FORM_STANDARD;
 		else
 			smurf_struct->smurf_pic->format_type = FORM_PIXELPAK;
 
-		if(BitsPerPixel == 8 && DataOffset < 768)				/* keine Palette -> Graustufenbild */
+		if (BitsPerPixel == 8 && DataOffset < 768)	/* keine Palette -> Graustufenbild */
 			smurf_struct->smurf_pic->col_format = GREY;
 		else
 			smurf_struct->smurf_pic->col_format = RGB;
-	} /* Erkennung */
+	}									/* Erkennung */
 
-	return(M_PICDONE);
+	return M_PICDONE;
 }
