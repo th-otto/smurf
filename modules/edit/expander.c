834634a3af99525072c648f295aa4581f638e5d9
Thorsten Otto <admin@tho-otto.de>
2022-11-29 15:40:39 +0100

    Cleanup edit/expander

--- modules/edit/expander.c
+++ modules/edit/expander.c
@@ -46,51 +46,51 @@
 
 static short (*busybox)(short pos);
 
-MOD_INFO module_info = {"Expander",
-						0x0030,
-						"Christian Eyrich",
-						"", "", "", "", "",
-						"", "", "", "", "",
-						"Slider 1",
-						"Slider 2",
-						"Slider 3",
-						"Slider 4",
-						"Checkbox 1",
-						"Checkbox 2",
-						"Checkbox 3",
-						"Checkbox 4",
-						"Edit 1",
-						"Edit 2",
-						"Edit 3",
-						"Edit 4",
-						0,64,
-						0,64,
-						0,64,
-						0,64,
-						0,10,
-						0,10,
-						0,10,
-						0,10,
-						0, 0, 0, 0,
-						0, 0, 0, 0,
-						0, 0, 0, 0,
-						1
-						};
+MOD_INFO module_info = {
+	"Expander",
+	0x0030,
+	"Christian Eyrich",
+	{ "", "", "", "", "", "", "", "", "", "" },
+	"Slider 1",
+	"Slider 2",
+	"Slider 3",
+	"Slider 4",
+	"Checkbox 1",
+	"Checkbox 2",
+	"Checkbox 3",
+	"Checkbox 4",
+	"Edit 1",
+	"Edit 2",
+	"Edit 3",
+	"Edit 4",
+	0, 64,
+	0, 64,
+	0, 64,
+	0, 64,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	1,
+	NULL, NULL, NULL, NULL, NULL, NULL
+};
 
 
 MOD_ABILITY module_ability = {
-						2, 4, 7, 8, 24,
-						0, 0, 0,
-						FORM_STANDARD,
-						FORM_STANDARD,
-						FORM_STANDARD,
-						FORM_BOTH,
-						FORM_PIXELPAK,
-						FORM_BOTH,
-						FORM_BOTH,
-						FORM_BOTH,
-						0,
-						};
+	2, 4, 7, 8, 24, 0, 0, 0,
+	FORM_STANDARD,
+	FORM_STANDARD,
+	FORM_STANDARD,
+	FORM_BOTH,
+	FORM_PIXELPAK,
+	FORM_BOTH,
+	FORM_BOTH,
+	FORM_BOTH,
+	0,
+};
 
 /* -------------------------------------------------*/
 /* -------------------------------------------------*/
@@ -102,264 +102,267 @@ MOD_ABILITY module_ability = {
 /* -------------------------------------------------*/
 void edit_module_main(GARGAMEL *smurf_struct)
 {
-	char *data, *odata, *palette, *pal, *normalize_map, *pixbuf, *line,
-		 BitsPerPixel, pixval, greyval, high, low;
+	uint8_t *data;
+	uint8_t *odata;
+	uint8_t *palette;
+	uint8_t *pal;
+	uint8_t *normalize_map;
+	uint8_t *pixbuf;
+	uint8_t *line;
+	uint8_t BitsPerPixel;
+	uint8_t pixval;
+	uint8_t greyval;
+	uint8_t high, low;
 
-	unsigned int x, y, width, height, bh, bl;
+	unsigned short x, y;
+	unsigned short width, height;
+	unsigned short bh, bl;
 
-	unsigned long *histogram,
-				  length, i, intensity, threshold_intensity;
-	unsigned long w, planelength;
+	unsigned long *histogram;
+	unsigned long length;
+	unsigned long i;
+	unsigned long intensity;
+	unsigned long threshold_intensity;
+	unsigned long w;
+	unsigned long planelength;
 
-
-/* Wenn das Modul zum ersten Mal gestartet wurde */
-	switch(smurf_struct->module_mode)
+	/* Wenn das Modul zum ersten Mal gestartet wurde */
+	switch (smurf_struct->module_mode)
 	{
-		case MSTART:
-			smurf_struct->module_mode = M_STARTED;
-			return;
+	case MSTART:
+		smurf_struct->module_mode = M_STARTED;
+		break;
 
-		case MEXEC:
+	case MEXEC:
 #if TIMER
-/* wie schnell sind wir? */
-	init_timer();
+		/* wie schnell sind wir? */
+		init_timer();
 #endif
-			busybox = smurf_struct->services->busybox;
+		busybox = smurf_struct->services->busybox;
 
-			BitsPerPixel = smurf_struct->smurf_pic->depth;
+		BitsPerPixel = smurf_struct->smurf_pic->depth;
 
-			width = smurf_struct->smurf_pic->pic_width;
-			height = smurf_struct->smurf_pic->pic_height;
+		width = smurf_struct->smurf_pic->pic_width;
+		height = smurf_struct->smurf_pic->pic_height;
 
-			histogram = (unsigned long *)calloc(256L * 4L, 1);
-			normalize_map = (char *)calloc(256L, 1);
+		histogram = (unsigned long *) Malloc(256 * sizeof(*histogram) + 256 * sizeof(*normalize_map));
 
-			if(histogram == 0 || normalize_map == 0)
+		if (histogram == NULL)
+		{
+			smurf_struct->module_mode = M_MEMORY;
+			return;
+		}
+		normalize_map = (uint8_t *)(histogram + 256);
+		memset(histogram, 0, 256 * sizeof(*histogram) + 256 * sizeof(*normalize_map));
+
+		if (BitsPerPixel != 16)
+		{
+			if (BitsPerPixel == 24)
 			{
-				free(normalize_map);
-				free(histogram);
-				smurf_struct->module_mode = M_MEMORY;
-				return;
-			}			
-	
-			if(BitsPerPixel != 16)
+				data = smurf_struct->smurf_pic->pic_data;
+				odata = data;
+			} else
 			{
-				if(BitsPerPixel == 24)
-				{
-					data = smurf_struct->smurf_pic->pic_data;
-					odata = data;
-				}
-				else
-				{
-					data = smurf_struct->smurf_pic->pic_data;
-					odata = data;
-					palette = smurf_struct->smurf_pic->palette;
-				}
+				data = smurf_struct->smurf_pic->pic_data;
+				odata = data;
+				palette = smurf_struct->smurf_pic->palette;
+			}
 
-				if((bh = height / 5) == 0)	 	/* busy-height */
-					bh = height;
-				bl = 0;							/* busy-length */
+			if ((bh = height / 5) == 0)	/* busy-height */
+				bh = height;
+			bl = 0;						/* busy-length */
 
-				/* Histogramm (Graustufen) erstellen */
-				if(BitsPerPixel == 24)
+			/* Histogramm (Graustufen) erstellen */
+			if (BitsPerPixel == 24)
+			{
+				y = 0;
+				do
 				{
+					if (!(y % bh))
+					{
+						busybox(bl);
+						bl += 15;
+					}
+
+					x = 0;
+					do
+					{
+						greyval = ((((long) *data++ * 871L) + ((long) *data++ * 2929L) + ((long) *data++ * 295L)) >> 12);
+
+						histogram[greyval]++;
+					} while (++x < width);
+				} while (++y < height);
+			} else
+			{
+				if (smurf_struct->smurf_pic->format_type == FORM_STANDARD)
+				{
+					pixbuf = (uint8_t *) Malloc(width + 7);
+
+					w = (unsigned long) ((width + 7) / 8);
+					planelength = w * (unsigned long) height;	/* LÑnge einer Plane in Bytes */
+
 					y = 0;
 					do
 					{
-						if(!(y%bh))
+						if (!(y % bh))
 						{
 							busybox(bl);
-							bl += 15;
+							bl += 25;
 						}
+
+						memset(pixbuf, 0, width);
+						getpix_std_line(data, pixbuf, BitsPerPixel, planelength, width);
+						data += w;
+						line = pixbuf;
 
 						x = 0;
 						do
 						{
-							greyval = (char)((((long)*data++ * 871L)
-											+ ((long)*data++ * 2929L)
-											+ ((long)*data++ * 295L)) >> 12);
+							pixval = *line++;
+							pal = palette + pixval + pixval + pixval;
+
+							greyval = ((((long) *pal++ * 871L) + ((long) *pal++ * 2929L) + ((long) *pal * 295L)) >> 12);
 
 							histogram[greyval]++;
-						} while(++x < width);
-					} while(++y < height);
-				}
-				else
+						} while (++x < width);
+					} while (++y < height);
+
+					Mfree(pixbuf);
+				} else
 				{
-					if(smurf_struct->smurf_pic->format_type == FORM_STANDARD)
-					{
-						pixbuf = (char *)Malloc(width + 7);
-
-						w = (unsigned long)((width + 7) / 8);
-						planelength = w * (unsigned long)height;   /* LÑnge einer Plane in Bytes */
-
-						y = 0;
-						do
-						{
-							if(!(y%bh))
-							{
-								busybox(bl);
-								bl += 25;
-							}
-
-							memset(pixbuf, 0x0, width);
-							getpix_std_line(data, pixbuf, BitsPerPixel, planelength, width);
-							data += w;
-							line = pixbuf;
-
-							x = 0;
-							do
-							{
-								pixval = *line++;
-								pal = palette + pixval + pixval + pixval;
-
-								greyval = (char)((((long)*pal++ * 871L)
-												+ ((long)*pal++ * 2929L)
-												+ ((long)*pal * 295L)) >> 12);
-
-								histogram[greyval]++;
-							} while(++x < width);
-						} while(++y < height);
-
-						Mfree(pixbuf);
-					}
-					else
-					{
-						y = 0;
-						do
-						{
-							if(!(y%bh))
-							{
-								busybox(bl);
-								bl += 25;
-							}
-
-							x = 0;
-							do
-							{
-								pixval = *data++;
-								pal = palette + pixval + pixval + pixval;
-
-								greyval = (char)((((long)*pal * 871L)
-												+ ((long)*(pal + 1) * 2929L)
-												+ ((long)*(pal + 2) * 295L)) >> 12);
-
-								histogram[greyval]++;
-							} while(++x < width);
-						} while(++y < height);
-					}
-				}
-
-				data = odata;
-
-				/* Histogrammgrenzen durch Suche nach 1 Prozent Schritt finden */
-				threshold_intensity = (unsigned int)(((long)width * (long)height) / 100);
-
-				intensity = 0;
-				for(low = 0; low < 255; low++)
-				{
-					intensity += histogram[low];
-
-					if(intensity > threshold_intensity)
-						break;
-				}
-
-				intensity = 0;
-				for(high = 255; high != 0; high--)
-				{
-					intensity += histogram[high];
-
-					if(intensity > threshold_intensity)
-						break;
-				}
-
-				if(low == high)
-				{
-					/* Fast kein Kontrast; 0-Grenze fÅr Grenzfindung benutzen. */
-					threshold_intensity = 0;
-					intensity = 0;
-					for(low = 0; low < 255; low++)
-					{
-						intensity += histogram[low];
-
-						if(intensity > threshold_intensity)
-							break;
-					}
-
-					intensity = 0;
-					for(high = 255; high != 0; high--)
-					{
-						intensity += histogram[high];
-
-						if(intensity > threshold_intensity)
-							break;
-					}
-
-					if(low == high)
-						return;  /* zero span bound */
-				}
-
-				/* Das Histogramm aufziehen um ein normalisiertes Mapping zu erstellen */
-				for(i = 0; i < 256; i++)
-					if(i < (int)low)
-						normalize_map[i] = 0;
-					else
-						if(i > (int)high)
-							normalize_map[i] = 255 - 1;
-						else
-							normalize_map[i] = (255 - 1) * (i - (int)low) / (int)(high - low);
-
-				/* Normalize */
-				if(BitsPerPixel == 24)
-				{
-					if((bh = height / 5) == 0)	 	/* busy-height */
-						bh = height;
-
 					y = 0;
 					do
 					{
-						if(!(y%bh))
+						if (!(y % bh))
 						{
 							busybox(bl);
-							bl += 10;
+							bl += 25;
 						}
 
 						x = 0;
 						do
 						{
-							*data++ = normalize_map[*data];
-							*data++ = normalize_map[*data];
-							*data++ = normalize_map[*data];
-						} while(++x < width);
-					} while(++y < height);
-				}
-				else
-				{
-					pal = palette;
-					length = 256L;
-					while(length--)
-					{
-						*pal++ = normalize_map[*pal];
-						*pal++ = normalize_map[*pal];
-						*pal++ = normalize_map[*pal];
-					}
+							pixval = *data++;
+							pal = palette + pixval + pixval + pixval;
+
+							greyval = ((((long) *pal * 871L) + ((long) *(pal + 1) * 2929L) + ((long) *(pal + 2) * 295L)) >> 12);
+
+							histogram[greyval]++;
+						} while (++x < width);
+					} while (++y < height);
 				}
 			}
 
-			free(histogram);
-			free(normalize_map);			
+			data = odata;
+
+			/* Histogrammgrenzen durch Suche nach 1 Prozent Schritt finden */
+			threshold_intensity = (((long) width * (long) height) / 100);
+
+			intensity = 0;
+			for (low = 0; low < 255; low++)
+			{
+				intensity += histogram[low];
+
+				if (intensity > threshold_intensity)
+					break;
+			}
+
+			intensity = 0;
+			for (high = 255; high != 0; high--)
+			{
+				intensity += histogram[high];
+
+				if (intensity > threshold_intensity)
+					break;
+			}
+
+			if (low == high)
+			{
+				/* Fast kein Kontrast; 0-Grenze fÅr Grenzfindung benutzen. */
+				threshold_intensity = 0;
+				intensity = 0;
+				for (low = 0; low < 255; low++)
+				{
+					intensity += histogram[low];
+
+					if (intensity > threshold_intensity)
+						break;
+				}
+
+				intensity = 0;
+				for (high = 255; high != 0; high--)
+				{
+					intensity += histogram[high];
+
+					if (intensity > threshold_intensity)
+						break;
+				}
+
+				if (low == high)
+					return;				/* zero span bound */
+			}
+
+			/* Das Histogramm aufziehen um ein normalisiertes Mapping zu erstellen */
+			for (i = 0; i < 256; i++)
+			{
+				if (i < low)
+					normalize_map[i] = 0;
+				else if (i > high)
+					normalize_map[i] = 255 - 1;
+				else
+					normalize_map[i] = (255 - 1) * (i - low) / (high - low);
+			}
+
+			/* Normalize */
+			if (BitsPerPixel == 24)
+			{
+				if ((bh = height / 5) == 0)	/* busy-height */
+					bh = height;
+
+				y = 0;
+				do
+				{
+					if (!(y % bh))
+					{
+						busybox(bl);
+						bl += 10;
+					}
+
+					x = 0;
+					do
+					{
+						*data++ = normalize_map[*data];
+						*data++ = normalize_map[*data];
+						*data++ = normalize_map[*data];
+					} while (++x < width);
+				} while (++y < height);
+			} else
+			{
+				pal = palette;
+				length = 256;
+				while (length--)
+				{
+					*pal++ = normalize_map[*pal];
+					*pal++ = normalize_map[*pal];
+					*pal++ = normalize_map[*pal];
+				}
+			}
+		}
+
+		Mfree(histogram);
 
 #if TIMER
-/* wie schnell waren wir? */
-	printf("%lu\n", get_timer());
-	getch();
+		/* wie schnell waren wir? */
+		printf("%lu\n", get_timer());
+		(void)Cnecin();
 #endif
 
-			smurf_struct->module_mode = M_DONEEXIT;
-			return;
+		smurf_struct->module_mode = M_DONEEXIT;
+		break;
 
 		/* Mterm empfangen - Speicher freigeben und beenden */
-		case MTERM:
-			smurf_struct->module_mode = M_EXIT;
-			return;
+	case MTERM:
+		smurf_struct->module_mode = M_EXIT;
+		break;
 	}
 }
