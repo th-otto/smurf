e86f4196215147722a07319b7f3a7d8454397ad0
Thorsten Otto <admin@tho-otto.de>
2022-11-29 16:18:03 +0100

    Cleanup edit/gamma

--- modules/bubble/readme.txt
+++ modules/bubble/readme.txt
@@ -3,20 +3,20 @@ and use the builtin preference dialog from smurf instead.
 For those, assignments to bubble IDs are (from the MOD_FORM dialog
 resource of smurf):
 
- 9 - Slider 1
-11 - Slider 1
-13 - Slider 1
-15 - Slider 1
+ 9/10/20 - Slider 1 (bar/slider/editfield)
+11/12/21 - Slider 2
+13/14/22 - Slider 3
+15/16/23 - Slider 4
 
 25 - Checkbox 1
 27 - Checkbox 2
 29 - Checkbox 3
 31 - Checkbox 4
 
-33 - Editfield 1
-35 - Editfield 2
-37 - Editfield 3
-39 - Editfield 4
+32/33 - Editfield 1 (title/editfield)
+34/35 - Editfield 2
+36/37 - Editfield 3
+38/39 - Editfield 4
 
 41 - Picture info
 42 - Load configuration
--- modules/edit/gamma.c
+++ modules/edit/gamma.c
@@ -52,51 +52,51 @@
 
 #define TIMER 0
 
-MOD_INFO module_info = {TEXT1,
-						0x0020,
-						"Christian Eyrich",
-						"", "", "", "", "",
-						"", "", "", "", "",
-						"Gamma R",
-						"Gamma G",
-						"Gamma B",
-						"",
-						"",
-						"",
-						"",
-						"",
-						"",
-						"",
-						"",
-						"",
-						1,40,
-						1,40,
-						1,40,
-						0,64,
-						0,10,
-						0,10,
-						0,10,
-						0,10,
-						10, 10, 10, 0,
-						0, 0, 0, 0,
-						0, 0, 0, 0,
-						1
-						};
+MOD_INFO module_info = {
+	TEXT1,
+	0x0020,
+	"Christian Eyrich",
+	{ "", "", "", "", "", "", "", "", "", "" },
+	"Gamma R",
+	"Gamma G",
+	"Gamma B",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	1, 40,
+	1, 40,
+	1, 40,
+	0, 64,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 10,
+	10, 10, 10, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	1,
+	NULL, NULL, NULL, NULL, NULL, NULL
+};
 
 
 MOD_ABILITY module_ability = {
-						4, 7, 8, 16, 24,
-						0, 0, 0,
-						FORM_STANDARD,
-						FORM_STANDARD,
-						FORM_BOTH,
-						FORM_PIXELPAK,
-						FORM_PIXELPAK,
-						FORM_BOTH,
-						FORM_BOTH,
-						FORM_BOTH,
-						0,
-						};
+	4, 7, 8, 16, 24, 0, 0, 0,
+	FORM_STANDARD,
+	FORM_STANDARD,
+	FORM_BOTH,
+	FORM_PIXELPAK,
+	FORM_PIXELPAK,
+	FORM_BOTH,
+	FORM_BOTH,
+	FORM_BOTH,
+	0,
+};
 
 /* -------------------------------------------------*/
 /* -------------------------------------------------*/
@@ -104,110 +104,101 @@ MOD_ABILITY module_ability = {
 /*		4-8, 16 und 24 Bit 							*/
 /* -------------------------------------------------*/
 /* -------------------------------------------------*/
-void edit_module_main(GARGAMEL *smurf_struct)
+void edit_module_main(GARGAMEL * smurf_struct)
 {
-	char *data,
-		 r, g, b, BitsPerPixel;
-	
-	int module_id;
-	unsigned int *data16, 
-				 width, height, i;
-	unsigned int redgammamap[256], grngammamap[256], blugammamap[256];
-	
+	uint8_t *data;
+	uint8_t r, g, b;
+	uint8_t BitsPerPixel;
+
+	uint16_t *data16;
+	unsigned short width, height, i;
+	unsigned short redgammamap[256];
+	unsigned short grngammamap[256];
+	unsigned short blugammamap[256];
 	unsigned long length;
+	double gammared, gammagrn, gammablu;
 
-	double gammared, gammagrn, gammablu;	
 
-
-	module_id = smurf_struct->module_number;
-
-/* Wenn das Modul zum ersten Mal gestartet wurde, */
-/* ein Einstellformular anfordern....             */
-	if(smurf_struct->module_mode == MSTART)
+	switch (smurf_struct->module_mode)
 	{
-		smurf_struct->services->f_module_prefs(&module_info, module_id);
+	/* Wenn das Modul zum ersten Mal gestartet wurde, */
+	/* ein Einstellformular anfordern....             */
+	case MSTART:
+		smurf_struct->services->f_module_prefs(&module_info, smurf_struct->module_number);
 		smurf_struct->module_mode = M_WAITING;
-		return;
-	}
-/* Einstellformular wurde mit START verlassen - Funktion ausfhren */
-	else
-		if(smurf_struct->module_mode == MEXEC)
+		break;
+
+	case MEXEC:
+	/* Einstellformular wurde mit START verlassen - Funktion ausfhren */
+#if TIMER
+		/* wie schnell sind wir? */
+		init_timer();
+#endif
+
+		gammared = smurf_struct->slide1 / 10.0;
+		gammagrn = smurf_struct->slide2 / 10.0;
+		gammablu = smurf_struct->slide3 / 10.0;
+
+		BitsPerPixel = smurf_struct->smurf_pic->depth;
+
+		width = smurf_struct->smurf_pic->pic_width;
+		height = smurf_struct->smurf_pic->pic_height;
+
+		for (i = 0; i < 256; i++)
 		{
-#if TIMER
-/* wie schnell sind wir? */
-	init_timer();
-#endif
-
-			gammared = (int)smurf_struct->slide1 / 10.0;
-			gammagrn = (int)smurf_struct->slide2 / 10.0;
-			gammablu = (int)smurf_struct->slide3 / 10.0;
-
-			BitsPerPixel = smurf_struct->smurf_pic->depth;
-
-			width = smurf_struct->smurf_pic->pic_width;
-			height = smurf_struct->smurf_pic->pic_height;
-
-			for( i = 0; i < 256; i++ )
-			{
-		    	redgammamap[i] = (unsigned int)(0.5 + 255 * pow(i / 255.0, 1.0 / gammared));
-		    	grngammamap[i] = (unsigned int)(0.5 + 255 * pow(i / 255.0, 1.0 / gammagrn));
-		    	blugammamap[i] = (unsigned int)(0.5 + 255 * pow(i / 255.0, 1.0 / gammablu));
-		    }
-		
-			if(BitsPerPixel != 16)
-			{
-				if(BitsPerPixel == 24)
-				{
-					data = smurf_struct->smurf_pic->pic_data;
-
-					length = (unsigned long)width * (unsigned long)height;
-				}
-				else
-				{
-					data = smurf_struct->smurf_pic->palette;
-
-					length = 256L;
-				}
-
-				while(length--)
-				{
-					*data++ = redgammamap[*data];
-					*data++ = grngammamap[*data];
-					*data++ = blugammamap[*data];
-				}
-			}
-			else
-				if(BitsPerPixel == 16)
-				{
-					data16 = smurf_struct->smurf_pic->pic_data;
-
-					length = (unsigned long)width * (unsigned long)height;
-	
-					do
-					{
-						r = redgammamap[(*data16 & 0xf800) >> 8];
-						g = grngammamap[(*data16 & 0x7e0) >> 3];
-						b = blugammamap[(*data16 & 0x1f) << 3];
-
-						*data16++ = ((r & 0xf8) << 8) | ((g & 0xfc) << 3) | (b >> 3);
-					} while(--length);
-
-				}
-	
-#if TIMER
-/* wie schnell waren wir? */
-	printf("%lu\n", get_timer());
-	getch();
-#endif
-
-			smurf_struct->module_mode = M_PICDONE;
-			return;
+			redgammamap[i] = 0.5 + 255 * pow(i / 255.0, 1.0 / gammared);
+			grngammamap[i] = 0.5 + 255 * pow(i / 255.0, 1.0 / gammagrn);
+			blugammamap[i] = 0.5 + 255 * pow(i / 255.0, 1.0 / gammablu);
 		}
-		/* Mterm empfangen - Speicher freigeben und beenden */
-		else 
-			if(smurf_struct->module_mode == MTERM)
+
+		if (BitsPerPixel != 16)
+		{
+			if (BitsPerPixel == 24)
 			{
-				smurf_struct->module_mode = M_EXIT;
-				return;
+				data = smurf_struct->smurf_pic->pic_data;
+
+				length = (unsigned long) width * (unsigned long) height;
+			} else
+			{
+				data = smurf_struct->smurf_pic->palette;
+
+				length = 256;
 			}
+
+			while (length--)
+			{
+				*data++ = redgammamap[*data];
+				*data++ = grngammamap[*data];
+				*data++ = blugammamap[*data];
+			}
+		} else
+		{
+			data16 = smurf_struct->smurf_pic->pic_data;
+
+			length = (unsigned long) width * (unsigned long) height;
+
+			do
+			{
+				r = redgammamap[(*data16 & 0xf800) >> 8];
+				g = grngammamap[(*data16 & 0x7e0) >> 3];
+				b = blugammamap[(*data16 & 0x1f) << 3];
+
+				*data16++ = ((r & 0xf8) << 8) | ((g & 0xfc) << 3) | (b >> 3);
+			} while (--length);
+
+		}
+#if TIMER
+		/* wie schnell waren wir? */
+		printf("%lu\n", get_timer());
+		(void)Cnecin();
+#endif
+
+		smurf_struct->module_mode = M_PICDONE;
+		break;
+
+	case MTERM:
+	/* Mterm empfangen - Speicher freigeben und beenden */
+		smurf_struct->module_mode = M_EXIT;
+		break;
+	}
 }
