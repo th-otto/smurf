7105fabd9618f5d098e908ae4487e0f7b2824f7b
Thorsten Otto <admin@tho-otto.de>
2022-11-30 07:25:19 +0100

    Cleanup edit/scatman

--- modules/edit/scatman.c
+++ modules/edit/scatman.c
@@ -62,58 +62,59 @@
 #define TIMER 0
 
 #undef random
-#define random( x ) (rand() % (x))
+#define random(x) (rand() % (x))
 
 static void *(*SMalloc)(long amount);
 static void (*SMfree)(void *ptr);
 
 short (*busybox)(short pos);
 
-MOD_INFO module_info = {"Scatman's World",
-						0x0030,
-						"Christian Eyrich",
-						"", "", "", "", "",
-						"", "", "", "", "",
-						"",
-						"",
-						"",
-						"",
-						"Clean",
-						"",
-						"",
-						"",
-						TEXT1,
-						TEXT2,
-						"",
-						"",
-						0,64,
-						0,64,
-						0,64,
-						0,64,
-						2,16,
-						2,16,
-						0,10,
-						0,10,
-						0, 0, 0, 0,
-						1, 0, 0, 0,
-						3, 3, 0, 0,
-						1
-						};
+MOD_INFO module_info = {
+	"Scatman's World",
+	0x0030,
+	"Christian Eyrich",
+	{ "", "", "", "", "", "", "", "", "", "" },
+	"",
+	"",
+	"",
+	"",
+	"Clean",
+	"",
+	"",
+	"",
+	TEXT1,
+	TEXT2,
+	"",
+	"",
+	0, 64,
+	0, 64,
+	0, 64,
+	0, 64,
+	2, 16,
+	2, 16,
+	0, 10,
+	0, 10,
+	0, 0, 0, 0,
+	1, 0, 0, 0,
+	3, 3, 0, 0,
+	1,
+	NULL, NULL, NULL, NULL, NULL, NULL
+};
 
 
 MOD_ABILITY module_ability = {
-						8, 16, 24, 0, 0,
-						0, 0, 0,
-						FORM_PIXELPAK,
-						FORM_PIXELPAK,
-						FORM_PIXELPAK,
-						FORM_BOTH,
-						FORM_BOTH,
-						FORM_BOTH,
-						FORM_BOTH,
-						FORM_BOTH,
-						0,
-						};
+	8, 16, 24, 0, 0,
+	0, 0, 0,
+	FORM_PIXELPAK,
+	FORM_PIXELPAK,
+	FORM_PIXELPAK,
+	FORM_BOTH,
+	FORM_BOTH,
+	FORM_BOTH,
+	FORM_BOTH,
+	FORM_BOTH,
+	0,
+};
 
 /* -------------------------------------------------*/
 /* -------------------------------------------------*/
@@ -134,157 +135,159 @@ MOD_ABILITY module_ability = {
 /* -------------------------------------------------*/
 void edit_module_main(GARGAMEL *smurf_struct)
 {
-	char *ziel, *buffer, *obuffer,
-		 clean, BitsPerPixel, pixlen;
+	uint8_t *ziel;
+	uint8_t *buffer;
+	uint8_t *obuffer;
+	uint8_t clean;
+	uint8_t BitsPerPixel;
+	uint8_t pixlen;
 
-	int module_id, randwidth, randheight, randwidth2, randheight2;
-	unsigned int width, height, bh, bl, i;
+	short randwidth, randheight, randwidth2, randheight2;
+	unsigned short width, height;
+	unsigned short bh, bl;
+	unsigned short i;
 
-	long Scatadubbelidau, scatland[512], realwidth, length, lengthtorun;
+	long Scatadubbelidau;
+	long scatland[512];
+	long realwidth;
+	long length;
+	long lengthtorun;
 
 
-	module_id = smurf_struct->module_number;
-
-/* Wenn das Modul zum ersten Mal gestartet wurde, */
-/* ein Einstellformular anfordern....             */
-	if(smurf_struct->module_mode == MSTART)
+	switch (smurf_struct->module_mode)
 	{
-		smurf_struct->services->f_module_prefs(&module_info, module_id);
+	case MSTART:
+		/* Wenn das Modul zum ersten Mal gestartet wurde, */
+		/* ein Einstellformular anfordern....             */
+		smurf_struct->services->f_module_prefs(&module_info, smurf_struct->module_number);
 		smurf_struct->module_mode = M_WAITING;
-		return;
-	}
+		break;
 
-/* Einstellformular wurde mit START verlassen - Funktion ausfhren */
-	else
-		if(smurf_struct->module_mode == MEXEC)
+	case MEXEC:
+		/* Einstellformular wurde mit START verlassen - Funktion ausfhren */
+		SMalloc = smurf_struct->services->SMalloc;
+		SMfree = smurf_struct->services->SMfree;
+
+		busybox = smurf_struct->services->busybox;
+
+		smurf_struct->services->reset_busybox(0, "scatten ...");
+#if TIMER
+		/* wie schnell sind wir? */
+		init_timer();
+#endif
+
+		/* Zufallsgenerator zuf„llig initialisieren */
+		srand((unsigned int) Random());
+
+		clean = smurf_struct->check1;
+		randwidth = (short) smurf_struct->edit1;
+		randwidth2 = randwidth / 2;
+		randheight = (short) smurf_struct->edit2;
+		randheight2 = randheight / 2;
+
+		BitsPerPixel = smurf_struct->smurf_pic->depth;
+
+		width = smurf_struct->smurf_pic->pic_width;
+		height = smurf_struct->smurf_pic->pic_height;
+
+		ziel = smurf_struct->smurf_pic->pic_data;
+
+		if (clean)
 		{
-			SMalloc = smurf_struct->services->SMalloc;
-			SMfree = smurf_struct->services->SMfree;
-
-			busybox = smurf_struct->services->busybox;
-
-			smurf_struct->services->reset_busybox(0, "scatten ...");
-#if TIMER
-/* wie schnell sind wir? */
-	init_timer();
-#endif
-
-			/* Zufallsgenerator zuf„llig initialisieren */
-			srand((unsigned int)time(NULL));
-
-			clean = smurf_struct->check1;
-			randwidth = (int)smurf_struct->edit1;
-			randwidth2 = randwidth / 2;
-			randheight = (int)smurf_struct->edit2;
-			randheight2 = randheight / 2;
-
-			BitsPerPixel = smurf_struct->smurf_pic->depth;
-
-			width = smurf_struct->smurf_pic->pic_width;
-			height = smurf_struct->smurf_pic->pic_height;
-
-			ziel = smurf_struct->smurf_pic->pic_data;
-
-			if(clean)
+			if ((buffer = SMalloc(((unsigned long) width * (unsigned long) height * BitsPerPixel) >> 3)) == 0)
 			{
-				if((buffer = SMalloc(((unsigned long)width * (unsigned long)height * BitsPerPixel) >> 3)) == 0)
-				{
-					smurf_struct->module_mode = M_PICDONE;
-					return;
-				}
-				memcpy(buffer, ziel, ((unsigned long)width * (unsigned long)height * BitsPerPixel) >> 3);
-			}
-			else
-				buffer = ziel;
-	
-			obuffer = buffer;
-
-			pixlen = BitsPerPixel >> 3;
-	
-			realwidth = (unsigned long)width * (unsigned long)pixlen;
-	
-			length = (unsigned long)width * (unsigned long)height;
-
-			if((bh = (int)(length / 20)) == 0)		/* busy-height */
-				bh = (int)length;
-			bl = 0;							/* busy-length */
-
-			i = 0;
-			do
-			{
-				scatland[i] = (long)(random(randheight) - randheight2) * realwidth + (long)(random(randwidth) - randwidth2) * pixlen;
-			} while(++i < 512);
-
-
-			lengthtorun = length;
-
-			if(BitsPerPixel == 24)
-			{
-				do
-				{
-					if(!(lengthtorun%bh))
-					{
-						busybox(bl);
-						bl += 6;
-					}
-	
-					Scatadubbelidau = scatland[random(512)];
-					if(Scatadubbelidau < lengthtorun && Scatadubbelidau > (lengthtorun - length))
-					{
-						*ziel++ = ((unsigned int)*(buffer + Scatadubbelidau) + (unsigned int)*ziel) >> 1;
-						*ziel++ = ((unsigned int)*(buffer + 1 + Scatadubbelidau) + (unsigned int)*ziel) >> 1;
-						*ziel++ = ((unsigned int)*(buffer + 2 + Scatadubbelidau) + (unsigned int)*ziel) >> 1;
-					}
-					else
-						ziel += pixlen;
-	
-					buffer += pixlen;
-				} while(--lengthtorun);
-			}
-			else
-			{
-				do
-				{
-					if(!(lengthtorun%bh))
-					{
-						busybox(bl);
-						bl += 6;
-					}
-	
-					Scatadubbelidau = scatland[random(512)];
-					if(Scatadubbelidau < lengthtorun && Scatadubbelidau > (lengthtorun - length))
-					{
-						*ziel++ = *(buffer + Scatadubbelidau);
-
-						if(BitsPerPixel == 16)
-							*ziel++ = *(buffer + 1 + Scatadubbelidau);
-					}
-					else
-						ziel += pixlen;
-	
-					buffer += pixlen;
-				} while(--lengthtorun);
-			}
-	
-			buffer = obuffer;
-			if(clean)
-				SMfree(buffer);
-
-#if TIMER
-/* wie schnell waren wir? */
-	printf("%lu\n", get_timer());
-	getch();
-#endif
-
-			smurf_struct->module_mode = M_PICDONE;
-			return;
-		}
-
-		/* Mterm empfangen - Speicher freigeben und beenden */
-		else 
-			if(smurf_struct->module_mode == MTERM)
-			{
-				smurf_struct->module_mode = M_EXIT;
+				smurf_struct->module_mode = M_PICDONE;
 				return;
 			}
+			memcpy(buffer, ziel, ((unsigned long) width * (unsigned long) height * BitsPerPixel) >> 3);
+		} else
+			buffer = ziel;
+
+		obuffer = buffer;
+
+		pixlen = BitsPerPixel >> 3;
+
+		realwidth = (unsigned long) width * (unsigned long) pixlen;
+
+		length = (unsigned long) width * (unsigned long) height;
+
+		if ((bh = (short) (length / 20)) == 0)	/* busy-height */
+			bh = (short) length;
+		bl = 0;							/* busy-length */
+
+		i = 0;
+		do
+		{
+			scatland[i] =
+				(long) (random(randheight) - randheight2) * realwidth + (long) (random(randwidth) -
+																				randwidth2) * pixlen;
+		} while (++i < 512);
+
+
+		lengthtorun = length;
+
+		if (BitsPerPixel == 24)
+		{
+			do
+			{
+				if (!(lengthtorun % bh))
+				{
+					busybox(bl);
+					bl += 6;
+				}
+
+				Scatadubbelidau = scatland[random(512)];
+				if (Scatadubbelidau < lengthtorun && Scatadubbelidau > (lengthtorun - length))
+				{
+					*ziel++ = (*(buffer + 0 + Scatadubbelidau) + *ziel) >> 1;
+					*ziel++ = (*(buffer + 1 + Scatadubbelidau) + *ziel) >> 1;
+					*ziel++ = (*(buffer + 2 + Scatadubbelidau) + *ziel) >> 1;
+				} else
+				{
+					ziel += pixlen;
+				}
+				buffer += pixlen;
+			} while (--lengthtorun);
+		} else
+		{
+			do
+			{
+				if (!(lengthtorun % bh))
+				{
+					busybox(bl);
+					bl += 6;
+				}
+
+				Scatadubbelidau = scatland[random(512)];
+				if (Scatadubbelidau < lengthtorun && Scatadubbelidau > (lengthtorun - length))
+				{
+					*ziel++ = *(buffer + Scatadubbelidau);
+
+					if (BitsPerPixel == 16)
+						*ziel++ = *(buffer + 1 + Scatadubbelidau);
+				} else
+					ziel += pixlen;
+
+				buffer += pixlen;
+			} while (--lengthtorun);
+		}
+
+		buffer = obuffer;
+		if (clean)
+			SMfree(buffer);
+
+#if TIMER
+		/* wie schnell waren wir? */
+		printf("%lu\n", get_timer());
+		(void)Cnecin();
+#endif
+
+		smurf_struct->module_mode = M_PICDONE;
+		break;
+
+	case MTERM:
+		/* Mterm empfangen - Speicher freigeben und beenden */
+		smurf_struct->module_mode = M_EXIT;
+		break;
+	}
 }
