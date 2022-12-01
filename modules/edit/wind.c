78ac570283fb3db01ae8c475deb2a63bd030696b
Thorsten Otto <admin@tho-otto.de>
2022-12-01 06:07:14 +0100

    Cleanup edit/wind

--- modules/edit/wind.c
+++ modules/edit/wind.c
@@ -70,56 +70,316 @@
 #define LINKS	1
 #define RECHTS	2
 
-int wind_left(char *data, unsigned int width, unsigned int height);
-int wind_right(char *data, unsigned int width, unsigned int height, char BitsPerPixel);
-int storm_left(char *data, unsigned int width, unsigned int height);
-int storm_right(char *data, unsigned int width, unsigned int height, char BitsPerPixel);
-
-MOD_INFO module_info = {"Wind",
-						0x0020,
-						"Christian Eyrich",
-						"", "", "", "", "",
-						"", "", "", "", "",
-						"",
-						"",
-						"",
-						"",
-						TEXT1,
-						TEXT2,
-						TEXT3,
-						TEXT4,
-						"",
-						"",
-						"",
-						"",
-						0,64,
-						0,64,
-						0,64,
-						0,64,
-						0,10,
-						0,10,
-						0,10,
-						0,10,
-						0, 0, 0, 0,
-						2, 3, 3, 3,
-						0, 0, 0, 0,
-						1
-						};
+MOD_INFO module_info = {
+	"Wind",
+	0x0020,
+	"Christian Eyrich",
+	{ "", "", "", "", "", "", "", "", "", "" },
+	"",
+	"",
+	"",
+	"",
+	TEXT1,
+	TEXT2,
+	TEXT3,
+	TEXT4,
+	"",
+	"",
+	"",
+	"",
+	0, 64,
+	0, 64,
+	0, 64,
+	0, 64,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 0, 0, 0,
+	2, 3, 3, 3,
+	0, 0, 0, 0,
+	1,
+	NULL, NULL, NULL, NULL, NULL, NULL
+};
 
 
 MOD_ABILITY module_ability = {
-						24, 0, 0, 0, 0,
-						0, 0, 0,
-						FORM_PIXELPAK,
-						FORM_BOTH,
-						FORM_BOTH,
-						FORM_BOTH,
-						FORM_BOTH,
-						FORM_BOTH,
-						FORM_BOTH,
-						FORM_BOTH,
-						0,
-						};
+	24, 0, 0, 0, 0,	0, 0, 0,
+	FORM_PIXELPAK,
+	FORM_BOTH,
+	FORM_BOTH,
+	FORM_BOTH,
+	FORM_BOTH,
+	FORM_BOTH,
+	FORM_BOTH,
+	FORM_BOTH,
+	0,
+};
+
+
+
+
+/* bl„st von links sanft durchs Bild */
+static void wind_left(uint8_t *data, unsigned short width, unsigned short height)
+{
+	uint8_t r, g, b;
+	uint8_t init_pix, this_pix;
+	short intens;
+	unsigned short x, y, daempfung;
+
+	y = height;
+	while (y--)
+	{
+		init_pix = (uint8_t) ((((long) *data * 872L) + ((long) *(data + 1) * 2930L) + ((long) *(data + 2) * 296L)) >> 12);
+
+		r = *data++;
+		g = *data++;
+		b = *data++;
+
+		/* Wert fr ersten Start initialisieren */
+		intens = 95;
+		daempfung = (random(45) >> 1) + 3;
+
+		this_pix = (uint8_t) ((((long) *data * 872L) + ((long) *(data + 1) * 2930L) + ((long) *(data + 2) * 296L)) >> 12);
+
+		x = width - 1;
+		while (x--)
+		{
+			while (init_pix > this_pix && intens > 0 && x)
+			{
+				*data++ = ((r * intens + *data * (128 - intens)) >> 7);
+				*data++ = ((g * intens + *data * (128 - intens)) >> 7);
+				*data++ = ((b * intens + *data * (128 - intens)) >> 7);
+				x--;
+
+				intens -= daempfung;
+
+				this_pix = (uint8_t) ((((long) *data * 872L) + ((long) *(data + 1) * 2930L) + ((long) *(data + 2) * 296L)) >> 12);
+			}
+
+			data -= 3;
+			x++;
+
+			do
+			{
+				data += 3;
+				x--;
+
+				init_pix = this_pix;
+
+				this_pix = (uint8_t) ((((long) *(data + 3) * 872L) + ((long) *(data + 4) * 2930L) + ((long) *(data + 5) * 296L)) >> 12);
+			} while (init_pix <= this_pix && x);
+
+			r = *data++;
+			g = *data++;
+			b = *data++;
+
+			/* Wert fr neuen Start initialisieren */
+			intens = 95;
+			daempfung = (random(45) >> 1) + 3;
+		}
+	}
+}
+
+
+/* bl„st von rechts sanft durchs Bild */
+static void wind_right(uint8_t *data, unsigned short width, unsigned short height)
+{
+	char r, g, b;
+	uint8_t init_pix, this_pix;
+	short intens;
+	unsigned short x, y, daempfung;
+
+	/* auf Blaukomponente des letzten Bildpixels setzen */
+	data += (long) width * (long) height * 3L - 1;
+
+	y = height;
+	while (y--)
+	{
+		init_pix = (uint8_t) ((((long) *(data - 2) * 872L) + ((long) *(data - 1) * 2930L) + ((long) *data * 296L)) >> 12);
+
+		b = *data--;
+		g = *data--;
+		r = *data--;
+
+		/* Wert fr ersten Start initialisieren */
+		intens = 95;
+		daempfung = (random(45) >> 1) + 3;
+
+		this_pix = (uint8_t) ((((long) *(data - 2) * 871L) + ((long) *(data - 1) * 2929L) + ((long) *data * 295L)) >> 12);
+
+		x = width - 1;
+		while (x--)
+		{
+			intens -= daempfung;
+
+			while (init_pix > this_pix && intens > 0 && x)
+			{
+				*data-- = ((b * intens + *data * (128 - intens)) >> 7);
+				*data-- = ((g * intens + *data * (128 - intens)) >> 7);
+				*data-- = ((r * intens + *data * (128 - intens)) >> 7);
+				x--;
+
+				intens -= daempfung;
+
+				this_pix = (uint8_t) ((((long) *(data - 2) * 872L) + ((long) *(data - 1) * 2930L) + ((long) *data * 296L)) >> 12);
+			}
+
+			data += 3;
+			x++;
+
+			do
+			{
+				data -= 3;
+				x--;
+
+				init_pix = this_pix;
+
+				this_pix = (uint8_t) ((((long) *(data - 5) * 872L) + ((long) *(data - 4) * 2930L) + ((long) *(data - 3) * 296L)) >> 12);
+			} while (init_pix <= this_pix && x);
+
+			b = *data--;
+			g = *data--;
+			r = *data--;
+
+			/* Wert fr neuen Start initialisieren */
+			intens = 95;
+			daempfung = (random(45) >> 1) + 3;
+		}
+	}
+}
+
+
+/* bl„st von links kr„ftig durchs Bild */
+static void storm_left(uint8_t *data, unsigned short width, unsigned short height)
+{
+	uint8_t r, g, b;
+	uint8_t init_pix, this_pix;
+	short intens, intens2;
+	unsigned short x, y, daempfung;
+
+	y = height;
+	while (y--)
+	{
+		init_pix = (uint8_t) ((((long) *data * 872L) + ((long) *(data + 1) * 2930L) + ((long) *(data + 2) * 296L)) >> 12);
+
+		r = *data++;
+		g = *data++;
+		b = *data++;
+
+		/* Wert fr ersten Start initialisieren */
+		intens2 = intens = 105;
+		daempfung = (random(45) >> 1) + 3;
+
+		this_pix = (uint8_t) ((((long) *data * 872L) + ((long) *(data + 1) * 2930L) + ((long) *(data + 2) * 296L)) >> 12);
+
+		x = width - 1;
+		while (x--)
+		{
+			while (init_pix > this_pix && intens > 0 && x)
+			{
+				*data++ = ((r * intens2 + *data * (128 - intens2)) >> 7);
+				*data++ = ((g * intens2 + *data * (128 - intens2)) >> 7);
+				*data++ = ((b * intens2 + *data * (128 - intens2)) >> 7);
+				x--;
+
+				intens -= daempfung;
+
+				this_pix = (uint8_t) ((((long) *data * 872L) + ((long) *(data + 1) * 2930L) + ((long) *(data + 2) * 296L)) >> 12);
+			}
+
+			data -= 3;
+			x++;
+
+			do
+			{
+				data += 3;
+				x--;
+
+				init_pix = this_pix;
+
+				this_pix = (uint8_t) ((((long) *(data + 3) * 872L) + ((long) *(data + 4) * 2930L) + ((long) *(data + 5) * 296L)) >> 12);
+			} while (init_pix <= this_pix && x);
+
+			r = *data++;
+			g = *data++;
+			b = *data++;
+
+			/* Wert fr neuen Start initialisieren */
+			intens2 = intens = 105;
+			daempfung = (random(45) >> 1) + 3;
+		}
+	}
+}
+
+
+/* bl„st von rechts kr„ftig durchs Bild */
+static void storm_right(uint8_t *data, unsigned short width, unsigned short height)
+{
+	uint8_t r, g, b;
+	uint8_t init_pix, this_pix;
+	short intens, intens2;
+	unsigned short x, y, daempfung;
+
+	/* auf Blaukomponente des letzten Bildpixels setzen */
+	data += (long) width * (long) height * 3L - 1;
+
+	y = height;
+	while (y--)
+	{
+		init_pix = (uint8_t) ((((long) *(data - 2) * 872L) + ((long) *(data - 1) * 2930L) + ((long) *data * 296L)) >> 12);
+
+		b = *data--;
+		g = *data--;
+		r = *data--;
+
+		/* Wert fr ersten Start initialisieren */
+		intens2 = intens = 105;
+		daempfung = (random(45) >> 1) + 3;
+
+		this_pix = (uint8_t) ((((long) *(data - 2) * 872L) + ((long) *(data - 1) * 2930L) + ((long) *data * 296L)) >> 12);
+
+		x = width - 1;
+		while (x--)
+		{
+			intens -= daempfung;
+
+			while (init_pix > this_pix && intens > 0 && x)
+			{
+				*data-- = ((b * intens2 + *data * (128 - intens2)) >> 7);
+				*data-- = ((g * intens2 + *data * (128 - intens2)) >> 7);
+				*data-- = ((r * intens2 + *data * (128 - intens2)) >> 7);
+				x--;
+
+				intens -= daempfung;
+
+				this_pix = (uint8_t) ((((long) *(data - 2) * 872L) + ((long) *(data - 1) * 2930L) + ((long) *data * 296L)) >> 12);
+			}
+
+			data += 3;
+			x++;
+
+			do
+			{
+				data -= 3;
+				x--;
+
+				init_pix = this_pix;
+
+				this_pix = (uint8_t) ((((long) *(data - 5) * 872L) + ((long) *(data - 4) * 2930L) + ((long) *(data - 3) * 296L)) >> 12);
+			} while (init_pix <= this_pix && x);
+
+			b = *data--;
+			g = *data--;
+			r = *data--;
+
+			/* Wert fr neuen Start initialisieren */
+			intens2 = intens = 105;
+			daempfung = (random(45) >> 1) + 3;
+		}
+	}
+}
+
 
 /* -------------------------------------------------*/
 /* -------------------------------------------------*/
@@ -130,376 +390,55 @@ MOD_ABILITY module_ability = {
 /* -------------------------------------------------*/
 void edit_module_main(GARGAMEL *smurf_struct)
 {
-	char *data,
-		 BitsPerPixel;
-	
-	int module_id;
-	unsigned int width, height;
+	uint8_t *data;
+	uint8_t BitsPerPixel;
+	unsigned short width, height;
 
-	
-	module_id = smurf_struct->module_number;
-
+	switch (smurf_struct->module_mode)
+	{
 	/* Wenn das Modul zum ersten Mal gestartet wurde, */
 	/* ein Einstellformular anfordern....             */
-	if(smurf_struct->module_mode == MSTART)
-	{
-		smurf_struct->services->f_module_prefs(&module_info, module_id);
+	case MSTART:
+		smurf_struct->services->f_module_prefs(&module_info, smurf_struct->module_number);
 		smurf_struct->module_mode = M_WAITING;
-		return;
-	}
+		break;
+
 	/* Einstellformular wurde mit START verlassen - Funktion ausfhren */
-	else
-		if(smurf_struct->module_mode == MEXEC)
-		{
-			data = smurf_struct->smurf_pic->pic_data;
+	case MEXEC:
+		data = smurf_struct->smurf_pic->pic_data;
 
-			BitsPerPixel = smurf_struct->smurf_pic->depth;
-
-			width = smurf_struct->smurf_pic->pic_width;
-			height = smurf_struct->smurf_pic->pic_height;
-
+		width = smurf_struct->smurf_pic->pic_width;
+		height = smurf_struct->smurf_pic->pic_height;
 
 #if TIMER
-/* wie schnell sind wir? */
-	init_timer();
+		/* wie schnell sind wir? */
+		init_timer();
 #endif
 
-			/* Zufallsgenerator zuf„llig initialisieren */
-			srand((unsigned int)time(NULL));
+		/* Zufallsgenerator zuf„llig initialisieren */
+		srand((unsigned int) Random());
 
-			if(smurf_struct->check1)
-				wind_left(data, width, height);
-			else
-				if(smurf_struct->check2)
-					wind_right(data, width, height, BitsPerPixel);
-				else
-					if(smurf_struct->check3)
-						storm_left(data, width, height);
-					else
-						storm_right(data, width, height, BitsPerPixel);
+		if (smurf_struct->check1)
+			wind_left(data, width, height);
+		else if (smurf_struct->check2)
+			wind_right(data, width, height);
+		else if (smurf_struct->check3)
+			storm_left(data, width, height);
+		else
+			storm_right(data, width, height);
 
 #if TIMER
-/* wie schnell waren wir? */
-	printf("%lu\n", get_timer());
-	getch();
+		/* wie schnell waren wir? */
+		printf("%lu\n", get_timer());
+		(void)Cnecin();
 #endif
 
-			smurf_struct->module_mode = M_PICDONE;
-			return;
-		}
-		/* Mterm empfangen - Speicher freigeben und beenden */
-		else 
-			if(smurf_struct->module_mode == MTERM)
-			{
-				smurf_struct->module_mode = M_EXIT;
-				return;
-			}
+		smurf_struct->module_mode = M_PICDONE;
+		break;
+
+	/* Mterm empfangen - Speicher freigeben und beenden */
+	case MTERM:
+		smurf_struct->module_mode = M_EXIT;
+		break;
+	}
 }
-
-
-/* bl„st von links sanft durchs Bild */
-int wind_left(char *data, unsigned int width, unsigned int height)
-{
-	char r, g, b, init_pix, this_pix;
-
-	int intens;
-	unsigned int x, y, daempfung;
-
-
-
-	y = height;
-	while(y--)
-	{
-		init_pix = (char)((((long)*data * 872L)
-						 + ((long)*(data + 1) * 2930L)
-						 + ((long)*(data + 2) * 296L)) >> 12);
-
-		r = *data++;
-		g = *data++;
-		b = *data++;
-
-		/* Wert fr ersten Start initialisieren */
-		intens = 95;
-		daempfung = (random(45) >> 1) + 3;
-
-		this_pix = (char)((((long)*data * 872L)
-						 + ((long)*(data + 1) * 2930L)
-						 + ((long)*(data + 2) * 296L)) >> 12);
-
-		x = width - 1;
-		while(x--)
-		{
-			while(init_pix > this_pix && intens > 0 && x)
-			{
-				*data++ = (char)((r * intens + *data * (128 - intens)) >> 7);
-				*data++ = (char)((g * intens + *data * (128 - intens)) >> 7);
-				*data++ = (char)((b * intens + *data * (128 - intens)) >> 7);
-				x--;
-
-				intens -= daempfung;
-
-				this_pix = (char)((((long)*data * 872L)
-								 + ((long)*(data + 1) * 2930L)
-								 + ((long)*(data + 2) * 296L)) >> 12);
-			}
-
-			data -= 3;
-			x++;
-
-			do
-			{
-				data += 3;
-				x--;
-
-				init_pix = this_pix;
-
-				this_pix = (char)((((long)*(data + 3) * 872L)
-								 + ((long)*(data + 4) * 2930L)
-								 + ((long)*(data + 5) * 296L)) >> 12);
-			} while(init_pix <= this_pix && x);
-
-			r = *data++;
-			g = *data++;
-			b = *data++;
-
-			/* Wert fr neuen Start initialisieren */
-			intens = 95;
-			daempfung = (random(45) >> 1) + 3;
-		}
-	}
-
-	return(0);
-} /* wind_left */
-
-
-/* bl„st von rechts sanft durchs Bild */
-int wind_right(char *data, unsigned int width, unsigned int height, char BitsPerPixel)
-{
-	char r, g, b, init_pix, this_pix;
-
-	int intens;
-	unsigned int x, y, daempfung;
-
-
-	/* auf Blaukomponente des letzten Bildpixels setzen */
-	data += (((long)width * (long)height * BitsPerPixel) >> 3) - 1;
-
-	y = height;
-	while(y--)
-	{
-		init_pix = (char)((((long)*(data - 2) * 872L)
-						 + ((long)*(data - 1) * 2930L)
-						 + ((long)*data * 296L)) >> 12);
-
-		b = *data--;
-		g = *data--;
-		r = *data--;
-
-		/* Wert fr ersten Start initialisieren */
-		intens = 95;
-		daempfung = (random(45) >> 1) + 3;
-
-		this_pix = (char)((((long)*(data - 2) * 871L)
-						 + ((long)*(data - 1) * 2929L)
-						 + ((long)*data * 295L)) >> 12);
-
-		x = width - 1;
-		while(x--)
-		{
-			intens -= daempfung;
-
-			while(init_pix > this_pix && intens > 0 && x)
-			{
-				*data-- = (char)((b * intens + *data * (128 - intens)) >> 7);
-				*data-- = (char)((g * intens + *data * (128 - intens)) >> 7);
-				*data-- = (char)((r * intens + *data * (128 - intens)) >> 7);
-				x--;
-
-				intens -= daempfung;
-
-				this_pix = (char)((((long)*(data - 2) * 872L)
-								 + ((long)*(data - 1) * 2930L)
-								 + ((long)*data * 296L)) >> 12);
-			}
-
-			data += 3;
-			x++;
-
-			do
-			{
-				data -= 3;
-				x--;
-
-				init_pix = this_pix;
-
-				this_pix = (char)((((long)*(data - 5) * 872L)
-								 + ((long)*(data - 4) * 2930L)
-								 + ((long)*(data - 3) * 296L)) >> 12);
-			} while(init_pix <= this_pix && x);
-
-			b = *data--;
-			g = *data--;
-			r = *data--;
-
-			/* Wert fr neuen Start initialisieren */
-			intens = 95;
-			daempfung = (random(45) >> 1) + 3;
-		}
-	}
-
-	return(0);
-} /* wind_right */
-
-
-/* bl„st von links kr„ftig durchs Bild */
-int storm_left(char *data, unsigned int width, unsigned int height)
-{
-	char r, g, b, init_pix, this_pix;
-
-	int intens, intens2;
-	unsigned int x, y, daempfung;
-
-
-
-	y = height;
-	while(y--)
-	{
-		init_pix = (char)((((long)*data * 872L)
-						 + ((long)*(data + 1) * 2930L)
-						 + ((long)*(data + 2) * 296L)) >> 12);
-
-		r = *data++;
-		g = *data++;
-		b = *data++;
-
-		/* Wert fr ersten Start initialisieren */
-		intens2 = intens = 105;
-		daempfung = (random(45) >> 1) + 3;
-
-		this_pix = (char)((((long)*data * 872L)
-						 + ((long)*(data + 1) * 2930L)
-						 + ((long)*(data + 2) * 296L)) >> 12);
-
-		x = width - 1;
-		while(x--)
-		{
-			while(init_pix > this_pix && intens > 0 && x)
-			{
-				*data++ = (char)((r * intens2 + *data * (128 - intens2)) >> 7);
-				*data++ = (char)((g * intens2 + *data * (128 - intens2)) >> 7);
-				*data++ = (char)((b * intens2 + *data * (128 - intens2)) >> 7);
-				x--;
-
-				intens -= daempfung;
-
-				this_pix = (char)((((long)*data * 872L)
-								 + ((long)*(data + 1) * 2930L)
-								 + ((long)*(data + 2) * 296L)) >> 12);
-			}
-
-			data -= 3;
-			x++;
-
-			do
-			{
-				data += 3;
-				x--;
-
-				init_pix = this_pix;
-
-				this_pix = (char)((((long)*(data + 3) * 872L)
-								 + ((long)*(data + 4) * 2930L)
-								 + ((long)*(data + 5) * 296L)) >> 12);
-			} while(init_pix <= this_pix && x);
-
-			r = *data++;
-			g = *data++;
-			b = *data++;
-
-			/* Wert fr neuen Start initialisieren */
-			intens2 = intens = 105;
-			daempfung = (random(45) >> 1) + 3;
-		}
-	}
-
-	return(0);
-} /* storm_left */
-
-
-/* bl„st von rechts kr„ftig durchs Bild */
-int storm_right(char *data, unsigned int width, unsigned int height, char BitsPerPixel)
-{
-	char r, g, b, init_pix, this_pix;
-
-	int intens, intens2;
-	unsigned int x, y, daempfung;
-
-
-	/* auf Blaukomponente des letzten Bildpixels setzen */
-	data += (((long)width * (long)height * BitsPerPixel) >> 3) - 1;
-
-	y = height;
-	while(y--)
-	{
-		init_pix = (char)((((long)*(data - 2) * 872L)
-						 + ((long)*(data - 1) * 2930L)
-						 + ((long)*data * 296L)) >> 12);
-
-		b = *data--;
-		g = *data--;
-		r = *data--;
-
-		/* Wert fr ersten Start initialisieren */
-		intens2 = intens = 105;
-		daempfung = (random(45) >> 1) + 3;
-
-		this_pix = (char)((((long)*(data - 2) * 872L)
-						 + ((long)*(data - 1) * 2930L)
-						 + ((long)*data * 296L)) >> 12);
-
-		x = width - 1;
-		while(x--)
-		{
-			intens -= daempfung;
-
-			while(init_pix > this_pix && intens > 0 && x)
-			{
-				*data-- = (char)((b * intens2 + *data * (128 - intens2)) >> 7);
-				*data-- = (char)((g * intens2 + *data * (128 - intens2)) >> 7);
-				*data-- = (char)((r * intens2 + *data * (128 - intens2)) >> 7);
-				x--;
-
-				intens -= daempfung;
-
-				this_pix = (char)((((long)*(data - 2) * 872L)
-								 + ((long)*(data - 1) * 2930L)
-								 + ((long)*data * 296L)) >> 12);
-			}
-
-			data += 3;
-			x++;
-
-			do
-			{
-				data -= 3;
-				x--;
-
-				init_pix = this_pix;
-
-				this_pix = (char)((((long)*(data - 5) * 872L)
-								 + ((long)*(data - 4) * 2930L)
-								 + ((long)*(data - 3) * 296L)) >> 12);
-			} while(init_pix <= this_pix && x);
-
-			b = *data--;
-			g = *data--;
-			r = *data--;
-
-			/* Wert fr neuen Start initialisieren */
-			intens2 = intens = 105;
-			daempfung = (random(45) >> 1) + 3;
-		}
-	}
-
-	return(0);
-} /* storm_right */
