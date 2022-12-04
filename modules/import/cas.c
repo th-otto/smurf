bbfe860b9e72da65e2df25f00146778e8c3e237d
Thorsten Otto <admin@tho-otto.de>
2022-12-04 14:13:16 +0100

    Cleanup import/cas

--- modules/import/cas.c
+++ modules/import/cas.c
@@ -47,156 +47,166 @@
 #include "import.h"
 #include "smurfine.h"
 
-char *decodePCX(char *buffer, int BytePerLine, int height);
 #define NOMEM   ( (void*) 0L )
 #define DEBUG           0
 
 /* Infostruktur fÅr Hauptmodul */
-MOD_INFO module_info = {"DCX-CAS-Fax Importer",
-						0x0050,
-                        "Dale Russell",
-                        "DCX", "", "", "", "",
-                        "", "", "", "", "",
-                        "Slider 1",
-                        "Slider 2",
-                        "Slider 3",
-                        "Slider 4",
-                        "Checkbox 1",
-                        "Checkbox 2",
-                        "Checkbox 3",
-                        "Checkbox 4",
-                        "Edit 1",
-                        "Edit 2",
-                        "Edit 3",
-                        "Edit 4",
-                        0,128,
-                        0,128,
-                        0,128,
-                        0,128,
-                        0,10,
-                        0,10,
-                        0,10,
-                        0,10,
-                        0,0,0,0,
-                        0,0,0,0,
-                        0,0,0,0,
-                        0
-                        };
+MOD_INFO module_info = {
+	"DCX-CAS-Fax Importer",
+	0x0050,
+	"Dale Russell",
+	{ "DCX", "", "", "", "", "", "", "", "", "" },
+	"Slider 1",
+	"Slider 2",
+	"Slider 3",
+	"Slider 4",
+	"Checkbox 1",
+	"Checkbox 2",
+	"Checkbox 3",
+	"Checkbox 4",
+	"Edit 1",
+	"Edit 2",
+	"Edit 3",
+	"Edit 4",
+	0, 128,
+	0, 128,
+	0, 128,
+	0, 128,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0,
+	NULL, NULL, NULL, NULL, NULL, NULL
+};
+
+
+/*************************************************************/
+/*  PCX ggf. dekodieren                                      */
+/*************************************************************/
+static uint8_t *decodePCX(uint8_t *buffer, short BytePerLine, short height)
+{
+	short x, c, xc, yc;
+	uint8_t *output;
+	uint8_t *RetBuf;
+
+	output = (uint8_t *)Malloc((long) BytePerLine * (long) height);
+	if (!output)
+		return NULL;
+	RetBuf = output;
+	yc = 0;
+	while (yc++ < height)
+	{
+		xc = 0;
+		while (xc < BytePerLine)
+		{
+			x = *(buffer++);
+			if (x >= 192)
+			{
+				x -= 192;
+				xc += x;
+				c = *(buffer++);
+				while (x--)
+					*(output++) = c;
+			} else
+			{
+				*(output++) = x;
+				xc++;
+			}
+		}
+	}
+	return RetBuf;
+}
 
 /* -------------------------------------------------*/
 /* -------------------------------------------------*/
 /*      CAS FAX Standard                            */
 /* -------------------------------------------------*/
 /* -------------------------------------------------*/
-short imp_module_main(GARGAMEL *smurf_struct)
+short imp_module_main(GARGAMEL * smurf_struct)
 {
-char    *buffer, *GetBuf, *out2;
-int width=0, height=0, PicCnt,x;
-long PicOffset[1024];
-int lBorder,rBorder,uBorder,dBorder;
-int BytePerLine, KompFlag;
-/***************************************************************/
-/*      Kopfdaten analysieren                                  */
-/***************************************************************/
-buffer=smurf_struct->smurf_pic->pic_data;
+	uint8_t *buffer;
+	uint8_t *GetBuf;
+	uint8_t *out2;
+	short width;
+	short height;
+	short PicCnt;
+	short x;
+	long PicOffset[1024];
+	short lBorder, rBorder, uBorder, dBorder;
+	short BytePerLine, KompFlag;
 
-smurf_struct->services->reset_busybox(128, "CAS Fax 1 Bit");
+	/***************************************************************/
+	/*      Kopfdaten analysieren                                  */
+	/***************************************************************/
+	buffer = smurf_struct->smurf_pic->pic_data;
 
-if ( (*(buffer)!=0xb1) || (*(buffer+1)!=0x68) ||
-        (*(buffer+2)!=0xde) || (*(buffer+3)!=0x3a) ) return(M_INVALID);
+	smurf_struct->services->reset_busybox(128, "CAS Fax 1 Bit");
 
-PicCnt=0;                       /* BildzÑhler */            
-for (x=0; x<1024; x++)          /* Eintragen der Offsets */
-{
-    GetBuf=buffer+0x04+(x<<2);
-    PicOffset[x]=(long)*(GetBuf)+(long)(*(GetBuf+1)<<8)+(long)
-                (*(GetBuf+2)<<16L)+(long)(*(GetBuf+3)<<24L);
-    if (PicOffset[x]!=0L) PicCnt++;     
-}
-if (!PicCnt)
-{
-        form_alert(1, ERROR1 );
-        return(M_PICERR);
-}               
-if (PicCnt>1)
-        form_alert(1, ERROR2 );
+	if ((*(buffer) != 0xb1) || (*(buffer + 1) != 0x68) || (*(buffer + 2) != 0xde) || (*(buffer + 3) != 0x3a))
+		return M_INVALID;
 
-strncpy(smurf_struct->smurf_pic->format_name, "CAS Fax Standard     ", 21);
+	PicCnt = 0;							/* BildzÑhler */
+	for (x = 0; x < 1024; x++)			/* Eintragen der Offsets */
+	{
+		GetBuf = buffer + 0x04 + (x << 2);
+		PicOffset[x] = (long) *(GetBuf) + (long) (*(GetBuf + 1) << 8) + (long)
+			(*(GetBuf + 2) << 16L) + (long) (*(GetBuf + 3) << 24L);
+		if (PicOffset[x] != 0L)
+			PicCnt++;
+	}
+	if (!PicCnt)
+	{
+		form_alert(1, ERROR1);
+		return M_PICERR;
+	}
+	if (PicCnt > 1)
+		form_alert(1, ERROR2);
 
-/**********************************************************/
-/*  Auslesen der PCX Struktur                             */
-/**********************************************************/
-GetBuf=buffer+PicOffset[0];     /* 1. Bild */
-if (*(GetBuf)!=0x0a)
-{
-    form_alert(1, ERROR3 );
-    return(M_PICERR);
-}
-KompFlag=*(GetBuf+2);
-lBorder=*(GetBuf+4)+(*(GetBuf+5)<<8);
-uBorder=*(GetBuf+6)+(*(GetBuf+7)<<8);
-rBorder=*(GetBuf+8)+(*(GetBuf+9)<<8);
-dBorder=*(GetBuf+10)+(*(GetBuf+11)<<8);
-BytePerLine=*(GetBuf+0x42)+(*(GetBuf+0x43)<<8);
-width=(rBorder-lBorder)+1;
-height=(dBorder-uBorder)+1;
+	strcpy(smurf_struct->smurf_pic->format_name, "CAS Fax Standard     ");
 
-if (KompFlag)
-{
-    out2=decodePCX(GetBuf+128, BytePerLine, height);
-    if (out2==NOMEM)
-    {
-        return(M_MEMORY);
-    }   
-    else Mfree(buffer);
-    buffer=out2;
-}
+	/**********************************************************/
+	/*  Auslesen der PCX Struktur                             */
+	/**********************************************************/
+	GetBuf = buffer + PicOffset[0];		/* 1. Bild */
+	if (*(GetBuf) != 0x0a)
+	{
+		form_alert(1, ERROR3);
+		return M_PICERR;
+	}
+	KompFlag = *(GetBuf + 2);
+	lBorder = *(GetBuf + 4) + (*(GetBuf + 5) << 8);
+	uBorder = *(GetBuf + 6) + (*(GetBuf + 7) << 8);
+	rBorder = *(GetBuf + 8) + (*(GetBuf + 9) << 8);
+	dBorder = *(GetBuf + 10) + (*(GetBuf + 11) << 8);
+	BytePerLine = *(GetBuf + 0x42) + (*(GetBuf + 0x43) << 8);
+	width = (rBorder - lBorder) + 1;
+	height = (dBorder - uBorder) + 1;
 
-else if (!KompFlag)     
-    buffer=GetBuf+128;  /* Rohdaten */
-        
-/***************************************************************/
-/*      Strukturen fÅllen                                      */
-/***************************************************************/
-smurf_struct->smurf_pic->pic_width=width;
-smurf_struct->smurf_pic->pic_height=height;
-smurf_struct->smurf_pic->depth=1;
-smurf_struct->smurf_pic->bp_pal=0;
-smurf_struct->smurf_pic->pic_data=buffer;
-smurf_struct->smurf_pic->format_type=1;
+	if (KompFlag)
+	{
+		out2 = decodePCX(GetBuf + 128, BytePerLine, height);
+		if (out2 == NULL)
+			return M_MEMORY;
+		Mfree(buffer);
+		buffer = out2;
+	} else
+	{
+		buffer = GetBuf + 128;			/* Rohdaten */
+	}
 
-return (M_PICDONE);
-}
+	/***************************************************************/
+	/*      Strukturen fÅllen                                      */
+	/***************************************************************/
+	smurf_struct->smurf_pic->pic_width = width;
+	smurf_struct->smurf_pic->pic_height = height;
+	smurf_struct->smurf_pic->depth = 1;
+	smurf_struct->smurf_pic->bp_pal = 0;
+	smurf_struct->smurf_pic->pic_data = buffer;
+	smurf_struct->smurf_pic->format_type = 1;
 
-/*************************************************************/
-/*  PCX ggf. dekodieren                                      */
-/*************************************************************/
-char *decodePCX(char *buffer, int BytePerLine, int height)
-{
-register int x,c,xc,yc;
-register char *output, *RetBuf;
-output=Mxalloc( (long)BytePerLine*(long)height, 2);
-if (!output)
-    return(NOMEM);
-RetBuf=output;
-yc=0;
- while(yc++<height)
- {
-    xc=0;
-    while(xc<BytePerLine)
-    {
-        x=*(buffer++);
-        if (x >= 192)
-        {   
-            x -= 192;
-            xc+=x;
-            c=*(buffer++);
-            while (x--)
-                *(output++)=c;
-        }
-        else  {
-            *(output++)=x; xc++; }
-    } /* while xc */
- } /* while yc */   
-return(RetBuf);
+	return M_PICDONE;
 }
