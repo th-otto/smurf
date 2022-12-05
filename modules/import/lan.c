713178ce6a5c6912f6d3591ff315f43c6be7cb6f
Thorsten Otto <admin@tho-otto.de>
2022-12-05 14:03:18 +0100

    Cleanup import/lan

--- modules/import/lan.c
+++ modules/import/lan.c
@@ -36,41 +36,48 @@
 #include "import.h"
 #include "smurfine.h"
 
+#ifdef __PUREC__
 /* Dies bastelt direkt ein rol.w #8,d0 inline ein. */
-unsigned int swap_word(unsigned int w)
-	0xE058;
+static unsigned short swap_word(unsigned short w) 0xE058;
+#else
+static unsigned short swap_word(unsigned short w)
+{
+	return (w >> 8) | (w << 8);
+}
+#endif
 
 /* Infostruktur fr Hauptmodul */
-MOD_INFO module_info = {"Erdas Image-Format",
-						0x0020,
-						"Christian Eyrich",
-						"LAN", "GIS", "", "", "",
-						"", "", "", "", "",
-						"Slider 1",
-						"Slider 2",
-						"Slider 3",
-						"Slider 4",
-						"Checkbox 1",
-						"Checkbox 2",
-						"Checkbox 3",
-						"Checkbox 4",
-						"Edit 1",
-						"Edit 2",
-						"Edit 3",
-						"Edit 4",
-						0,128,
-						0,128,
-						0,128,
-						0,128,
-						0,10,
-						0,10,
-						0,10,
-						0,10,
-						0, 0, 0, 0,
-						0, 0, 0, 0,
-						0, 0, 0, 0,
-						0
-						};
+MOD_INFO module_info = {
+	"Erdas Image-Format",
+	0x0020,
+	"Christian Eyrich",
+	{ "LAN", "GIS", "", "", "", "", "", "", "", "" },
+	"Slider 1",
+	"Slider 2",
+	"Slider 3",
+	"Slider 4",
+	"Checkbox 1",
+	"Checkbox 2",
+	"Checkbox 3",
+	"Checkbox 4",
+	"Edit 1",
+	"Edit 2",
+	"Edit 3",
+	"Edit 4",
+	0, 128,
+	0, 128,
+	0, 128,
+	0, 128,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0,
+	NULL, NULL, NULL, NULL, NULL, NULL
+};
 
 /* -------------------------------------------------*/
 /* -------------------------------------------------*/
@@ -80,92 +87,93 @@ MOD_INFO module_info = {"Erdas Image-Format",
 /* -------------------------------------------------*/
 short imp_module_main(GARGAMEL *smurf_struct)
 {
-	char *buffer, *obuffer, *ziel, *pal,
-		 Planes, BitsPerPixel, DatenOffset;
-	char dummy[3], impmessag[21];
-
-	unsigned int x, y, width, height, i;
-
+	uint8_t *buffer;
+	uint8_t *obuffer;
+	uint8_t *ziel;
+	uint8_t *pal;
+	uint8_t Planes;
+	uint8_t BitsPerPixel;
+	unsigned short DatenOffset;
+	char dummy[3];
+	char impmessag[21];
+	unsigned short x, y, width, height, i;
 	unsigned long len;
 
 	buffer = smurf_struct->smurf_pic->pic_data;
 	obuffer = buffer;
 
-	if(*(unsigned long *)buffer != 0x48454144L || *(unsigned int *)(buffer + 4) != 0x3734)
-		return(M_INVALID);
+	if (*(uint32_t *) buffer != 0x48454144L || *(uint16_t *) (buffer + 4) != 0x3734)
+		return M_INVALID;
+	/* Liegt zwar als DWord vor, ich lese aber nur die hintersten beiden Bytes */
+	Planes = (char) swap_word(*(uint16_t *) (buffer + 0x0a));
+	if (Planes == 1)
+		BitsPerPixel = 8;
 	else
+		BitsPerPixel = 24;
+
+	/* Liegen zwar als DWord vor, ich lese aber nur die hintersten beiden Bytes */
+	width = swap_word(*(uint16_t *) (buffer + 0x12));
+	height = swap_word(*(uint16_t *) (buffer + 0x16));
+
+	DatenOffset = 0x80;
+
+	strcpy(smurf_struct->smurf_pic->format_name, "Erdas Image-Format .LAN, .SUN");
+	smurf_struct->smurf_pic->pic_width = width;
+	smurf_struct->smurf_pic->pic_height = height;
+	smurf_struct->smurf_pic->depth = BitsPerPixel;
+
+	strcpy(impmessag, "LAN Erdas ");
+	strcat(impmessag, itoa(BitsPerPixel, dummy, 10));
+	strcat(impmessag, " Bit");
+	smurf_struct->services->reset_busybox(128, impmessag);
+
+	ziel = buffer;
+
+	len = (((unsigned long) width * (unsigned long) height) * BitsPerPixel) >> 3;
+
+	if (BitsPerPixel == 8)
 	{
+		memmove(ziel, buffer + DatenOffset, len);
+	} else
+	{
+		buffer += DatenOffset;
 
-		/* Liegt zwar als DWord vor, ich lese aber nur die hintersten beiden Bytes */
-		Planes = (char)swap_word(*(unsigned int *)(buffer + 0x0a));
-		if(Planes == 1)
-			BitsPerPixel = 8;
-		else
-			BitsPerPixel = 24;
-
-		/* Liegen zwar als DWord vor, ich lese aber nur die hintersten beiden Bytes */
-		width = swap_word(*(unsigned int *)(buffer + 0x12));
-		height = swap_word(*(unsigned int *)(buffer + 0x16));
-
-		DatenOffset = 0x80;
-
-		strncpy(smurf_struct->smurf_pic->format_name, "Erdas Image-Format .LAN, .SUN", 21);
-		smurf_struct->smurf_pic->pic_width = width;
-		smurf_struct->smurf_pic->pic_height = height;
-		smurf_struct->smurf_pic->depth = BitsPerPixel;
-
-		strcpy(impmessag, "LAN Erdas ");
-		strcat(impmessag, itoa(BitsPerPixel, dummy, 10));
-		strcat(impmessag, " Bit");
-		smurf_struct->services->reset_busybox(128, impmessag);
-
-		ziel = buffer;
-
-		len = (((long)width * (long)height) * BitsPerPixel) >> 3;
-
-		if(BitsPerPixel == 8)
-			memcpy(ziel, buffer + DatenOffset, len);
-		else
+		y = 0;
+		do
 		{
-			buffer += DatenOffset;
-
-			y = 0;
+			x = 0;
 			do
 			{
-				x = 0;
-				do
-				{
-					*ziel++ = *buffer;
-					*ziel++ = *(buffer + width);
-					*ziel++ = *(buffer++ + (width << 1));
-				} while(++x < width);
-				buffer += (width << 1);
-			} while(++y < height);
-		}
+				*ziel++ = *buffer;
+				*ziel++ = *(buffer + width);
+				*ziel++ = *(buffer++ + (width << 1));
+			} while (++x < width);
+			buffer += (width << 1);
+		} while (++y < height);
+	}
 
-		buffer = obuffer;
-		ziel = buffer;
+	buffer = obuffer;
+	ziel = buffer;
 
-		_Mshrink(ziel, len);
+	_Mshrink(ziel, len);
 
-		smurf_struct->smurf_pic->pic_data = ziel;
+	smurf_struct->smurf_pic->pic_data = ziel;
 
-		smurf_struct->smurf_pic->format_type = FORM_PIXELPAK;
+	smurf_struct->smurf_pic->format_type = FORM_PIXELPAK;
 
-		if(BitsPerPixel == 8)
+	if (BitsPerPixel == 8)
+	{
+		pal = smurf_struct->smurf_pic->palette;
+
+		for (i = 0; i < 256; i++)
 		{
-			pal = smurf_struct->smurf_pic->palette;
-
-			for(i = 0; i < 256; i++)
-			{
-				*pal++ = (char)i;
-				*pal++ = (char)i;
-				*pal++ = (char)i;
-			}
+			*pal++ = (char) i;
+			*pal++ = (char) i;
+			*pal++ = (char) i;
 		}
+	}
 
-		smurf_struct->smurf_pic->col_format = RGB;
-	} /* Erkennung */
+	smurf_struct->smurf_pic->col_format = RGB;
 
-	return(M_PICDONE);
+	return M_PICDONE;
 }
