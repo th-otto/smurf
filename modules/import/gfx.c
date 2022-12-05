cb1844ace1ba5f5bd492e6550536e3f9e3735d65
Thorsten Otto <admin@tho-otto.de>
2022-12-05 10:35:17 +0100

    Cleanup import/gfx

--- modules/import/gfx.c
+++ modules/import/gfx.c
@@ -34,15 +34,24 @@
 #include "smurfine.h"
 
 /* Infostruktur fÅr Hauptmodul */
-MOD_INFO	module_info={
-"XGrabber",0x0110,"Bjîrn Spruck",
-"GFX","","","","","","","","","",
+MOD_INFO module_info = {
+	"XGrabber",
+	0x0110,
+	"Bjîrn Spruck",
+	{ "GFX", "", "", "", "", "", "", "", "", "" },
 /* Objekttitel */
-"","","","","","","","","","","","",
+	"", "", "", "",
+	"", "", "", "",
+	"", "", "", "",
 /* Objektgrenzwerte */
-0,128,0,128,0,128,0,128,0,10,0,10,0,10,0,10,
+	0, 128, 0, 128, 0, 128, 0, 128,
+	0, 10, 0, 10, 0, 10, 0, 10,
 /* Slider-Defaultwerte */
-0,0,0,0,0,0,0,0,0,0,0,0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0,
+	NULL, NULL, NULL, NULL, NULL, NULL
 };
 
 /*
@@ -78,63 +87,94 @@ Die Auflîsungsinformationen:
 18 .w  | $FFFF8264.w     | H-Scroll
 */
 
-void convertiere_bild(unsigned short *sc1,unsigned short *sc2,unsigned int x,unsigned int y,unsigned int e)
+static void convertiere_bild(unsigned short *sc1, unsigned short *sc2, unsigned short x, unsigned short y, unsigned short e)
 {
 	long i;
-	switch(e){
-		case 1 :{
-			for( i=0; i<(x+15)/16*y; i++ ){
-				*sc2++=*sc1++;
+
+	switch (e)
+	{
+	case 1:
+		{
+			for (i = 0; i < (x + 15) / 16 * y; i++)
+			{
+				*sc2++ = *sc1++;
 			}
-		} break;
-		case 2 :{
-			unsigned short *s1, *s2;
-			s1=sc2;
-			s2=s1+(x+15)/16*y;
-			for( i=0; i<(x+15)/16*y; i++ ){
-				*s1++=*sc1++;
-				*s2++=*sc1++;
+		}
+		break;
+	case 2:
+		{
+			unsigned short *s1,
+			*s2;
+
+			s1 = sc2;
+			s2 = s1 + (x + 15) / 16 * y;
+			for (i = 0; i < (x + 15) / 16 * y; i++)
+			{
+				*s1++ = *sc1++;
+				*s2++ = *sc1++;
 			}
-		} break;
-		case 4 :{
-			unsigned short *s1, *s2, *s3, *s4;
-			s1=sc2;
-			s2=s1+(x+15)/16*y;
-			s3=s2+(x+15)/16*y;
-			s4=s3+(x+15)/16*y;
-			for( i=0; i<(x+15)/16*y; i++ ){
-				*s1++=*sc1++;
-				*s2++=*sc1++;
-				*s3++=*sc1++;
-				*s4++=*sc1++;
+		}
+		break;
+	case 4:
+		{
+			unsigned short *s1,
+			*s2,
+			*s3,
+			*s4;
+
+			s1 = sc2;
+			s2 = s1 + (x + 15) / 16 * y;
+			s3 = s2 + (x + 15) / 16 * y;
+			s4 = s3 + (x + 15) / 16 * y;
+			for (i = 0; i < (x + 15) / 16 * y; i++)
+			{
+				*s1++ = *sc1++;
+				*s2++ = *sc1++;
+				*s3++ = *sc1++;
+				*s4++ = *sc1++;
 			}
-		} break;
-		case 8 :{
-			unsigned short *s1, *s2, *s3, *s4, *s5, *s6, *s7, *s8;
-			s1=sc2;
-			s2=s1+(x+15)/16*y;
-			s3=s2+(x+15)/16*y;
-			s4=s3+(x+15)/16*y;
-			s5=s4+(x+15)/16*y;
-			s6=s5+(x+15)/16*y;
-			s7=s6+(x+15)/16*y;
-			s8=s7+(x+15)/16*y;
-			for( i=0; i<(x+15)/16*y; i++ ){
-				*s1++=*sc1++;
-				*s2++=*sc1++;
-				*s3++=*sc1++;
-				*s4++=*sc1++;
-				*s5++=*sc1++;
-				*s6++=*sc1++;
-				*s7++=*sc1++;
-				*s8++=*sc1++;
+		}
+		break;
+	case 8:
+		{
+			unsigned short *s1,
+			*s2,
+			*s3,
+			*s4,
+			*s5,
+			*s6,
+			*s7,
+			*s8;
+
+			s1 = sc2;
+			s2 = s1 + (x + 15) / 16 * y;
+			s3 = s2 + (x + 15) / 16 * y;
+			s4 = s3 + (x + 15) / 16 * y;
+			s5 = s4 + (x + 15) / 16 * y;
+			s6 = s5 + (x + 15) / 16 * y;
+			s7 = s6 + (x + 15) / 16 * y;
+			s8 = s7 + (x + 15) / 16 * y;
+			for (i = 0; i < (x + 15) / 16 * y; i++)
+			{
+				*s1++ = *sc1++;
+				*s2++ = *sc1++;
+				*s3++ = *sc1++;
+				*s4++ = *sc1++;
+				*s5++ = *sc1++;
+				*s6++ = *sc1++;
+				*s7++ = *sc1++;
+				*s8++ = *sc1++;
 			}
-		} break;
-		case 16 :{
-			for( i=0; i<(x+15)/16*y*32; i++ ){
-				*sc2++=*sc1++;
+		}
+		break;
+	case 16:
+		{
+			for (i = 0; i < (x + 15) / 16 * y * 32; i++)
+			{
+				*sc2++ = *sc1++;
 			}
-		} break;
+		}
+		break;
 	}
 }
 
@@ -145,64 +185,81 @@ void convertiere_bild(unsigned short *sc1,unsigned short *sc2,unsigned int x,uns
 /* -------------------------------------------------*/
 short imp_module_main(GARGAMEL *smurf_struct)
 {
-	char *bs, *locbuffer;
-	int fmode=1, fe=4, *head;
-	int w,h;
-	
-	if(smurf_struct->smurf_pic->file_len<=38+1024+32) return(M_INVALID);
-	
-	bs=smurf_struct->smurf_pic->pic_data;
+	uint8_t *bs;
+	uint8_t *locbuffer;
+	short fmode = 1;
+	short fe = 4;
+	short *head;
+	short w, h;
 
-	head=(void *)bs;
-	bs+=38;/* Header */
+	if (smurf_struct->smurf_pic->file_len <= 38 + 1024 + 32)
+		return M_INVALID;
 
-	w=head[16]+head[17];
-	h=(head[12]-head[11])/2;
-	if( head[15]&0x1) h=h/2;
-	if( head[15]&0x2)	h=h*2;
-	if( head[1]&0x10){
-		fe=8;
-		w+=w;
-	}else{
-		if( head[1]&0x100){
-			fe=16;
-		}else{
-			fe=4;
-			w+=w;
-			w+=w;
-			if( head[0]&0x100){
-				fe=2;
-				w+=w;
-				fmode=0;
+	bs = smurf_struct->smurf_pic->pic_data;
+
+	head = (void *) bs;
+	bs += 38;							/* Header */
+
+	w = head[16] + head[17];
+	h = (head[12] - head[11]) / 2;
+	if (head[15] & 0x1)
+		h = h / 2;
+	if (head[15] & 0x2)
+		h = h * 2;
+	if (head[1] & 0x10)
+	{
+		fe = 8;
+		w += w;
+	} else
+	{
+		if (head[1] & 0x100)
+		{
+			fe = 16;
+		} else
+		{
+			fe = 4;
+			w += w;
+			w += w;
+			if (head[0] & 0x100)
+			{
+				fe = 2;
+				w += w;
+				fmode = 0;
 			}
-			if( head[1]&0x400){
-				fe=1;
-				w+=w;
-				w+=w;
+			if (head[1] & 0x400)
+			{
+				fe = 1;
+				w += w;
+				w += w;
 			}
 		}
 	}
-	if(fe!=16) w=(w+15)&0xFFF0;
-	
+	if (fe != 16)
+		w = (w + 15) & 0xFFF0;
+
 /*	if( head[5]<0xb0) fmode=0;*/
-	if(smurf_struct->smurf_pic->file_len!=38+1024+32+(long)w*h/8*fe ) return(M_INVALID);
-	
+	if (smurf_struct->smurf_pic->file_len != 38 + 1024 + 32 + (long) w * h / 8 * fe)
+		return M_INVALID;
+
 	strcpy(smurf_struct->smurf_pic->format_name, "XGrabber Format");
 
 	{
-		unsigned char *f=(unsigned char *)smurf_struct->smurf_pic->palette;
+		uint8_t *f = (uint8_t *) smurf_struct->smurf_pic->palette;
 		int i;
 
-		if( fmode){	
-			for( i=0; i<256; i++){
-				*f++=(unsigned char)*bs++;
-				*f++=(unsigned char)*bs++;
+		if (fmode)
+		{
+			for (i = 0; i < 256; i++)
+			{
+				*f++ = *bs++;
+				*f++ = *bs++;
 				bs++;
-				*f++=(unsigned char)*bs++;
+				*f++ = *bs++;
 			}
-			bs+=32;
-		}else{
-			bs+=1024;
+			bs += 32;
+		} else
+		{
+			bs += 1024;
 /*
   321076543210
   ||||||||||||
@@ -210,37 +267,43 @@ short imp_module_main(GARGAMEL *smurf_struct)
   ||||++++------Green intensity
   ++++----------Red intensity
 */
-			for(i=0; i<16; i++){
-				unsigned int x;
-				x=*(unsigned int *)bs;
-				*f++=(unsigned char)(((unsigned int)(x>>3)&0xE0)+ ((x&0x800)?0x10:0));
-				*f++=(unsigned char)(((unsigned int)(x<<1)&0xE0)+ ((x&0x80)?0x10:0));
-				*f++=(unsigned char)(((unsigned int)(x<<5)&0xE0)+ ((x&0x8)?0x10:0));
-				bs+=2;
+			for (i = 0; i < 16; i++)
+			{
+				uint16_t x;
+
+				x = *(uint16_t *) bs;
+				*f++ = (((x >> 3) & 0xE0) + ((x & 0x800) ? 0x10 : 0));
+				*f++ = (((x << 1) & 0xE0) + ((x & 0x80) ? 0x10 : 0));
+				*f++ = (((x << 5) & 0xE0) + ((x & 0x8) ? 0x10 : 0));
+				bs += 2;
 			}
 		}
 	}
-	memcpy(head,bs,(long)w*h/8*fe);
-	bs = (char *)head;
-	if( fe<=8 && fe>1){
-		locbuffer=Malloc((long)w*h/8*fe);
-		if( locbuffer==0) return(M_MEMORY);
-		convertiere_bild( (unsigned short *)bs, (unsigned short *)locbuffer, w, h, fe);
-		Mfree( bs);
-		bs=locbuffer;
+	memcpy(head, bs, (long) w * h / 8 * fe);
+	bs = (uint8_t *) head;
+	if (fe <= 8 && fe > 1)
+	{
+		locbuffer = (uint8_t *)Malloc((long) w * h / 8 * fe);
+		if (locbuffer == 0)
+			return M_MEMORY;
+		convertiere_bild((unsigned short *) bs, (unsigned short *) locbuffer, w, h, fe);
+		Mfree(bs);
+		bs = locbuffer;
 	}
 
-	smurf_struct->smurf_pic->pic_width=w;
-	smurf_struct->smurf_pic->pic_height=h;
-	smurf_struct->smurf_pic->depth=fe;
-	smurf_struct->smurf_pic->col_format=RGB;
-	if(fe<=8){
-		smurf_struct->smurf_pic->format_type=FORM_STANDARD;
-	}else{
-		smurf_struct->smurf_pic->format_type=FORM_PIXELPAK;
+	smurf_struct->smurf_pic->pic_width = w;
+	smurf_struct->smurf_pic->pic_height = h;
+	smurf_struct->smurf_pic->depth = fe;
+	smurf_struct->smurf_pic->col_format = RGB;
+	if (fe <= 8)
+	{
+		smurf_struct->smurf_pic->format_type = FORM_STANDARD;
+	} else
+	{
+		smurf_struct->smurf_pic->format_type = FORM_PIXELPAK;
 	}
-	_Mshrink(bs,(long)w*h/8*fe);
-	smurf_struct->smurf_pic->pic_data=bs;
+	_Mshrink(bs, (long) w * h / 8 * fe);
+	smurf_struct->smurf_pic->pic_data = bs;
 
-	return(M_PICDONE);
+	return M_PICDONE;
 }
