6c8b957b0a7aef1a4270d3ac282def7331502d77
Thorsten Otto <admin@tho-otto.de>
2022-12-06 09:09:16 +0100

    Cleanup import/neoch

--- modules/import/neoch.c
+++ modules/import/neoch.c
@@ -30,36 +30,37 @@
 #include "smurfine.h"
 
 /* Infostruktur fr Hauptmodul */
-MOD_INFO module_info = {"Neochrome-Importer",
-                        0x0090,
-                        "Dale Russell",
-                        "NEO", "", "", "", "",
-                        "", "", "", "", "",
-                        "Slider 1",
-                        "Slider 2",
-                        "Slider 3",
-                        "Slider 4",
-                        "Checkbox 1",
-                        "Checkbox 2",
-                        "Checkbox 3",
-                        "Checkbox 4",
-                        "Edit 1",
-                        "Edit 2",
-                        "Edit 3",
-                        "Edit 4",
-                        0,128,
-                        0,128,
-                        0,128,
-                        0,128,
-                        0,10,
-                        0,10,
-                        0,10,
-                        0,10,
-                        0,0,0,0,
-                        0,0,0,0,
-                        0,0,0,0,
-                        0
-                        };
+MOD_INFO module_info = {
+	"Neochrome-Importer",
+	0x0090,
+	"Dale Russell",
+	{ "NEO", "", "", "", "", "", "", "", "", "" },
+	"Slider 1",
+	"Slider 2",
+	"Slider 3",
+	"Slider 4",
+	"Checkbox 1",
+	"Checkbox 2",
+	"Checkbox 3",
+	"Checkbox 4",
+	"Edit 1",
+	"Edit 2",
+	"Edit 3",
+	"Edit 4",
+	0, 128,
+	0, 128,
+	0, 128,
+	0, 128,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0,
+	NULL, NULL, NULL, NULL, NULL, NULL
+};
 
 /* -------------------------------------------------*/
 /* -------------------------------------------------*/
@@ -68,82 +69,114 @@ MOD_INFO module_info = {"Neochrome-Importer",
 /* -------------------------------------------------*/
 short imp_module_main(GARGAMEL *smurf_struct)
 {
-char    *buffer, *smpal, *paddr, *opaddr;
-char mdummy[3], impmessag[21];
-int     *ibuf, *palet;
-unsigned int    e,f,v1, r,g,b, width, height, BitsPerPixel,x ,y;
-unsigned int    mask_bytes[18]={0x8000, 0x4000, 0x2000, 0x1000, 
-                        0x0800, 0x0400, 0x0200, 0x0100, 0x0080, 
-                        0x0040, 0x0020, 0x0010, 0x0008, 0x0004,
-                        0x0002,0x0001};
-unsigned int    dummy;
-unsigned    int merk_bytes1, merk_bytes2, merk_bytes3, merk_bytes4;
-char extend[4];
-char *filenam;
+	uint8_t *buffer;
+	uint8_t *smpal;
+	uint8_t *paddr;
+	uint8_t *opaddr;
+	char mdummy[3];
+	char impmessag[21];
+	uint16_t *ibuf;
+	uint16_t *palet;
+	unsigned short e, f, v1;
+	unsigned short r, g, b;
+	unsigned short width, height;
+	unsigned short BitsPerPixel;
+	unsigned short x, y;
 
-filenam=smurf_struct->smurf_pic->filename;
-strncpy(extend, filenam+(strlen(filenam)-3), 3);
-buffer=smurf_struct->smurf_pic->pic_data;
-if ( strncmp(extend, "NEO", 3) )
-            return(M_INVALID);
+	static unsigned short const mask_bytes[18] = {
+		0x8000, 0x4000, 0x2000, 0x1000,
+		0x0800, 0x0400, 0x0200, 0x0100, 0x0080,
+		0x0040, 0x0020, 0x0010, 0x0008, 0x0004,
+		0x0002, 0x0001
+	};
+	unsigned short dummy;
+	uint16_t merk_bytes1, merk_bytes2, merk_bytes3, merk_bytes4;
+	char extend[4];
+	char *filenam;
 
-strncpy(smurf_struct->smurf_pic->format_name, "Neochrome .NEO      ", 21);
-v1=*(buffer+2)*256+*(buffer+3);             /* Bilddimensionen */
-if (v1==0)  { width=320; height=200; BitsPerPixel=4; }
-else if (v1==1) { width=640; height=200; BitsPerPixel=2;}
-else { width=640; height=400; BitsPerPixel=1;}
-palet=(int *)(buffer+4);
+	filenam = smurf_struct->smurf_pic->filename;
+	strcpy(extend, filenam + (strlen(filenam) - 3));
+	buffer = smurf_struct->smurf_pic->pic_data;
+	if (strnicmp(extend, "NEO", 3) != 0)
+		return M_INVALID;
 
-strcpy(impmessag, "Neochrome ");
-strcat(impmessag, itoa(BitsPerPixel, mdummy, 10));
-strcat(impmessag, " Bit");
-smurf_struct->services->reset_busybox(128, impmessag);
+	strcpy(smurf_struct->smurf_pic->format_name, "Neochrome .NEO");
+	v1 = *(buffer + 2) * 256 + *(buffer + 3);	/* Bilddimensionen */
+	if (v1 == 0)
+	{
+		width = 320;
+		height = 200;
+		BitsPerPixel = 4;
+	} else if (v1 == 1)
+	{
+		width = 640;
+		height = 200;
+		BitsPerPixel = 2;
+	} else
+	{
+		width = 640;
+		height = 400;
+		BitsPerPixel = 1;
+	}
+	palet = (uint16_t *) (buffer + 4);
 
-paddr=Malloc((long)width*(long)height);
-if (!paddr) return(M_MEMORY);
-smpal=smurf_struct->smurf_pic->palette;
+	strcpy(impmessag, "Neochrome ");
+	strcat(impmessag, itoa(BitsPerPixel, mdummy, 10));
+	strcat(impmessag, " Bit");
+	smurf_struct->services->reset_busybox(128, impmessag);
 
-for (e=0; e<16; e++)
-{
-    v1=*(palet++);
+	paddr = (uint8_t *)Malloc((unsigned long) width * height);
+	if (!paddr)
+		return M_MEMORY;
+	smpal = smurf_struct->smurf_pic->palette;
 
-    r=v1 & 0x0f00;
-    g=v1 & 0x00f0;
-    b=v1 & 0x000f;
- 
-    *(smpal++)=(r>>4);
-    *(smpal++)=g;
-    *(smpal++)=(b<<4);
-}
+	for (e = 0; e < 16; e++)
+	{
+		v1 = *(palet++);
 
-opaddr=paddr;
-ibuf=(int *)(buffer+128);
+		r = v1 & 0x0f00;
+		g = v1 & 0x00f0;
+		b = v1 & 0x000f;
 
-for (y=0; y<height; y++) {
-    for (x=0; x<20; x++) {
-            merk_bytes1=*(ibuf++);
-            merk_bytes2=*(ibuf++);
-            merk_bytes3=*(ibuf++);
-            merk_bytes4=*(ibuf++);
+		*(smpal++) = (r >> 4);
+		*(smpal++) = g;
+		*(smpal++) = (b << 4);
+	}
 
-        for (f=0; f<16; f++)
-        {
-        dummy=0;
-        if (merk_bytes1 & mask_bytes[f]) dummy+=1;
-        if (merk_bytes2 & mask_bytes[f]) dummy+=2;
-        if (merk_bytes3 & mask_bytes[f]) dummy+=4;
-        if (merk_bytes4 & mask_bytes[f]) dummy+=8;
-        *(paddr++)=dummy;
-        }
-    }
-}   
-Mfree(smurf_struct->smurf_pic->pic_data);
-smurf_struct->smurf_pic->pic_data=opaddr;
-smurf_struct->smurf_pic->col_format=RGB;
-smurf_struct->smurf_pic->format_type=0;
-smurf_struct->smurf_pic->pic_width=width;
-smurf_struct->smurf_pic->pic_height=height;
-smurf_struct->smurf_pic->bp_pal=24;
-smurf_struct->smurf_pic->depth=BitsPerPixel;
-return(M_PICDONE);
+	opaddr = paddr;
+	ibuf = (uint16_t *) (buffer + 128);
+
+	for (y = 0; y < height; y++)
+	{
+		for (x = 0; x < 20; x++)
+		{
+			merk_bytes1 = *(ibuf++);
+			merk_bytes2 = *(ibuf++);
+			merk_bytes3 = *(ibuf++);
+			merk_bytes4 = *(ibuf++);
+
+			for (f = 0; f < 16; f++)
+			{
+				dummy = 0;
+				if (merk_bytes1 & mask_bytes[f])
+					dummy += 1;
+				if (merk_bytes2 & mask_bytes[f])
+					dummy += 2;
+				if (merk_bytes3 & mask_bytes[f])
+					dummy += 4;
+				if (merk_bytes4 & mask_bytes[f])
+					dummy += 8;
+				*(paddr++) = dummy;
+			}
+		}
+	}
+	Mfree(smurf_struct->smurf_pic->pic_data);
+	smurf_struct->smurf_pic->pic_data = opaddr;
+	smurf_struct->smurf_pic->col_format = RGB;
+	smurf_struct->smurf_pic->format_type = FORM_PIXELPAK;
+	smurf_struct->smurf_pic->pic_width = width;
+	smurf_struct->smurf_pic->pic_height = height;
+	smurf_struct->smurf_pic->bp_pal = 24;
+	smurf_struct->smurf_pic->depth = BitsPerPixel;
+	return M_PICDONE;
 }
