904fec7046a29b6045c9b570b9f1003920df0a56
Thorsten Otto <admin@tho-otto.de>
2022-12-06 10:03:10 +0100

    Cleanup import/pcp

--- modules/import/pcp.c
+++ modules/import/pcp.c
@@ -34,36 +34,37 @@
 #define B_PIX       0
 
 /* Infostruktur fr Hauptmodul */
-MOD_INFO module_info = {"Atari Grafik Format",
-                        0x0010,
-                        "Dale Russell",
-                        "PCP", "", "", "", "",
-                        "", "", "", "", "",
-                        "Slider 1",
-                        "Slider 2",
-                        "Slider 3",
-                        "Slider 4",
-                        "Checkbox 1",
-                        "Checkbox 2",
-                        "Checkbox 3",
-                        "Checkbox 4",
-                        "Edit 1",
-                        "Edit 2",
-                        "Edit 3",
-                        "Edit 4",
-                        0,128,
-                        0,128,
-                        0,128,
-                        0,128,
-                        0,10,
-                        0,10,
-                        0,10,
-                        0,10,
-                        0,0,0,0,
-                        0,0,0,0,
-                        0,0,0,0,
-                        0
-                        };
+MOD_INFO module_info = {
+	"Atari Grafik Format",
+	0x0010,
+	"Dale Russell",
+	{ "PCP", "", "", "", "", "", "", "", "", "" },
+	"Slider 1",
+	"Slider 2",
+	"Slider 3",
+	"Slider 4",
+	"Checkbox 1",
+	"Checkbox 2",
+	"Checkbox 3",
+	"Checkbox 4",
+	"Edit 1",
+	"Edit 2",
+	"Edit 3",
+	"Edit 4",
+	0, 128,
+	0, 128,
+	0, 128,
+	0, 128,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0,
+	NULL, NULL, NULL, NULL, NULL, NULL
+};
 
 /* -------------------------------------------------*/
 /* -------------------------------------------------*/
@@ -72,38 +73,41 @@ MOD_INFO module_info = {"Atari Grafik Format",
 /* -------------------------------------------------*/
 short imp_module_main(GARGAMEL *smurf_struct)
 {
-char *buffer=smurf_struct->smurf_pic->pic_data;
-int width=0, height=0;
-long len=0;
-char *filenam, *retbuf;
-char extend[4];
+	uint8_t *buffer = smurf_struct->smurf_pic->pic_data;
+	unsigned short width;
+	unsigned short height;
+	long len = 0;
+	char *filenam;
+	uint8_t *retbuf;
+	char extend[4];
 
-/*****************************************************/
-/*          MAGIC Code berprfen                    */
-/*****************************************************/
-filenam=smurf_struct->smurf_pic->filename;
-strncpy(extend, filenam+(strlen(filenam)-3), 3);
-if(strncmp(extend, "PCP", 3)!=0) 
-    return M_INVALID;
+	/*****************************************************/
+	/*          MAGIC Code berprfen                    */
+	/*****************************************************/
+	filenam = smurf_struct->smurf_pic->filename;
+	strcpy(extend, filenam + (strlen(filenam) - 3));
+	if (strnicmp(extend, "PCP", 3) != 0)
+		return M_INVALID;
 
-smurf_struct->services->reset_busybox(128, "Atari Grafik 1 Bit");
+	smurf_struct->services->reset_busybox(128, "Atari Grafik 1 Bit");
 
-width=(*(buffer)<<8)+*(buffer+1);
-height=(*(buffer+2)<<8)+*(buffer+3);
-len=(long)((width+7)/8)*(long)height;
-/*****************************************************/
-/*  Smurf-Strukturen fllen                          */
-/*****************************************************/
-strncpy(smurf_struct->smurf_pic->format_name, "Atari Grafik .PCP    ", 21);
-smurf_struct->smurf_pic->format_type=FORM_STANDARD;
-smurf_struct->smurf_pic->pic_width=width;
-smurf_struct->smurf_pic->pic_height=height;
-smurf_struct->smurf_pic->depth=1;
-smurf_struct->smurf_pic->col_format=WURSCHT;
-retbuf=Malloc(len);
-memcpy(retbuf, buffer+6, len);
-Mfree(smurf_struct->smurf_pic->pic_data);
-smurf_struct->smurf_pic->pic_data=retbuf;
+	width = (*(buffer) << 8) + *(buffer + 1);
+	height = (*(buffer + 2) << 8) + *(buffer + 3);
+	len = (unsigned long) ((width + 7) / 8) * height;
 
-return(M_PICDONE);
+	/*****************************************************/
+	/*  Smurf-Strukturen fllen                          */
+	/*****************************************************/
+	strcpy(smurf_struct->smurf_pic->format_name, "Atari Grafik .PCP");
+	smurf_struct->smurf_pic->format_type = FORM_STANDARD;
+	smurf_struct->smurf_pic->pic_width = width;
+	smurf_struct->smurf_pic->pic_height = height;
+	smurf_struct->smurf_pic->depth = 1;
+	smurf_struct->smurf_pic->col_format = WURSCHT;
+	retbuf = (uint8_t *)Malloc(len);
+	memcpy(retbuf, buffer + 6, len);
+	Mfree(smurf_struct->smurf_pic->pic_data);
+	smurf_struct->smurf_pic->pic_data = retbuf;
+
+	return M_PICDONE;
 }
