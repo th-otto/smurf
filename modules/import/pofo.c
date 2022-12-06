6816c6c24b457417b4508a7418f58fe41ea3d1b9
Thorsten Otto <admin@tho-otto.de>
2022-12-06 16:28:41 +0100

    Cleanup import/pofo

--- modules/import/pofo.c
+++ modules/import/pofo.c
@@ -27,7 +27,7 @@
 /*                  Umstellung auf Smurf Malloc/Free				*/
 /* Version 0.3  --  05.04.2003										*/
 /*                  Kann nun auch PGX-Dateien lesen. Es wird nur das*/
-/*									erste Bild geladen.				*/ 
+/*									erste Bild geladen.				*/
 /* Version 0.2  --  01.04.2003										*/
 /* Version 0.1  --  29.03.2003										*/
 /*																	*/
@@ -39,14 +39,17 @@
 #define ERROR1 "Falsche Gr”že (!=1920) fr ein PGF!"
 #define ERROR2 "Diese Portfolio PGX-Datei enth„lt keine|PGC Grafik."
 #define ERROR3 "Dies ist keine Portfolio PGX-Datei"
+#define ERROR4 "Dies ist keine Portfolio PGC-Datei"
 #elif COUNTRY==0
 #define ERROR1 "Wrong size (not=1920) for a PGF!"
 #define ERROR2 "This Portfolio PGX file doesn't hold|a PGC graphic!"
 #define ERROR3 "This is not a Portfolio PGX file!"
+#define ERROR4 "This is not a Portfolio PGC file!"
 #elif COUNTRY==2
 #define ERROR1 "Wrong size (not=1920) for a PGF!"
 #define ERROR2 "This Portfolio PGX file doesn't hold|a PGC graphic!"
 #define ERROR3 "This is not a Portfolio PGX file!"
+#define ERROR4 "This is not a Portfolio PGC file!"
 #else
 #error "Keine Sprache!"
 #endif
@@ -58,81 +61,91 @@
 #include "import.h"
 #include "smurfine.h"
 
-static void *(*SMalloc)(long amount);
-static void (*SMfree)(void *ptr);
-
-static WORD (*f_alert)(char *alertstring, char *b1, char *b2, char *b3, WORD defbt);
-
-static char *fileext(char *filename);
 
 
 /* Infostruktur fr Hauptmodul */
-MOD_INFO module_info = {"Portfolio PGF",
-						0x0030,
-						"Gerhard Stoll",
-						"PGF", "PGC", "PGX", "", "",
-						"", "", "", "", "",
-						"Slider 1",
-						"Slider 2",
-						"Slider 3",
-						"Slider 4",
-						"Checkbox 1",
-						"Checkbox 2",
-						"Checkbox 3",
-						"Checkbox 4",
-						"Edit 1",
-						"Edit 2",
-						"Edit 3",
-						"Edit 4",
-						0,128,
-						0,128,
-						0,128,
-						0,128,
-						0,10,
-						0,10,
-						0,10,
-						0,10,
-						0, 0, 0, 0,
-						0, 0, 0, 0,
-						0, 0, 0, 0,
-						0
-						};
+MOD_INFO module_info = {
+	"Portfolio PGF",
+	0x0030,
+	"Gerhard Stoll",
+	{ "PGF", "PGC", "PGX", "", "", "", "", "", "", "" },
+	"Slider 1",
+	"Slider 2",
+	"Slider 3",
+	"Slider 4",
+	"Checkbox 1",
+	"Checkbox 2",
+	"Checkbox 3",
+	"Checkbox 4",
+	"Edit 1",
+	"Edit 2",
+	"Edit 3",
+	"Edit 4",
+	0, 128,
+	0, 128,
+	0, 128,
+	0, 128,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0,
+	NULL, NULL, NULL, NULL, NULL, NULL
+};
 
+
+
+/*---------------------------------------------------------------------*/
+
+static char *fileext(const char *filename)
+{
+	char *extstart;
+
+	if ((extstart = strrchr(filename, '.')) != NULL)
+		extstart++;
+	else
+		extstart = strrchr(filename, '\0');
+
+	return extstart;
+}
 
 /* -------------------------------------------------*/
 /* -------------------------------------------------*/
 /*		PGF files Importer														*/
 /* -------------------------------------------------*/
 /* -------------------------------------------------*/
-short imp_module_main ( GARGAMEL *smurf_struct)
+short imp_module_main(GARGAMEL *smurf_struct)
 {
-	char *buffer, *pal, *fname, *ziel;
-	char index, data, BitsPerPixel;
+	uint8_t *buffer;
+	uint8_t *pal;
+	char *fname;
+	uint8_t *ziel;
+	uint8_t index;
+	uint8_t data;
+	uint8_t BitsPerPixel;
 
-	int i, k, dest;
-	unsigned int width, height;
+	short i, k, dest;
+	unsigned short width, height;
 	long file_len;
-
-	f_alert = smurf_struct->services->f_alert;
 
 	fname = smurf_struct->smurf_pic->filename;
 
-	if ( stricmp(fileext(fname), "PGF") != 0 && 
-	     stricmp(fileext(fname), "PGC") != 0 &&
-	     stricmp(fileext(fname), "PGX") != 0 )
-		return(M_INVALID);
+	if (stricmp(fileext(fname), "PGF") != 0 &&
+		stricmp(fileext(fname), "PGC") != 0 &&
+		stricmp(fileext(fname), "PGX") != 0)
+		return M_INVALID;
 
 	file_len = smurf_struct->smurf_pic->file_len;
 
-	if(stricmp(fileext(fname), "PGF") == 0 && file_len != 1920L)
+	if (stricmp(fileext(fname), "PGF") == 0 && file_len != 1920L)
 	{
-		f_alert( ERROR1 , "OK", NULL, NULL, 1);
-		return(M_PICERR);
+		smurf_struct->services->f_alert(ERROR1, "OK", NULL, NULL, 1);
+		return M_PICERR;
 	}
 
-	SMalloc = smurf_struct->services->SMalloc;
-	SMfree = smurf_struct->services->SMfree;
-	
 	buffer = smurf_struct->smurf_pic->pic_data;
 
 	width = 240;
@@ -140,7 +153,7 @@ short imp_module_main ( GARGAMEL *smurf_struct)
 
 	BitsPerPixel = 1;
 
-	strncpy(smurf_struct->smurf_pic->format_name, "Portfolio Graphics",18);
+	strcpy(smurf_struct->smurf_pic->format_name, "Portfolio Graphics");
 	smurf_struct->smurf_pic->pic_width = width;
 	smurf_struct->smurf_pic->pic_height = height;
 	smurf_struct->smurf_pic->depth = BitsPerPixel;
@@ -154,112 +167,97 @@ short imp_module_main ( GARGAMEL *smurf_struct)
 	pal[4] = 0;
 	pal[5] = 0;
 
-	if ( stricmp(fileext(fname), "PGC") == 0 )
+	if (stricmp(fileext(fname), "PGC") == 0)
 	{
-		if ( buffer[0] != 'P' || buffer[1] != 'G' || buffer[2] != 1)
+		if (buffer[0] != 'P' || buffer[1] != 'G' || buffer[2] != 1)
 		{
-			f_alert("Dies ist keine Portfolio PGC-Datei", "OK", NULL, NULL, 1);
-			return(M_PICERR);
+			smurf_struct->services->f_alert(ERROR4, "OK", NULL, NULL, 1);
+			return M_PICERR;
 		}
-		strncpy(smurf_struct->smurf_pic->format_name, "Portfolio Graphics Compressed",29);
+		strcpy(smurf_struct->smurf_pic->format_name, "Portfolio Graphics Compressed");
 
-		ziel = (char *)SMalloc( 1920L );
-		memset(ziel, 0x0, 1920L );
-	
-		i = 0;	
+		ziel = (uint8_t *) smurf_struct->services->SMalloc(1920L);
+		if (ziel == NULL)
+			return M_MEMORY;
+
+		i = 0;
 		dest = 0;
-		do 
+		do
 		{
-			index = buffer[i+3];
-			if ( (index & 0x80 ) != 0 )			/* High Bit ist gesetzt						*/
-			{																/* Gleiche Bytefolge							*/
-				index -= 0x80;								/* Anzahl der Bytes 							*/
+			index = buffer[i + 3];
+			if ((index & 0x80) != 0)	/* High Bit ist gesetzt                     */
+			{							/* Gleiche Bytefolge                            */
+				index -= 0x80;			/* Anzahl der Bytes                             */
 				i++;
-				data = buffer[i+3];
-				for ( k = 0; k < index; k++ )
-					ziel [dest++] = data;
+				data = buffer[i + 3];
+				for (k = 0; k < index; k++)
+					ziel[dest++] = data;
 				i++;
+			} else
+			{							/* Unterschiedliche Bytefolge           */
+				i++;
+				for (k = 0; k < index; k++, i++)
+					ziel[dest++] = buffer[i + 3];
 			}
-			else
-			{																/* Unterschiedliche Bytefolge			*/
-				i++;
-				for ( k = 0; k < index; k++, i++ )
-					ziel [dest++] = buffer[i+3];
-			} 
-		}while (dest < 1920 );
-		SMfree(buffer);
+		} while (dest < 1920);
+		smurf_struct->services->SMfree(buffer);
 
 		smurf_struct->smurf_pic->pic_data = ziel;
 	}
 
-	if ( stricmp(fileext(fname), "PGX") == 0 )
+	if (stricmp(fileext(fname), "PGX") == 0)
 	{
-		if ( buffer[0] != 'P' || buffer[1] != 'G' || buffer[2] != 'X')
+		if (buffer[0] != 'P' || buffer[1] != 'G' || buffer[2] != 'X')
 		{
-			f_alert( ERROR3 , "OK", NULL, NULL, 1);
-			return(M_PICERR);
+			smurf_struct->services->f_alert(ERROR3, "OK", NULL, NULL, 1);
+			return M_PICERR;
 		}
-		strncpy(smurf_struct->smurf_pic->format_name, "Portfolio Graphics, PGX",23);
+		strcpy(smurf_struct->smurf_pic->format_name, "Portfolio Graphics, PGX");
 
-		i = 8;														/* Start der Daten								*/
+		i = 8;							/* Start der Daten                              */
 		do
 		{
-			if ( buffer [i] == 0xff )				/* Ende erreicht?									*/
+			if (buffer[i] == 0xff)		/* Ende erreicht?                                   */
 			{
-				f_alert( ERROR2 , "OK", NULL, NULL, 1);
-				return(M_PICERR);
+				smurf_struct->services->f_alert(ERROR2, "OK", NULL, NULL, 1);
+				return M_PICERR;
 			}
-			if ( buffer [i] == 0x00 )				/* PGC-Datei											*/
+			if (buffer[i] == 0x00)		/* PGC-Datei                                            */
 			{
-				ziel = (char *)SMalloc( 1920L );
-				memset(ziel, 0x0, 1920L );
-			
-				i += 8;	
+				ziel = (uint8_t *) smurf_struct->services->SMalloc(1920L);
+				if (ziel == NULL)
+					return M_MEMORY;
+
+				i += 8;
 				dest = 0;
-				do 
+				do
 				{
 					index = buffer[i];
-					if ( (index & 0x80 ) != 0 )	/* High Bit ist gesetzt						*/
-					{														/* Gleiche Bytefolge							*/
-						index -= 0x80;						/* Anzahl der Bytes 							*/
+					if ((index & 0x80) != 0)	/* High Bit ist gesetzt                     */
+					{					/* Gleiche Bytefolge                            */
+						index -= 0x80;	/* Anzahl der Bytes                             */
 						i++;
 						data = buffer[i];
-						for ( k = 0; k < index; k++ )
-							ziel [dest++] = data;
+						for (k = 0; k < index; k++)
+							ziel[dest++] = data;
 						i++;
+					} else
+					{					/* Unterschiedliche Bytefolge           */
+						i++;
+						for (k = 0; k < index; k++, i++)
+							ziel[dest++] = buffer[i];
 					}
-					else
-					{														/* Unterschiedliche Bytefolge			*/
-						i++;
-						for ( k = 0; k < index; k++, i++ )
-							ziel [dest++] = buffer[i];
-					} 
-				}while (dest < 1920 );
-				SMfree(buffer);
-		
+				} while (dest < 1920);
+				smurf_struct->services->SMfree(buffer);
+
 				smurf_struct->smurf_pic->pic_data = ziel;
 				break;
 			}
-			i += 8 + buffer[i+1] + (buffer[i+2]  * 256);
-		}while ( i < file_len );
+			i += 8 + buffer[i + 1] + (buffer[i + 2] * 256);
+		} while (i < file_len);
 	}
 
 	smurf_struct->smurf_pic->format_type = FORM_STANDARD;
 
 	return M_PICDONE;
 }
-
-/*---------------------------------------------------------------------*/
-
-char *fileext(char *filename)
-{
-	char *extstart;
-
-
-	if((extstart = strrchr(filename, '.')) != NULL)
-		extstart++;
-	else
-		extstart = strrchr(filename, '\0');
-	
-	return(extstart);
-} /* fileext */
