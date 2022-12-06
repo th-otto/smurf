acef75f982015bf7e8e563bdb26aeb99c3d34e0d
Thorsten Otto <admin@tho-otto.de>
2022-12-06 17:17:43 +0100

    Cleanup import/ras

--- modules/import/ras.c
+++ modules/import/ras.c
@@ -58,40 +58,38 @@
 #include "import.h"
 #include "smurfine.h"
 
-static void *(*SMalloc)(long amount);
-static void (*SMfree)(void *ptr);
-
 /* Infostruktur fr Hauptmodul */
-MOD_INFO module_info = {"Sun Rasterfile",
-						0x0060,
-						"Christian Eyrich",
-						"RAS", "SUN", "", "", "",
-						"", "", "", "", "",
-						"Slider 1",
-						"Slider 2",
-						"Slider 3",
-						"Slider 4",
-						"Checkbox 1",
-						"Checkbox 2",
-						"Checkbox 3",
-						"Checkbox 4",
-						"Edit 1",
-						"Edit 2",
-						"Edit 3",
-						"Edit 4",
-						0,128,
-						0,128,
-						0,128,
-						0,128,
-						0,10,
-						0,10,
-						0,10,
-						0,10,
-						0, 0, 0, 0,
-						0, 0, 0, 0,
-						0, 0, 0, 0,
-						0
-						};
+MOD_INFO module_info = {
+	"Sun Rasterfile",
+	0x0060,
+	"Christian Eyrich",
+	{ "RAS", "SUN", "", "", "", "", "", "", "", "" },
+	"Slider 1",
+	"Slider 2",
+	"Slider 3",
+	"Slider 4",
+	"Checkbox 1",
+	"Checkbox 2",
+	"Checkbox 3",
+	"Checkbox 4",
+	"Edit 1",
+	"Edit 2",
+	"Edit 3",
+	"Edit 4",
+	0, 128,
+	0, 128,
+	0, 128,
+	0, 128,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0,
+	NULL, NULL, NULL, NULL, NULL, NULL
+};
 
 /* -------------------------------------------------*/
 /* -------------------------------------------------*/
@@ -102,197 +100,201 @@ MOD_INFO module_info = {"Sun Rasterfile",
 /* -------------------------------------------------*/
 short imp_module_main(GARGAMEL *smurf_struct)
 {
-	char *buffer, *obuffer, *ziel, *oziel, *pal, *ppal,
-		 BitsPerPixel, imgtype, v1, v2, colmaptype;
-	char dummy[3], impmessag[21];
+	uint8_t *buffer;
+	uint8_t *obuffer;
+	uint8_t *ziel;
+	uint8_t *oziel;
+	uint8_t *pal;
+	uint8_t *ppal;
+	uint8_t BitsPerPixel;
+	uint8_t imgtype;
+	uint8_t v1, v2;
+	uint8_t colmaptype;
+	char dummy[3];
+	char impmessag[21];
 
-	unsigned int x, y, i, n, DatenOffset, colmaplength, width, height;
+	unsigned short x, y, i, n;
+	unsigned short DatenOffset;
+	unsigned short colmaplength;
+	unsigned short width, height;
 
 	unsigned long w, length, written, flen;
 
 
-	SMalloc = smurf_struct->services->SMalloc;
-	SMfree = smurf_struct->services->SMfree;
-
 	buffer = smurf_struct->smurf_pic->pic_data;
 	obuffer = buffer;
 
-	if (*(unsigned long *)buffer != 0x59a66a95L)
-		return(M_INVALID);
+	if (*(uint32_t *) buffer != 0x59a66a95L)
+		return M_INVALID;
+
+	width = (unsigned int) *(unsigned long *) (buffer + 0x04);
+	height = (unsigned int) *(unsigned long *) (buffer + 0x08);
+
+	BitsPerPixel = *(unsigned long *) (buffer + 0x0c);
+
+	imgtype = (char) *(unsigned long *) (buffer + 0x14);
+	if (imgtype == 0xff)
+		return M_UNKNOWN_TYPE;
+
+	/* Scanline auf ein Vielfaches von 16 Bit alignen wenn komprimiert! */
+	if (imgtype == 0x02)
+		width = (width + 1) / 2 * 2;
+
+	colmaptype = *(uint32_t *) (buffer + 0x18);
+
+	colmaplength = *(uint32_t *) (buffer + 0x1c);
+
+	DatenOffset = colmaplength + 0x20;
+
+	strcpy(smurf_struct->smurf_pic->format_name, "Sun-Rasterfile .RAS, .SUN");
+	smurf_struct->smurf_pic->pic_width = width;
+	smurf_struct->smurf_pic->pic_height = height;
+	smurf_struct->smurf_pic->depth = BitsPerPixel;
+
+	strcpy(impmessag, "Sun Raster ");
+	strcat(impmessag, itoa(BitsPerPixel, dummy, 10));
+	strcat(impmessag, " Bit");
+	smurf_struct->services->reset_busybox(128, impmessag);
+
+	if (BitsPerPixel == 1)
+		w = (width + 7) / 8;
 	else
+		w = ((unsigned long) width * BitsPerPixel) >> 3;
+
+	if ((ziel = smurf_struct->services->SMalloc(w * height)) == 0)
+		return M_MEMORY;
+
+	oziel = ziel;
+
+	buffer += DatenOffset;
+
+	flen = smurf_struct->smurf_pic->file_len - DatenOffset;
+
+	switch (imgtype)
 	{
-		width = (unsigned int)*(unsigned long *)(buffer + 0x04); 
-		height = (unsigned int)*(unsigned long *)(buffer + 0x08);
-
-		BitsPerPixel = *(unsigned long *)(buffer + 0x0c);
-
-		imgtype = (char)*(unsigned long *)(buffer + 0x14);
-		if(imgtype == 0xff)
-			return(M_UNKNOWN_TYPE);
-
-		/* Scanline auf ein Vielfaches von 16 Bit alignen wenn komprimiert! */
-		if(imgtype == 0x02)
-			width = (width + 1) / 2 * 2;
-
-		colmaptype = (char)*(unsigned long *)(buffer + 0x18);
-
-		colmaplength = (unsigned int)*(unsigned long *)(buffer + 0x1c);
-
-		DatenOffset = colmaplength + 0x20;
-
-		strncpy(smurf_struct->smurf_pic->format_name, "Sun-Rasterfile .RAS, .SUN", 21);
-		smurf_struct->smurf_pic->pic_width = width;
-		smurf_struct->smurf_pic->pic_height = height;
-		smurf_struct->smurf_pic->depth = BitsPerPixel;
-
-		strcpy(impmessag, "Sun Raster ");
-		strcat(impmessag, itoa(BitsPerPixel, dummy, 10));
-		strcat(impmessag, " Bit");
-		smurf_struct->services->reset_busybox(128, impmessag);
-
-		if(BitsPerPixel == 1)
-			w = (width + 7) / 8;
-		else
-			w = ((long)width * (long)BitsPerPixel) >> 3;
-
-		if((ziel = SMalloc(w * (long)height)) == 0)
-			return(M_MEMORY);
-		else
+	case 2:
+		length = w * (long) height;
+		written = 0;
+		while (flen && written < length)	/* Byte-encoded */
 		{
-			oziel = ziel;
-
-			buffer += DatenOffset;
-
-			flen = smurf_struct->smurf_pic->file_len - DatenOffset;
-
-			switch(imgtype)
+			if ((v1 = *buffer++) != 0x80)
 			{
-				case 2:	length = w * (long)height;
-						written = 0;
-						while(flen && written < length)				/* Byte-encoded */
-						{
-							if((v1 = *buffer++) != 0x80)
-							{
-								*ziel++ = v1;
-								written++;
-								flen--;
-							}
-							else
-								if((n = *buffer++) == 0x00)
-								{
-									*ziel++ = 0x80;
-									written++;
-									flen -= 2;
-								}
-								else
-								{
-									v2 = *buffer++;
-									n++;
-									written += n;
-									while(n--)
-										*ziel++ = v2;
-									flen -= 3;
-								}
-						}
-
-						break;
-				default: /* Old, Standard, RGB, TIFF, IFF */
-						 if(BitsPerPixel == 32)
-						 {
-							 /* Alphachannel rausoperieren */
-							 y = 0;
-							 do
-							 {
-								 x = 0;
-								 do
-								 {
-									 buffer++;
-									 *ziel++ = *buffer++;
-									 *ziel++ = *buffer++;
-									 *ziel++ = *buffer++;
-								 } while(++x < width);
-							 } while(++y < height);
-						 }
-						 else
-						 {
-							 y = 0;
-							 do
-							 {
-								 memcpy(ziel, buffer, w);
-								 buffer += w;
-								 ziel += w;
-							 } while(++y < height);
-						 }
-						 break;
-			} /* imagetype */
-
-			buffer = obuffer;
-			ziel = oziel;
-
-	
-			if(BitsPerPixel <= 8)
+				*ziel++ = v1;
+				written++;
+				flen--;
+			} else if ((n = *buffer++) == 0x00)
 			{
-				pal = smurf_struct->smurf_pic->palette;
-
-				switch(colmaptype)
-				{
-					/* siehe auch xvsunras */
-					case 0: if(BitsPerPixel == 1)
-							{
-								pal[0] = 0;
-								pal[1] = 0;
-								pal[2] = 0;
-								pal[3] = 255;
-								pal[4] = 255;
-								pal[5] = 255;
-							}
-							else
-							{
-								for(i = 0; i < 256; i++)
-								{
-									*pal++ = (char)i;
-									*pal++ = (char)i;
-									*pal++ = (char)i;
-								}
-							}
-							break;
-					case 1:	ppal = buffer + 0x20;
-							colmaplength /= 3;
-							for(i = 0; i < colmaplength; i++)
-							{
-								*pal++ = *ppal;
-								*pal++ = *(ppal + colmaplength);
-								*pal++ = *(ppal + colmaplength + colmaplength);
-								ppal++;
-							}
-							break;
-	 				case 2:	ppal = buffer + 0x20;
-							colmaplength /= 3;
-							for(i = 0; i < colmaplength; i++)
-							{
-								*pal++ = *ppal++;
-								*pal++ = *ppal++;
-								*pal++ = *ppal++;
-							}
-							break;
-				} /* Palette */
+				*ziel++ = 0x80;
+				written++;
+				flen -= 2;
+			} else
+			{
+				v2 = *buffer++;
+				n++;
+				written += n;
+				while (n--)
+					*ziel++ = v2;
+				flen -= 3;
 			}
+		}
+		break;
 
-			if(BitsPerPixel == 1)
-				smurf_struct->smurf_pic->format_type = FORM_STANDARD;
-			else
-				smurf_struct->smurf_pic->format_type = FORM_PIXELPAK;
+	default:					/* Old, Standard, RGB, TIFF, IFF */
+		if (BitsPerPixel == 32)
+		{
+			/* Alphachannel rausoperieren */
+			y = 0;
+			do
+			{
+				x = 0;
+				do
+				{
+					buffer++;
+					*ziel++ = *buffer++;
+					*ziel++ = *buffer++;
+					*ziel++ = *buffer++;
+				} while (++x < width);
+			} while (++y < height);
+		} else
+		{
+			y = 0;
+			do
+			{
+				memcpy(ziel, buffer, w);
+				buffer += w;
+				ziel += w;
+			} while (++y < height);
+		}
+		break;
+	}							/* imagetype */
 
-			if(imgtype != 3 && BitsPerPixel >= 24)
-				smurf_struct->smurf_pic->col_format = BGR;
-			else
-				smurf_struct->smurf_pic->col_format = RGB;
+	buffer = obuffer;
+	ziel = oziel;
 
-			smurf_struct->smurf_pic->pic_data = ziel;
-		} /* Malloc */
-	} /* Erkennung */
 
-	SMfree(buffer);
+	if (BitsPerPixel <= 8)
+	{
+		pal = smurf_struct->smurf_pic->palette;
 
-	return(M_PICDONE);
+		switch (colmaptype)
+		{
+			/* siehe auch xvsunras */
+		case 0:
+			if (BitsPerPixel == 1)
+			{
+				pal[0] = 0;
+				pal[1] = 0;
+				pal[2] = 0;
+				pal[3] = 255;
+				pal[4] = 255;
+				pal[5] = 255;
+			} else
+			{
+				for (i = 0; i < 256; i++)
+				{
+					*pal++ = (char) i;
+					*pal++ = (char) i;
+					*pal++ = (char) i;
+				}
+			}
+			break;
+		case 1:
+			ppal = buffer + 0x20;
+			colmaplength /= 3;
+			for (i = 0; i < colmaplength; i++)
+			{
+				*pal++ = *ppal;
+				*pal++ = *(ppal + colmaplength);
+				*pal++ = *(ppal + colmaplength + colmaplength);
+				ppal++;
+			}
+			break;
+		case 2:
+			ppal = buffer + 0x20;
+			colmaplength /= 3;
+			for (i = 0; i < colmaplength; i++)
+			{
+				*pal++ = *ppal++;
+				*pal++ = *ppal++;
+				*pal++ = *ppal++;
+			}
+			break;
+		}						/* Palette */
+	}
+
+	if (BitsPerPixel == 1)
+		smurf_struct->smurf_pic->format_type = FORM_STANDARD;
+	else
+		smurf_struct->smurf_pic->format_type = FORM_PIXELPAK;
+
+	if (imgtype != 3 && BitsPerPixel >= 24)
+		smurf_struct->smurf_pic->col_format = BGR;
+	else
+		smurf_struct->smurf_pic->col_format = RGB;
+
+	smurf_struct->smurf_pic->pic_data = ziel;
+
+	smurf_struct->services->SMfree(buffer);
+
+	return M_PICDONE;
 }
