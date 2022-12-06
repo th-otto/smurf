c44e246c2813b2b7b586a8c7f25c51a7c141573c
Thorsten Otto <admin@tho-otto.de>
2022-12-06 16:55:38 +0100

    Cleanup improt/psd

--- modules/import/psd.c
+++ modules/import/psd.c
@@ -51,40 +51,38 @@
 #include "import.h"
 #include "smurfine.h"
 
-static void *(*SMalloc)(long amount);
-static void (*SMfree)(void *ptr);
-
 /* Infostruktur fr Hauptmodul */
-MOD_INFO module_info = {"Photoshop 3.0",
-						0x0030,
-						"Christian Eyrich",
-						"PSD", "", "", "", "",
-						"", "", "", "", "",
-						"Slider 1",
-						"Slider 2",
-						"Slider 3",
-						"Slider 4",
-						"Checkbox 1",
-						"Checkbox 2",
-						"Checkbox 3",
-						"Checkbox 4",
-						"Edit 1",
-						"Edit 2",
-						"Edit 3",
-						"Edit 4",
-						0,128,
-						0,128,
-						0,128,
-						0,128,
-						0,10,
-						0,10,
-						0,10,
-						0,10,
-						0, 0, 0, 0,
-						0, 0, 0, 0,
-						0, 0, 0, 0,
-						0
-						};
+MOD_INFO module_info = {
+	"Photoshop 3.0",
+	0x0030,
+	"Christian Eyrich",
+	{ "PSD", "", "", "", "", "", "", "", "", "" },
+	"Slider 1",
+	"Slider 2",
+	"Slider 3",
+	"Slider 4",
+	"Checkbox 1",
+	"Checkbox 2",
+	"Checkbox 3",
+	"Checkbox 4",
+	"Edit 1",
+	"Edit 2",
+	"Edit 3",
+	"Edit 4",
+	0, 128,
+	0, 128,
+	0, 128,
+	0, 128,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0,
+	NULL, NULL, NULL, NULL, NULL, NULL
+};
 
 /* -------------------------------------------------*/
 /* -------------------------------------------------*/
@@ -95,194 +93,203 @@ MOD_INFO module_info = {"Photoshop 3.0",
 /* -------------------------------------------------*/
 short imp_module_main(GARGAMEL *smurf_struct)
 {
-	char v1, v2;
-	char *buffer, *obuffer, *ziel, *oziel, *pal, *ppal, 
-		 n, p, comp, BitsPerPixel, Channels, Planes, ColMode;
-	char dummy[3], impmessag[21];
-	
-	unsigned int i, x, y, w, width, height, cols;
-
+	uint8_t v1, v2;
+	uint8_t *buffer;
+	uint8_t *obuffer;
+	uint8_t *ziel;
+	uint8_t *oziel;
+	uint8_t *pal;
+	uint8_t *ppal;
+	uint8_t n;
+	uint8_t p;
+	uint8_t comp;
+	uint8_t BitsPerPixel;
+	uint8_t Channels;
+	uint8_t Planes;
+	uint8_t ColMode;
+	char dummy[3];
+	char impmessag[21];
+	unsigned short i, x, y, w, width, height, cols;
 	unsigned long length;
-
-	SMalloc = smurf_struct->services->SMalloc;
-	SMfree = smurf_struct->services->SMfree;
 
 	buffer = smurf_struct->smurf_pic->pic_data;
 	obuffer = buffer;
 
-	if(*(unsigned long *)buffer != '8BPS')
-		return(M_INVALID);
-	else
-	{
-		if(*(unsigned int *)(buffer + 4) != 1)
-		{
-			form_alert(0, ERROR1 );
-			return(M_PICERR);
-		}	
+	if (*(uint32_t *) buffer != 0x38325053L) /* '8BPS' */
+		return M_INVALID;
 
-		Channels = Planes = (char)*(unsigned int *)(buffer + 0x0c);
-		if(Planes > 3)
-			Planes = 3;
+	if (*(uint16_t *) (buffer + 4) != 1)
+	{
+		form_alert(1, ERROR1);
+		return M_PICERR;
+	}
+
+	Channels = Planes = *(uint16_t *) (buffer + 0x0c);
+	if (Planes > 3)
+		Planes = 3;
 
 	/* BitsPerPixel  = BitsPerChannel * Planes */
-		BitsPerPixel = (char)(*(unsigned int *)(buffer + 0x016) * Planes);
+	BitsPerPixel = (*(uint16_t *) (buffer + 0x016) * Planes);
 
-		height = (unsigned int)*(unsigned long *)(buffer + 0x0e); 
-		width = (unsigned int)*(unsigned long *)(buffer + 0x12);
+	height = (unsigned short) *(uint32_t *) (buffer + 0x0e);
+	width = (unsigned short) *(uint32_t *) (buffer + 0x12);
 
-		/* 0 = Bitmap, 1 = Grayscale, 2 = indexed, 3 = RGB */
-		/* 4 = CMYK, 7 = Multichannel, 8 = Duotone, 9 = Lab */
-		ColMode = (char)*(unsigned int *)(buffer + 0x18);
+	/* 0 = Bitmap, 1 = Grayscale, 2 = indexed, 3 = RGB */
+	/* 4 = CMYK, 7 = Multichannel, 8 = Duotone, 9 = Lab */
+	ColMode = *(uint16_t *) (buffer + 0x18);
 
-		buffer += 0x1a;
+	buffer += 0x1a;
 	/* Color Mode Data */
-		length = *((unsigned long *)buffer)++;
-		if(ColMode == 2)									/* Palettenbild */
-			cols = (unsigned int) (length / 3);
-		else
-			if(ColMode == 1 || ColMode == 8)				/* Graustufen oder Duotone */
-				cols = 1 << BitsPerPixel;
-			else
-				cols = 0;
-		ppal = buffer + 4;
-		buffer += length;									/* Block berspringen */
+	length = *((uint32_t *) buffer);
+	buffer += 4;
+	if (ColMode == 2)				/* Palettenbild */
+		cols = (unsigned short) (length / 3);
+	else if (ColMode == 1 || ColMode == 8)	/* Graustufen oder Duotone */
+		cols = 1 << BitsPerPixel;
+	else
+		cols = 0;
+	ppal = buffer + 4;
+	buffer += length;				/* Block berspringen */
 
 	/* Image Resources Block */
-		length = *((unsigned long *)buffer)++;
-		buffer += length;									/* Block berspringen */
+	length = *((uint32_t *) buffer);
+	buffer += 4;
+	buffer += length;				/* Block berspringen */
 
 	/* Layer and Mask Information Block */
-		length = *((unsigned long *)buffer)++;
-		buffer += length;									/* Block berspringen */
+	length = *((uint32_t *) buffer);
+	buffer += 4;
+	buffer += length;				/* Block berspringen */
 
 	/* Image Data Block */
-		comp = (char)*((unsigned int *)buffer)++;
-		if(comp)
-			buffer += (long)Channels * (long)height * 2L;	/* L„ngentabelle berspringen */
+	comp = *((uint16_t *) buffer);
+	buffer += 2;
+	if (comp)
+		buffer += (unsigned long) Channels * height * 2;	/* L„ngentabelle berspringen */
 
-		strncpy(smurf_struct->smurf_pic->format_name, "Photoshop 2.5/3.0 .PSD", 21);
-		smurf_struct->smurf_pic->pic_width = width;
-		smurf_struct->smurf_pic->pic_height = height;
-		smurf_struct->smurf_pic->depth = BitsPerPixel;
+	strcpy(smurf_struct->smurf_pic->format_name, "Photoshop 2.5/3.0 .PSD");
+	smurf_struct->smurf_pic->pic_width = width;
+	smurf_struct->smurf_pic->pic_height = height;
+	smurf_struct->smurf_pic->depth = BitsPerPixel;
 
-		strcpy(impmessag, "Photoshop ");
-		strcat(impmessag, itoa(BitsPerPixel, dummy, 10));
-		strcat(impmessag, " Bit");
-		smurf_struct->services->reset_busybox(128, impmessag);
+	strcpy(impmessag, "Photoshop ");
+	strcat(impmessag, itoa(BitsPerPixel, dummy, 10));
+	strcat(impmessag, " Bit");
+	smurf_struct->services->reset_busybox(128, impmessag);
 
-		if((ziel = SMalloc(((long)width * (long)height * BitsPerPixel) >> 3)) == 0)
-			return(M_MEMORY);
-		else
+	if ((ziel = smurf_struct->services->SMalloc(((long) width * (long) height * BitsPerPixel) >> 3)) == 0)
+		return M_MEMORY;
+
+	oziel = ziel;
+
+	if (BitsPerPixel == 1)
+		w = (width + 7) / 8;
+	else
+		w = width;
+
+	for (p = 0; p < Planes; p++)	/* Planes */
+	{
+		ziel = oziel + p;
+
+		y = 0;
+		do
 		{
-			oziel = ziel;
-
-			if(BitsPerPixel == 1)
-				w = (width + 7) / 8;
-			else
-				w = width;
-			
-			for(p = 0; p < Planes; p++) /* Planes */
+			x = 0;
+			do
 			{
-				ziel = oziel + p;
-
-				y = 0;
-				do
+				if (!comp)
 				{
-					x = 0;
-					do
+					*ziel = *buffer++;
+					ziel += Planes;
+					x++;
+				} else
+				{
+					v1 = *buffer++;
+#if 0
+					/* -128 kommt lt. Doku nie vor */
+					if (v1 != -128)
+#endif
 					{
-						if(!comp)
+						if (v1 > 0x7f)
 						{
-							*ziel = *buffer++;
-							ziel += Planes;
-							x++;
-						}
-						else
+							n = 0x101 - v1;
+
+							v2 = *buffer++;
+
+							x += n;
+
+							while (n--)
+							{
+								*ziel = v2;
+								ziel += Planes;
+							}
+						} else
 						{
-							v1 = *buffer++;
-/*	/* -128 kommt lt. Doku nie vor */
-							if(v1 != -128)
-							{ */
+							n = v1 + 1;
 
-								if(v1 > 0x7f)
-								{
-									n = 0x101 - v1;
+							x += n;
 
-									v2 = *buffer++;
-
-									x += n;
-
-									while(n--)
-									{
-										*ziel = v2;
-										ziel += Planes;
-									}
-								}
-								else
-								{
-									n = v1 + 1;
-
-									x += n;
-
-									while(n--)
-									{
-										*ziel = *buffer++;
-										ziel += Planes;
-									}
-								}
-/*							} /* -128? */ */
-						} /* comp? */
-					} while(x < w); /* x */
-					y++;
-				} while(y < height); /* y */
-			} /* p */
-
-			buffer = obuffer;
-			ziel = oziel;
-
-			smurf_struct->smurf_pic->pic_data = ziel;
-
-			if(BitsPerPixel == 1)
-				smurf_struct->smurf_pic->format_type = FORM_STANDARD;
-			else
-				smurf_struct->smurf_pic->format_type = FORM_PIXELPAK;
-
-			pal = smurf_struct->smurf_pic->palette;
-			switch(ColMode)
-			{
-				/* Bitmap */
-				case 0:	pal[0] = 255;
-						pal[1] = 255;
-						pal[2] = 255;
-						pal[3] = 0;
-						pal[4] = 0;
-						pal[5] = 0;
-						break;
-				/* Grayscale und Duotone */
-				case 1:
-				case 8:	for(i = 0; i < cols; i++)
-						{	
-							*pal++ = (char)i;
-							*pal++ = (char)i;
-							*pal++ = (char)i;
+							while (n--)
+							{
+								*ziel = *buffer++;
+								ziel += Planes;
+							}
 						}
-						break;
-				/* Indexed */
-				case 2:	for(i = 0; i < cols; i++)
-						{
-							*pal++ = *ppal;
-							*pal++ = *(ppal + cols);
-							*pal++ = *(ppal + cols + cols);
-							ppal++;
-						}
-						break;
-				default: break;
-			}
+					}
+				}
+			} while (x < w);
+			y++;
+		} while (y < height);
+	}
 
-			smurf_struct->smurf_pic->col_format = RGB;
-		} /* Malloc */
-	} /* Erkennung */
+	buffer = obuffer;
+	ziel = oziel;
 
-	SMfree(buffer);
-	return(M_PICDONE);
+	smurf_struct->smurf_pic->pic_data = ziel;
+
+	if (BitsPerPixel == 1)
+		smurf_struct->smurf_pic->format_type = FORM_STANDARD;
+	else
+		smurf_struct->smurf_pic->format_type = FORM_PIXELPAK;
+
+	pal = smurf_struct->smurf_pic->palette;
+	switch (ColMode)
+	{
+		/* Bitmap */
+	case 0:
+		pal[0] = 255;
+		pal[1] = 255;
+		pal[2] = 255;
+		pal[3] = 0;
+		pal[4] = 0;
+		pal[5] = 0;
+		break;
+		/* Grayscale und Duotone */
+	case 1:
+	case 8:
+		for (i = 0; i < cols; i++)
+		{
+			*pal++ = i;
+			*pal++ = i;
+			*pal++ = i;
+		}
+		break;
+		/* Indexed */
+	case 2:
+		for (i = 0; i < cols; i++)
+		{
+			*pal++ = *ppal;
+			*pal++ = *(ppal + cols);
+			*pal++ = *(ppal + cols + cols);
+			ppal++;
+		}
+		break;
+	default:
+		break;
+	}
+
+	smurf_struct->smurf_pic->col_format = RGB;
+
+	smurf_struct->services->SMfree(buffer);
+	return M_PICDONE;
 }
