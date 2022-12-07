7e18e8b414460da80d397c4e8c527f7fa9075a24
Thorsten Otto <admin@tho-otto.de>
2022-12-07 07:30:38 +0100

    Cleanup import/sgi

--- modules/import/sgi.c
+++ modules/import/sgi.c
@@ -60,180 +60,57 @@
 #include "import.h"
 #include "smurfine.h"
 
-static void *(*SMalloc)(long amount);
-static void (*SMfree)(void *ptr);
-
-void decode_1bpc(char *ziel, char *buffer, unsigned int width, unsigned int height, unsigned long *table, char Planes, char comp);
-void decode_2bpc(char *ziel, unsigned int *buffer, unsigned int width, unsigned int height, unsigned long *table, char Planes, char comp);
-void make_pal(char *buffer, char *pal, char BitsPerPixel, unsigned long length, char ColorMap);
-int switch_orient(char *buffer, unsigned int width, unsigned int height, char BitsPerPixel);
-void nulltospace(char *string, char length);
-
 /* Infostruktur fÅr Hauptmodul */
-MOD_INFO module_info = {"SGI-Format",
-						0x0040,
-						"Christian Eyrich",
-						"SGI", "RLE", "BW", "RGB", "RGBA",
-						"", "", "", "", "",
-						"Slider 1",
-						"Slider 2",
-						"Slider 3",
-						"Slider 4",
-						"Checkbox 1",
-						"Checkbox 2",
-						"Checkbox 3",
-						"Checkbox 4",
-						"Edit 1",
-						"Edit 2",
-						"Edit 3",
-						"Edit 4",
-						0,128,
-						0,128,
-						0,128,
-						0,128,
-						0,10,
-						0,10,
-						0,10,
-						0,10,
-						0, 0, 0, 0,
-						0, 0, 0, 0,
-						0, 0, 0, 0,
-						0
-						};
-
-/* -------------------------------------------------*/
-/* -------------------------------------------------*/
-/*      SGI - Silicon Graphic-Format (SGI)          */
-/*		8, 24 Bit (in 3 Ebenen),					*/
-/*		ohne Komprimierung/RLE						*/
-/* -------------------------------------------------*/
-/* -------------------------------------------------*/
-short imp_module_main(GARGAMEL *smurf_struct)
-{
-	char *buffer, *ziel,
-		 comp, Planes, BitsPerPixel, ColorMap, Bpc;
-	char dummy[3], impmessag[21];
-	
-	unsigned int width, height;
-
-	unsigned long *table,
-				  tablen;
+MOD_INFO module_info = {
+	"SGI-Format",
+	0x0040,
+	"Christian Eyrich",
+	{ "SGI", "RLE", "BW", "RGB", "RGBA", "", "", "", "", "" },
+	"Slider 1",
+	"Slider 2",
+	"Slider 3",
+	"Slider 4",
+	"Checkbox 1",
+	"Checkbox 2",
+	"Checkbox 3",
+	"Checkbox 4",
+	"Edit 1",
+	"Edit 2",
+	"Edit 3",
+	"Edit 4",
+	0, 128,
+	0, 128,
+	0, 128,
+	0, 128,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0,
+	NULL, NULL, NULL, NULL, NULL, NULL
+};
 
 
-	SMalloc = smurf_struct->services->SMalloc;
-	SMfree = smurf_struct->services->SMfree;
-
-	buffer = smurf_struct->smurf_pic->pic_data;
-
-	if(*(unsigned int *)buffer != 0x01da)
-		return(M_INVALID);
-	else
-	{
-		comp = *(buffer + 0x02);
-
-		Bpc = *(buffer + 0x03);
-
-#if 0
-		/* unwichtig */
-		Dim = (char)*(unsigned int *)(buffer + 0x04);
-#endif
-
-		width = *(unsigned int *)(buffer + 0x06); 
-		height = *(unsigned int *)(buffer + 0x08);
-
-		/* 1 = BW, dithered, colormapped; 3 = RGB; 4 = RGB + alpha channel */
-		Planes = (char)*(unsigned int *)(buffer + 0x0a);
-		if(Planes > 3)						/* sonst wÑren das ja ZustÑnde */
-			Planes = 3;
-
-		/* 0x00 - Normal pixel values: 1 Channel Greyscale, 3 Channels RGB */
-		/* 0x01 - Dithered Images: (8 BPP 3+3+2) */
-		/* 0x02 - single Channel Images */
-		/* 0x03 - color Map */
-		ColorMap = *(unsigned long *)(buffer + 0x6c);
-		if(ColorMap > 3)
-			return(M_UNKNOWN_TYPE);
-
-		BitsPerPixel = Planes * 8;
-
-
-		nulltospace(buffer + 0x18, 80);
-		strcpy(smurf_struct->smurf_pic->infotext, buffer + 0x18);
-		strncpy(smurf_struct->smurf_pic->format_name, "SGI-Format .SGI, .RLE", 21);
-		smurf_struct->smurf_pic->pic_width = width;
-		smurf_struct->smurf_pic->pic_height = height;
-		smurf_struct->smurf_pic->depth = BitsPerPixel;
-
-		strcpy(impmessag, "SGI ");
-		strcat(impmessag, itoa(BitsPerPixel, dummy, 10));
-		strcat(impmessag, " Bit");
-		smurf_struct->services->reset_busybox(128, impmessag);
-
-		if((ziel = (char *)SMalloc(((long)width * (long)height * BitsPerPixel) >> 3)) == 0)
-			return(M_MEMORY);
-		else
-		{
-			if(comp)
-			{
-				tablen = (long)height * (long)Planes * 4 * 2;
-
-				if((table = (unsigned long *)Malloc(tablen)) == 0)
-				{
-					SMfree(ziel);
-					return(M_MEMORY);
-				}
-				else
-					memcpy((char *)table, buffer + 0x200, tablen);
-			}
-			else
-				table = 0;
-
-			if(Bpc == 1)
-				decode_1bpc(ziel, buffer, width, height, table, Planes, comp);
-			else
-				decode_2bpc(ziel, (unsigned int *)buffer, width, height, table, Planes, comp);
-
-			Mfree(table);
-
-			if(!comp)
-				if(switch_orient(ziel, width, height, BitsPerPixel) != 0)
-					form_alert(1, ERROR );
-			
-			if(BitsPerPixel <= 8)
-				make_pal(ziel, smurf_struct->smurf_pic->palette, BitsPerPixel, (long)width * (long)height, ColorMap);
-
-			smurf_struct->smurf_pic->pic_data = ziel;
-
-			smurf_struct->smurf_pic->format_type = FORM_PIXELPAK;
-
-			if(BitsPerPixel == 8)
-				smurf_struct->smurf_pic->col_format = GREY;
-			else
-				smurf_struct->smurf_pic->col_format = RGB;
-		
-		} /* Malloc */
-	} /* Erkennung */
-
-	SMfree(buffer);
-	return(M_PICDONE);
-}
 
 
 /* Dekodiert Bilder mit einem und drei Bytes pro Pixel. */
-void decode_1bpc(char *ziel, char *buffer, unsigned int width, unsigned int height, unsigned long *table, char Planes, char comp)
+static void decode_1bpc(uint8_t *ziel, uint8_t *buffer, unsigned short width, unsigned short height, uint32_t *table, uint8_t Planes, uint8_t comp)
 {
-	char *oziel, *obuffer,
-		 p, n, v1, v2;
-
-	unsigned int x, y;
-
+	uint8_t *oziel;
+	uint8_t *obuffer;
+	uint8_t p;
+	uint8_t n;
+	uint8_t v1, v2;
+	unsigned short x, y;
 	unsigned long tablen3;
-
 
 	obuffer = buffer;
 	oziel = ziel;
 
-	buffer += 0x200;	
+	buffer += 0x200;
 
 	p = 0;
 	do
@@ -243,73 +120,69 @@ void decode_1bpc(char *ziel, char *buffer, unsigned int width, unsigned int heig
 		ziel = oziel + p;
 
 		y = height;
-		while(y--)				/* y absteigend, damit die Tabelle von unten her gelesen wird - */
-		{						/* so wird ein flip bei komprimierten Bildern ÅberflÅssig */
-			if(comp)
+		while (y--)						/* y absteigend, damit die Tabelle von unten her gelesen wird - */
+		{								/* so wird ein flip bei komprimierten Bildern ÅberflÅssig */
+			if (comp)
 				buffer = obuffer + table[tablen3 + y];
 
 			x = 0;
-			do /* width */
+			do							/* width */
 			{
-				if(!comp)
+				if (!comp)
 				{
 					x++;
 					*ziel = *buffer++;
 					ziel += Planes;
-				}
-				else
+				} else
 				{
 					v1 = *buffer++;
-					if((n = v1 & 0x7f) == 0)
+					if ((n = v1 & 0x7f) == 0)
 						return;
 
-					if(v1 & 0x80)
+					if (v1 & 0x80)
 					{
 						x += n;
 
-						while(n--)
+						while (n--)
 						{
 							*ziel = *buffer++;
 							ziel += Planes;
 						}
-					}
-					else
+					} else
 					{
 						v2 = *buffer++;
 
 						x += n;
-						while(n--)
+						while (n--)
 						{
 							*ziel = v2;
 							ziel += Planes;
 						}
 					}
-				} /* comp? */
-			} while(x < width); /* x */
-		} /* y */
-	} while(++p < Planes);
-
-	return;
-} /* decode_1bpc */
+				}
+			} while (x < width);
+		}
+	} while (++p < Planes);
+}
 
 
 /* Dekodiert Bilder mit 3 mal zwei Bytes pro Kanal. */
 /* Das sind Bilder, bei denen jeder RGB-Teil mit 16 Bit Genauigkeit vorliegt. */
 /* Die unteren 8 Bit shifte ich hier einfach weg. */
-void decode_2bpc(char *ziel, unsigned int *buffer, unsigned int width, unsigned int height, unsigned long *table, char Planes, char comp)
+static void decode_2bpc(uint8_t *ziel, uint16_t *buffer, unsigned short width, unsigned short height, uint32_t *table, uint8_t Planes, uint8_t comp)
 {
-	char *obuffer, *oziel,
-		 p, n, v1, v2;
-
-	unsigned int x, y;
-
+	uint8_t *obuffer;
+	uint8_t *oziel;
+	uint8_t p;
+	uint8_t n;
+	uint8_t v1, v2;
+	unsigned short x, y;
 	unsigned long tablen3;
 
-
-	obuffer = (char *)buffer;
+	obuffer = (uint8_t *) buffer;
 	oziel = ziel;
 
-	(char *)buffer += 0x200;
+	buffer += 0x200 / 2;
 
 	p = 0;
 	do
@@ -319,57 +192,51 @@ void decode_2bpc(char *ziel, unsigned int *buffer, unsigned int width, unsigned 
 		ziel = oziel + p;
 
 		y = height;
-		while(y--)
+		while (y--)
 		{
-			if(comp)
-				buffer = (unsigned int *)(obuffer + table[tablen3 + y]);
+			if (comp)
+				buffer = (uint16_t *) (obuffer + table[tablen3 + y]);
 
 			x = 0;
-			do /* width */
+			do							/* width */
 			{
-				if(comp == 0)
+				if (comp == 0)
 				{
 					x++;
 					*ziel++ = *buffer++ >> 8;
-				}
-				else
+				} else
 				{
-					v1 = (char)*buffer++;
-					if((n = v1 & 0x7f) == 0)
+					v1 = *buffer++;
+					if ((n = v1 & 0x7f) == 0)
 						return;
-					if(v1 & 0x80)
+					if (v1 & 0x80)
 					{
 						x += n;
-						while(n--)
+						while (n--)
 							*ziel++ = *buffer++ >> 8;
-					}
-					else
+					} else
 					{
 						v2 = *buffer++ >> 8;
 
 						x += n;
-						while(n--)
+						while (n--)
 							*ziel++ = v2;
 					}
-				} /* comp? */
-			} while(x < width); /* x */
-		} /* y */
-	} while(++p < Planes);
-
-	return;
-} /* decode_2bpc */
+				}
+			} while (x < width);
+		}
+	} while (++p < Planes);
+}
 
 
 /* Legt eine Palette fÅr "dithered" Bilder an */
-void make_pal(char *buffer, char *palette, char BitsPerPixel, unsigned long length, char ColorMap)
+static void make_pal(uint8_t *buffer, uint8_t *palette, uint8_t BitsPerPixel, unsigned long length, uint8_t ColorMap)
 {
-	char *pal,
-		 pixval;
+	uint8_t *pal;
+	uint8_t pixval;
+	unsigned short i;
 
-	unsigned int i;
-
-
-	if(BitsPerPixel == 1)
+	if (BitsPerPixel == 1)
 	{
 		pal = palette;
 
@@ -379,72 +246,65 @@ void make_pal(char *buffer, char *palette, char BitsPerPixel, unsigned long leng
 		pal[3] = 0;
 		pal[4] = 0;
 		pal[5] = 0;
-	}
-	else
-		if(BitsPerPixel == 8)
+	} else if (BitsPerPixel == 8)
+	{
+		if (ColorMap == 0x0)			/* Graustufenpalette fÅr 1 Channel 8 Bit erstellen */
 		{
-			if(ColorMap == 0x0)			/* Graustufenpalette fÅr 1 Channel 8 Bit erstellen */
+			pal = palette;
+
+			for (i = 0; i < 256; i++)
 			{
-				pal = palette;
-
-				for(i = 0; i < 256; i++)
-				{
-					*pal++ = (char)i;
-					*pal++ = (char)i;
-					*pal++ = (char)i;
-				}
+				*pal++ = i;
+				*pal++ = i;
+				*pal++ = i;
 			}
-			else
-				if(ColorMap == 0x01)		/* Palette aus 3r3g2b Bits erstellen */
-					while(length--)
-					{
-						pixval = *buffer++;
-						pal = palette + pixval + pixval + pixval;
+		} else if (ColorMap == 0x01)	/* Palette aus 3r3g2b Bits erstellen */
+			while (length--)
+			{
+				pixval = *buffer++;
+				pal = palette + pixval + pixval + pixval;
 
-						*pal++ = (pixval & 0x07) << 5;
-						*pal++ = (pixval & 0x38) << 2;
-						*pal++ = pixval & 0xc0;
-					}
-				else			
-					if(ColorMap == 0x03)	/* Palette aus extra File Åbertragen */
-					/* da ich das nicht kann, wird momentan eine Graustufenpalette erzeugt */
-					{
-						pal = palette;
+				*pal++ = (pixval & 0x07) << 5;
+				*pal++ = (pixval & 0x38) << 2;
+				*pal++ = pixval & 0xc0;
+		} else if (ColorMap == 0x03)	/* Palette aus extra File Åbertragen */
+			/* da ich das nicht kann, wird momentan eine Graustufenpalette erzeugt */
+		{
+			pal = palette;
 
-						for(i = 0; i < 256; i++)
-						{
-							*pal++ = (char)i;
-							*pal++ = (char)i;
-							*pal++ = (char)i;
-						}
-					}
+			for (i = 0; i < 256; i++)
+			{
+				*pal++ = i;
+				*pal++ = i;
+				*pal++ = i;
+			}
 		}
-} /* make_pal */
+	}
+}
 
 
 /* éndert die Orientierung der Bilder */
-int switch_orient(char *buffer, unsigned int width, unsigned int height, char BitsPerPixel)
+static short switch_orient(uint8_t *buffer, unsigned short width, unsigned short height, uint8_t BitsPerPixel)
 {
-	char *buffero, *bufferu, *temp;
-
-	unsigned int t;
-
+	uint8_t *buffero;
+	uint8_t *bufferu;
+	uint8_t *temp;
+	unsigned short t;
 	unsigned long w, len, planelength;
 
-
-	w = (unsigned long)width;	
+	w = width;
 
 	len = (w * BitsPerPixel) >> 3;
 	planelength = len * height;
 
-	if((temp = Malloc(len)) == 0)
-		return(-1);
+	if ((temp = (uint8_t *)Malloc(len)) == NULL)
+		return -1;
 
 	buffero = buffer;
 	bufferu = buffero + planelength - len;
 
 	t = height >> 1;
-	while(t--)
+	while (t--)
 	{
 		memcpy(temp, buffero, len);
 		memcpy(buffero, bufferu, len);
@@ -456,16 +316,130 @@ int switch_orient(char *buffer, unsigned int width, unsigned int height, char Bi
 
 	Mfree(temp);
 
-	return(0);
-} /* switch_orient */
+	return 0;
+}
 
 
-void nulltospace(char *string, char length)
+static void nulltospace(uint8_t *string, uint8_t length)
 {
-	while(--length)
+	while (--length)
 	{
-		if(*string == '\0')
+		if (*string == '\0')
 			*string = ' ';
 		string++;
 	}
-} /* nulltospace */
+}
+
+/* -------------------------------------------------*/
+/* -------------------------------------------------*/
+/*      SGI - Silicon Graphic-Format (SGI)          */
+/*		8, 24 Bit (in 3 Ebenen),					*/
+/*		ohne Komprimierung/RLE						*/
+/* -------------------------------------------------*/
+/* -------------------------------------------------*/
+short imp_module_main(GARGAMEL *smurf_struct)
+{
+	uint8_t *buffer;
+	uint8_t *ziel;
+	uint8_t comp;
+	uint8_t Planes;
+	uint8_t BitsPerPixel;
+	uint8_t ColorMap;
+	uint8_t Bpc;
+	char dummy[3];
+	char impmessag[21];
+	unsigned short width, height;
+	uint32_t *table;
+	unsigned long tablen;
+
+	buffer = smurf_struct->smurf_pic->pic_data;
+
+	if (*(uint16_t *) buffer != 0x01da)
+		return M_INVALID;
+
+	comp = *(buffer + 0x02);
+
+	Bpc = *(buffer + 0x03);
+
+#if 0
+	/* unwichtig */
+	Dim = *(uint16_t *) (buffer + 0x04);
+#endif
+
+	width = *(uint16_t *) (buffer + 0x06);
+	height = *(uint16_t *) (buffer + 0x08);
+
+	/* 1 = BW, dithered, colormapped; 3 = RGB; 4 = RGB + alpha channel */
+	Planes = *(uint16_t *) (buffer + 0x0a);
+	if (Planes > 3)					/* sonst wÑren das ja ZustÑnde */
+		Planes = 3;
+
+	/* 0x00 - Normal pixel values: 1 Channel Greyscale, 3 Channels RGB */
+	/* 0x01 - Dithered Images: (8 BPP 3+3+2) */
+	/* 0x02 - single Channel Images */
+	/* 0x03 - color Map */
+	ColorMap = *(uint32_t *) (buffer + 0x6c);
+	if (ColorMap > 3)
+		return M_UNKNOWN_TYPE;
+
+	BitsPerPixel = Planes * 8;
+
+
+	nulltospace(buffer + 0x18, 80);
+	strncpy(smurf_struct->smurf_pic->infotext, buffer + 0x18, sizeof(smurf_struct->smurf_pic->infotext) - 1);
+	strcpy(smurf_struct->smurf_pic->format_name, "SGI-Format .SGI, .RLE");
+	smurf_struct->smurf_pic->pic_width = width;
+	smurf_struct->smurf_pic->pic_height = height;
+	smurf_struct->smurf_pic->depth = BitsPerPixel;
+
+	strcpy(impmessag, "SGI ");
+	strcat(impmessag, itoa(BitsPerPixel, dummy, 10));
+	strcat(impmessag, " Bit");
+	smurf_struct->services->reset_busybox(128, impmessag);
+
+	if ((ziel = (uint8_t *) smurf_struct->services->SMalloc(((unsigned long) width * height * BitsPerPixel) >> 3)) == NULL)
+		return M_MEMORY;
+
+	if (comp)
+	{
+		tablen = (unsigned long) height * Planes * sizeof(*table) * 2;
+
+		if ((table = (uint32_t *) Malloc(tablen)) == NULL)
+		{
+			smurf_struct->services->SMfree(ziel);
+			return M_MEMORY;
+		} else
+		{
+			memcpy(table, buffer + 0x200, tablen);
+		}
+	} else
+	{
+		table = NULL;
+	}
+
+	if (Bpc == 1)
+		decode_1bpc(ziel, buffer, width, height, table, Planes, comp);
+	else
+		decode_2bpc(ziel, (uint16_t *) buffer, width, height, table, Planes, comp);
+
+	Mfree(table);
+
+	if (!comp)
+		if (switch_orient(ziel, width, height, BitsPerPixel) != 0)
+			form_alert(1, ERROR);
+
+	if (BitsPerPixel <= 8)
+		make_pal(ziel, smurf_struct->smurf_pic->palette, BitsPerPixel, (unsigned long) width * height, ColorMap);
+
+	smurf_struct->smurf_pic->pic_data = ziel;
+
+	smurf_struct->smurf_pic->format_type = FORM_PIXELPAK;
+
+	if (BitsPerPixel == 8)
+		smurf_struct->smurf_pic->col_format = GREY;
+	else
+		smurf_struct->smurf_pic->col_format = RGB;
+
+	smurf_struct->services->SMfree(buffer);
+	return M_PICDONE;
+}
