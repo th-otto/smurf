f10a46f576dba9a40adc8d3dcde73309b0b5e393
Thorsten Otto <admin@tho-otto.de>
2022-12-07 11:18:54 +0100

    Cleanup import/tny

--- modules/import/tny.c
+++ modules/import/tny.c
@@ -48,42 +48,52 @@
 #include "import.h"
 #include "smurfine.h"
 
-static void *(*SMalloc)(long amount);
-static void (*SMfree)(void *ptr);
-
-char *fileext(char *filename);
-
 /* Infostruktur fr Hauptmodul */
-MOD_INFO module_info = {"Tiny-Format",
-						0x0010,
-						"Christian Eyrich",
-						"TNY", "TN1", "TN2", "TN3", "",
-						"", "", "", "", "",
-						"Slider 1",
-						"Slider 2",
-						"Slider 3",
-						"Slider 4",
-						"Checkbox 1",
-						"Checkbox 2",
-						"Checkbox 3",
-						"Checkbox 4",
-						"Edit 1",
-						"Edit 2",
-						"Edit 3",
-						"Edit 4",
-						0,128,
-						0,128,
-						0,128,
-						0,128,
-						0,10,
-						0,10,
-						0,10,
-						0,10,
-						0, 0, 0, 0,
-						0, 0, 0, 0,
-						0, 0, 0, 0,
-						0
-						};
+MOD_INFO module_info = {
+	"Tiny-Format",
+	0x0010,
+	"Christian Eyrich",
+	{ "TNY", "TN1", "TN2", "TN3", "", "", "", "", "", "" },
+	"Slider 1",
+	"Slider 2",
+	"Slider 3",
+	"Slider 4",
+	"Checkbox 1",
+	"Checkbox 2",
+	"Checkbox 3",
+	"Checkbox 4",
+	"Edit 1",
+	"Edit 2",
+	"Edit 3",
+	"Edit 4",
+	0, 128,
+	0, 128,
+	0, 128,
+	0, 128,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0,
+	NULL, NULL, NULL, NULL, NULL, NULL
+};
+
+
+static char *fileext(const char *filename)
+{
+	char *extstart;
+
+	if ((extstart = strrchr(filename, '.')) != NULL)
+		extstart++;
+	else
+		extstart = strrchr(filename, '\0');
+
+	return extstart;
+}
+
 
 /* -------------------------------------------------*/
 /* -------------------------------------------------*/
@@ -93,142 +103,134 @@ MOD_INFO module_info = {"Tiny-Format",
 /* -------------------------------------------------*/
 short imp_module_main(GARGAMEL *smurf_struct)
 {
-	char *buffer1, *pal, *fname,
-		 BitsPerPixel, DatenOffset, v1, Planes;
-	char dummy[3], impmessag[21];
+	uint8_t *buffer1;
+	uint8_t *pal;
+	char *fname;
+	uint8_t BitsPerPixel;
+	uint8_t DatenOffset;
+	uint8_t v1;
+	uint8_t Planes;
+	char dummy[3];
+	char impmessag[21];
 
-	unsigned int *buffer2, *ziel, *ppal, i, n, width, height,
-				 controlbytes, datawords, length, v2;
+	uint16_t *buffer2;
+	uint16_t *ziel;
+	uint8_t *ppal;
+	unsigned short i;
+	unsigned short n;
+	unsigned short width, height;
+	unsigned short controlbytes;
+	unsigned short datawords;
+	unsigned short length;
+	unsigned short v2;
 
-	unsigned long src_pos2 = 0, dst_pos = 0;
-
-
-	SMalloc = smurf_struct->services->SMalloc;
-	SMfree = smurf_struct->services->SMfree;
+	unsigned long src_pos2;
+	unsigned long dst_pos;
 
 	buffer1 = smurf_struct->smurf_pic->pic_data;
-	buffer2 = (unsigned int *)buffer1;
+	buffer2 = (uint16_t *) buffer1;
 
 	fname = smurf_struct->smurf_pic->filename;
-	if(strnicmp(fileext(fname), "TN", 2) != 0 ||
-	   *(char *)buffer2 < 0x6)
-		return(M_INVALID);
-	else
+	if (strnicmp(fileext(fname), "TN", 2) != 0)
+		return M_INVALID;
+
+	switch (*buffer1)
 	{
-		switch(*(char *)buffer2)
+	case 0:
+		width = 320;
+		height = 200;
+		Planes = 4;
+		break;
+	case 1:
+		width = 640;
+		height = 200;
+		Planes = 2;
+		break;
+	case 2:
+		width = 640;
+		height = 400;
+		Planes = 1;
+		break;
+	default:
+		form_alert(1, ERROR);
+		return M_PICERR;
+	}
+	BitsPerPixel = Planes;
+
+	controlbytes = *(buffer1 + 0x23);
+	datawords = *(buffer1 + 0x25);	/* Anzahl Words sind abgespeichert! */
+
+	DatenOffset = 0x27;
+
+	strcpy(smurf_struct->smurf_pic->format_name, "Tiny-File .TNY");
+	smurf_struct->smurf_pic->pic_width = width;
+	smurf_struct->smurf_pic->pic_height = height;
+	smurf_struct->smurf_pic->depth = BitsPerPixel;
+
+	strcpy(impmessag, "Tiny-File ");
+	strcat(impmessag, itoa(BitsPerPixel, dummy, 10));
+	strcat(impmessag, " Bit");
+	smurf_struct->services->reset_busybox(128, impmessag);
+
+	if ((ziel = smurf_struct->services->SMalloc(((unsigned long) width * height * BitsPerPixel) >> 3)) == 0)
+		return M_MEMORY;
+
+	buffer1 += DatenOffset;
+	length = datawords;
+	src_pos2 = DatenOffset + controlbytes;
+	dst_pos = 0;
+
+	while (length)
+	{
+		if ((v1 = *buffer1++) > 0x80)
 		{
-			case 0:
-					width = 320;
-					height = 200;
-					Planes = 4;
-					break;
-			case 1:	width = 640;
-					height = 200;
-					Planes = 2;
-					break;
-			case 2:
-					width = 640;
-					height = 400;
-					Planes = 1;
-					break;
-			default:	form_alert(0,  ERROR);
-						return(M_PICERR);
+			n = 0x100 - v1;
+			for (i = 0; i < n; i++)
+				ziel[dst_pos++] = buffer2[src_pos2++];
+			length -= n;
+		} else if (v1 == 0x00)
+		{
+			n = (buffer1[0] << 8) | buffer1[1];
+			buffer1 += 2;
+			v2 = buffer2[src_pos2++];
+			for (i = 0; i < n; i++)
+				ziel[dst_pos++] = v2;
+			length -= n;
+		} else if (v1 == 0x01)
+		{
+			n = (buffer1[0] << 8) | buffer1[1];
+			buffer1 += 2;
+			ziel[dst_pos++] = buffer2[src_pos2++];
+			length -= n;
+		} else
+		{
+			n = v1;
+			v2 = buffer2[src_pos2++];
+			for (i = 0; i < n; i++)
+				ziel[dst_pos++] = v2;
+			length -= n;
 		}
-		BitsPerPixel = Planes;
+	}
 
-		controlbytes = *((char *)buffer2 + 0x23);
-		datawords = *((char *)buffer2 + 0x25);			/* Anzahl Words sind abgespeichert! */
+	smurf_struct->smurf_pic->format_type = 1;
 
-		DatenOffset = 0x27;
+	pal = smurf_struct->smurf_pic->palette;
+	ppal = (buffer1 + 0x01);
 
-		strncpy(smurf_struct->smurf_pic->format_name, "Tiny-File .TNY", 21);
-		smurf_struct->smurf_pic->pic_width = width;
-		smurf_struct->smurf_pic->pic_height = height;
-		smurf_struct->smurf_pic->depth = BitsPerPixel;
+	for (i = 0; i < 16; i++)
+	{
+		v2 = (ppal[0] << 8) | ppal[1];
+		ppal += 2;
 
-		strcpy(impmessag, "Tiny-File ");
-		strcat(impmessag, itoa(BitsPerPixel, dummy, 10));
-		strcat(impmessag, " Bit");
-		smurf_struct->services->reset_busybox(128, impmessag);
+		*pal++ = ((v2 & 0x700) >> 4);
+		*pal++ = (v2 & 0x070);
+		*pal++ = ((v2 & 0x007) << 4);
+	}
 
-		if((ziel = SMalloc(((long)width * (long)height * BitsPerPixel) >> 3)) == 0)
-			return(M_MEMORY);
-		else
-		{
-			buffer1 += DatenOffset;
-			length = datawords;
-			src_pos2 += DatenOffset + controlbytes;
+	smurf_struct->smurf_pic->pic_data = ziel;
 
-			while(length)
-			{
-				if((v1 = *buffer1++) > 0x80)
-				{
-					n = 0x100 - v1;
-					for(i = 0; i < n; i++)
-						ziel[dst_pos++] = buffer2[src_pos2++];
-					length -= n;
-				}
-				else
-					if(v1 == 0x00)
-					{
-						n = *(unsigned int *)buffer1++;
-						v2 = buffer2[src_pos2++];
-						for(i = 0; i < n; i++)
-							ziel[dst_pos++] = v2;
-						length -= n;
-					}
-					else
-						if(v1 == 0x01)
-						{
-							n = *(unsigned int *)buffer1++;
-							ziel[dst_pos++] = buffer2[src_pos2++];
-							length -= n;
-						}
-						else
-						{
-							n = v1;
-							v2 = buffer2[src_pos2++];
-							for(i = 0; i < n; i++)
-								ziel[dst_pos++] = v2;
-							length -= n;						
-						}
-			}
-		
-			smurf_struct->smurf_pic->format_type = 1;
-	
-			pal = smurf_struct->smurf_pic->palette;
-			ppal = (unsigned int *)(buffer1 + 0x01);
-				
-			for(i = 0; i < 16; i++)
-			{
-				v2 = *ppal++;
+	smurf_struct->smurf_pic->col_format = RGB;
 
-				*pal++ = (char)((v2 & 0x700) >> 4);
-				*pal++ = (char)(v2 & 0x070);
-				*pal++ = (char)((v2 & 0x007) << 4);
-				
-			} /* Palette */
-
-			smurf_struct->smurf_pic->pic_data = ziel;
-
-			smurf_struct->smurf_pic->col_format = RGB; 
-		} /* Malloc */
-	} /* Erkennung */
-
-	SMfree((char *)buffer2);
-	return(M_PICDONE);
+	smurf_struct->services->SMfree(buffer2);
+	return M_PICDONE;
 }
-
-
-char *fileext(char *filename)
-{
-	char *extstart;
-
-
-	if((extstart = strrchr(filename, '.')) != NULL)
-		extstart++;
-	else
-		extstart = strrchr(filename, '\0');
-	
-	return(extstart);
-} /* fileext */
