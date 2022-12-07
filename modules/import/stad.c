36622a284fca143b04ef96aa3aa6da15afe9895f
Thorsten Otto <admin@tho-otto.de>
2022-12-07 08:56:59 +0100

    Cleanup import/stad

--- modules/import/stad.c
+++ modules/import/stad.c
@@ -31,36 +31,37 @@
 #define DEBUG           0
 
 /* Infostruktur fr Hauptmodul */
-MOD_INFO module_info = {"Atari Stad-Grafik",
-                        0x0010,
-                        "Dale Russell",
-                        "PAC", "", "", "", "",
-                        "", "", "", "", "",
-                        "Slider 1",
-                        "Slider 2",
-                        "Slider 3",
-                        "Slider 4",
-                        "Checkbox 1",
-                        "Checkbox 2",
-                        "Checkbox 3",
-                        "Checkbox 4",
-                        "Edit 1",
-                        "Edit 2",
-                        "Edit 3",
-                        "Edit 4",
-                        0,128,
-                        0,128,
-                        0,128,
-                        0,128,
-                        0,10,
-                        0,10,
-                        0,10,
-                        0,10,
-                        0,0,0,0,
-                        0,0,0,0,
-                        0,0,0,0,
-                        0
-                        };
+MOD_INFO module_info = {
+	"Atari Stad-Grafik",
+	0x0010,
+	"Dale Russell",
+	{ "PAC", "", "", "", "", "", "", "", "", "" },
+	"Slider 1",
+	"Slider 2",
+	"Slider 3",
+	"Slider 4",
+	"Checkbox 1",
+	"Checkbox 2",
+	"Checkbox 3",
+	"Checkbox 4",
+	"Edit 1",
+	"Edit 2",
+	"Edit 3",
+	"Edit 4",
+	0, 128,
+	0, 128,
+	0, 128,
+	0, 128,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 10,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0,
+	NULL, NULL, NULL, NULL, NULL, NULL
+};
 
 /* -------------------------------------------------*/
 /* -------------------------------------------------*/
@@ -69,147 +70,174 @@ MOD_INFO module_info = {"Atari Stad-Grafik",
 /* -------------------------------------------------*/
 short imp_module_main(GARGAMEL *smurf_struct)
 {
-char    *buffer;
-int width=0, height=0, ID_Byte, Pack_Byte, Special_Byte;
-int xc,yc, x,c,n;
-char *buf, *out, *oout;
-int VertMode=FALSE, HorzMode=FALSE, ypos;
-int ytab[401];
+	uint8_t *buffer;
+	short width, height;
+	short ID_Byte;
+	short Pack_Byte;
+	short Special_Byte;
+	short xc, yc, x, c, n;
+	uint8_t *buf;
+	uint8_t *out;
+	uint8_t *oout;
+	short HorzMode;
+	short ypos;
+	short ytab[401];
+	uint8_t *pal;
 
 /***************************************************************/
 /*      Kopfdaten analysieren                                  */
 /***************************************************************/
-buffer=smurf_struct->smurf_pic->pic_data;
-if (strncmp(buffer,"pM85",4)==0) HorzMode=TRUE;
-else if (strncmp(buffer,"pM86",4)==0) VertMode=TRUE;
-else return(M_INVALID);
+	buffer = smurf_struct->smurf_pic->pic_data;
+	if (strncmp((char *)buffer, "pM85", 4) == 0)
+		HorzMode = TRUE;
+	else if (strncmp((char *)buffer, "pM86", 4) == 0)
+		HorzMode = FALSE;
+	else
+		return M_INVALID;
 
-smurf_struct->services->reset_busybox(128, "STAD 1 Bit");
+	smurf_struct->services->reset_busybox(128, "STAD 1 Bit");
 
 /*************************************************************/
 /*  Headerdaten auslesen                                     */
 /*************************************************************/
 
-ID_Byte=*(buffer+4);
-Pack_Byte=*(buffer+5);
-Special_Byte=*(buffer+6);
+	ID_Byte = *(buffer + 4);
+	Pack_Byte = *(buffer + 5);
+	Special_Byte = *(buffer + 6);
 
-strncpy(smurf_struct->smurf_pic->format_name, "Atari STAD Grafik   ", 21);
-memset(smurf_struct->smurf_pic->palette+3, 3, 3L);
-memset(smurf_struct->smurf_pic->palette, 255, 3L);
-width=640;
-height=400;
-buf=buffer+0x07;            /* Datenbeginn */
-out=Mxalloc(32000L, 0);
-if (!out) return(M_MEMORY);
-memset(out, 0, 32000L);
-oout=out;
+	strcpy(smurf_struct->smurf_pic->format_name, "Atari STAD Grafik");
+ 	pal = smurf_struct->smurf_pic->palette;
+	pal[0] = pal[1] = pal[2] = 255;
+	pal[3] = pal[4] = pal[5] = 0;
+
+	width = 640;
+	height = 400;
+	buf = buffer + 0x07;				/* Datenbeginn */
+	out = (uint8_t *)Malloc(32000L);
+	if (!out)
+		return M_MEMORY;
+	memset(out, 0, 32000L);
+	oout = out;
 #if DEBUG>0
-    (void) Cconws("\33E")
-    printf("\n\n        -> Id-Byte:%i",ID_Byte);
-    printf("\n      -> Pack-Byte:%i",Pack_Byte);
-    printf("\n      -> Special-Byte:%i",Special_Byte);
-    printf("\n      -> VMode:%i --- HMode:%i\n",VertMode,HorzMode);
-    #if DEBUG>1
-    for (x=0; x<256; x++)
-    {   printf("\nPos.%x (=%i) = %x (=%i)",x,x,*(buffer+x),*(buffer+x));
-        getch(); }
-    #endif  
-#endif  
+	(void) Cconws("\33E") printf("\n\n        -> Id-Byte:%i", ID_Byte);
+	printf("\n      -> Pack-Byte:%i", Pack_Byte);
+	printf("\n      -> Special-Byte:%i", Special_Byte);
+	printf("\n      -> VMode:%i --- HMode:%i\n", VertMode, HorzMode);
+#if DEBUG>1
+	for (x = 0; x < 256; x++)
+	{
+		printf("\nPos.%x (=%i) = %x (=%i)", x, x, *(buffer + x), *(buffer + x));
+		(void)Cnecin();
+	}
+#endif
+#endif
 
 /***************************************************************/
 /*      Horizontale Dekodierung                                */
 /***************************************************************/
-if (HorzMode==TRUE)
-{
-yc=0;
-while(yc++<height)
-{
-xc=0;
-  while(xc<80)
-  {
-    x=*(buf++);
-    if (x==ID_Byte)
-    {
-        n=*(buf++)+1;
-        xc+=n;
-        while (n--)
-            *(out++)=Pack_Byte;
-    }
-    else if (x==Special_Byte)
-    {
-        c=*(buf++);
-        n=*(buf++)+1;
-        xc+=n;
-        while(n--)
-            *(out++)=c;
-    }
-    else { *(out++)=x; xc++; }
-   } /* x-loop */
-} /* y-loop */
-}
+	if (HorzMode)
+	{
+		yc = 0;
+		while (yc++ < height)
+		{
+			xc = 0;
+			while (xc < 80)
+			{
+				x = *(buf++);
+				if (x == ID_Byte)
+				{
+					n = *(buf++) + 1;
+					xc += n;
+					while (n--)
+						*(out++) = Pack_Byte;
+				} else if (x == Special_Byte)
+				{
+					c = *(buf++);
+					n = *(buf++) + 1;
+					xc += n;
+					while (n--)
+						*(out++) = c;
+				} else
+				{
+					*(out++) = x;
+					xc++;
+				}
+			}
+		}
+	}
 /***************************************************************/
 /*      Vertikale Dekodierung                                  */
 /***************************************************************/
-else if (VertMode==TRUE)
-{
+	else
+	{
 
 /*  Y-Tabelle fllen */
 
-for (x=0; x<=400; x++)
-    ytab[x]=(x<<6)+(x<<4);      /* ypos x 80 */
-    
-xc=x=ypos=0;
-yc=0;
-while(xc<80)
-{
-if (yc>=400)  yc-=400; 
-  while(yc<400)
-  {
-    x=*(buf++);
-    if (x==ID_Byte)
-    {
-        n=*(buf++)+1;
-        yc+=n; 
-        while(n--)
-        {
-            *(out+xc+ytab[ypos])=Pack_Byte;
-            ypos++; 
-            if (ypos>399) 
-                {   ypos=0; xc++;}
-        }   
-    }
-    else if (x==Special_Byte)
-    {
-        c=*(buf++);
-        n=*(buf++)+1;
-        yc+=n;
-        while(n--)
-        {
-            *(out+xc+ytab[ypos])=c;
-            ypos++; 
-            if (ypos>399) 
-                {   ypos=0; xc++;}
-        }   
-    }
-    else { *(out+xc+ytab[ypos])=x; 
-            ypos++; yc++;
-            if (ypos>399)
-                { ypos=0; xc++;}
-            }
-   } /* y-loop */
-} /* x-loop */
-}
+		for (x = 0; x <= 400; x++)
+			ytab[x] = (x << 6) + (x << 4);	/* ypos x 80 */
+
+		xc = x = ypos = 0;
+		yc = 0;
+		while (xc < 80)
+		{
+			if (yc >= 400)
+				yc -= 400;
+			while (yc < 400)
+			{
+				x = *(buf++);
+				if (x == ID_Byte)
+				{
+					n = *(buf++) + 1;
+					yc += n;
+					while (n--)
+					{
+						*(out + xc + ytab[ypos]) = Pack_Byte;
+						ypos++;
+						if (ypos > 399)
+						{
+							ypos = 0;
+							xc++;
+						}
+					}
+				} else if (x == Special_Byte)
+				{
+					c = *(buf++);
+					n = *(buf++) + 1;
+					yc += n;
+					while (n--)
+					{
+						*(out + xc + ytab[ypos]) = c;
+						ypos++;
+						if (ypos > 399)
+						{
+							ypos = 0;
+							xc++;
+						}
+					}
+				} else
+				{
+					*(out + xc + ytab[ypos]) = x;
+					ypos++;
+					yc++;
+					if (ypos > 399)
+					{
+						ypos = 0;
+						xc++;
+					}
+				}
+			}							/* y-loop */
+		}								/* x-loop */
+	}
 /***************************************************************/
 /*      Strukturen fllen                                      */
 /***************************************************************/
-Mfree(buffer);
-smurf_struct->smurf_pic->pic_data=oout;
-smurf_struct->smurf_pic->pic_width=width;
-smurf_struct->smurf_pic->pic_height=height;
-smurf_struct->smurf_pic->depth=1;
-smurf_struct->smurf_pic->bp_pal=0;
-smurf_struct->smurf_pic->format_type=1;
-smurf_struct->smurf_pic->col_format=WURSCHT;
-return(M_PICDONE);
+	Mfree(buffer);
+	smurf_struct->smurf_pic->pic_data = oout;
+	smurf_struct->smurf_pic->pic_width = width;
+	smurf_struct->smurf_pic->pic_height = height;
+	smurf_struct->smurf_pic->depth = 1;
+	smurf_struct->smurf_pic->bp_pal = 0;
+	smurf_struct->smurf_pic->format_type = FORM_STANDARD;
+	smurf_struct->smurf_pic->col_format = WURSCHT;
+	return M_PICDONE;
 }
